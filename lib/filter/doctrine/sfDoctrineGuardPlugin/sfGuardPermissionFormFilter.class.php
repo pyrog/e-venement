87bdde1e7f85a50815234130ace4ec7395781b98
beta <betaglop@users.noreply.github.com>
2011-06-01 16:07:04 +0000

    (general) adding missing libs

---
+++ lib/filter/doctrine/sfDoctrineGuardPlugin/sfGuardPermissionFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * sfGuardPermission filter form.
+ *
+ * @package    e-venement
+ * @subpackage filter
+ * @author     Baptiste SIMON <baptiste.simon AT e-glop.net>
+ * @version    SVN: $Id: sfDoctrinePluginFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class sfGuardPermissionFormFilter extends PluginsfGuardPermissionFormFilter
+{
+  public function configure()
+  {
+  }
+}
