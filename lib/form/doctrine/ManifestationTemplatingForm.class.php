5e0b160c01d56c893e608bb7ad8c5147ea64492f
beta <betaglop@users.noreply.github.com>
2011-06-16 15:21:09 +0000

    (event) adding a needed form for templating

---
+++ lib/form/doctrine/ManifestationTemplatingForm.class.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * ManifestationTemplating form.
+ *
+ * @package    e-venement
+ * @subpackage form
+ * @author     Baptiste SIMON <baptiste.simon AT e-glop.net>
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class ManifestationTemplatingForm extends BaseFormDoctrine
+{
+  public function configure()
+  {
+    $this->widgetSchema->setNameFormat('template[%s]');
+    
+    // the template
+    $this->widgetSchema['manifestation_model'] = new sfWidgetFormDoctrineJQueryAutocompleter(array(
+      'model' => 'Manifestation',
+      'url' => url_for('manifestation/ajax'),
+    ));
+    $this->validatorSchema['manifestation_model'] = new sfValidatorDoctrineChoice(array(
+      'model' => 'Manifestation',
+    ));
+    
+    // where to applicate it
+    $this->widgetSchema['manifestations_list'] = new sfWidgetFormDoctrineChoice(array(
+      'model' => 'Manifestation',
+      'multiple' => true,
+      'query' => Doctrine::getTable('Manifestation')->createQuery('m')->andWhere('happens_at >= now()'),
+    ));
+    $this->validatorSchema['manifestations_list'] = new sfValidatorDoctrineChoice(array(
+      'model' => 'Manifestation',
+      'multiple' => true,
+    ));
+  }
+  
+  public function save($con = null)
+  {
+    $values = $this->getValues();
+    $q = Doctrine::getTable('PriceManifestation')->createQuery('mp')
+      ->andWhere('manifestation_id = ?',$values['manifestation_model']);
+    $manifprices = $q->execute();
+    
+    foreach ( $values['manifestations_list'] as $manifid )
+    {
+      $q = new Doctrine_Query();
+      $q->from('PriceManifestation mp')
+        ->where('mp.manifestation_id = ?',$manifid)
+        ->delete();
+      
+      foreach ( $manifprices as $manifprice )
+      {
+        $manifprice = $manifprice->copy();
+        $manifprice['id'] = null;
+        $manifprice['manifestation_id'] = $manifid;
+        $manifprice->save();
+      }
+    }
+  }
+
+  public function getModelName()
+  {
+    return 'Manifestation';
+  }
+}
