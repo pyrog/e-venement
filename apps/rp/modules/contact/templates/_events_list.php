308238bc687bf34eba9b40ab63486c60045dbc7d
beta <betaglop@users.noreply.github.com>
2011-06-22 08:13:28 +0000

    (rp) correcting the cross_app_link_to the ticketting process in the contact file

--- apps/rp/modules/contact/templates/_events_list.php
+++ apps/rp/modules/contact/templates/_events_list.php
@@ -2,7 +2,7 @@
 <ul class="sf_form_field_events_list">
 <?php foreach ($contact->Transactions as $transaction): ?>
   <li>
-    #<?php echo link_to($transaction,'tansaction/sell?id='.$transaction->id) ?>,
+    #<?php echo cross_app_link_to($transaction,'tck','ticket/sell?id='.$transaction->id) ?>,
     <?php echo format_date($transaction->created_at) ?>
     <?php echo __('for %%i%% ticket(s) and',array('%%i%%' => $transaction->Tickets->count())) ?> 
     <?php $sum = 0; foreach ( $transaction->Payments as $pay ) $sum += $pay->value; echo format_currency($sum,'€'); ?> 
