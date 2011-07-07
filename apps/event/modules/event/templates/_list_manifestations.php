f385cd8b4883f8db682410a1b6342604499d0f7d
beta <betaglop@users.noreply.github.com>
2011-07-07 08:58:25 +0000

    (event) adding a forgotten partial

---
+++ apps/event/modules/event/templates/_list_manifestations.php
@@ -0,0 +1,6 @@
+<?php $manifs = array() ?>
+<?php foreach ( $event->Manifestations as $manif ): ?>
+  <?php $manifs[$manif->happens_at] = link_to(format_date($manif->happens_at,'d MMM yyyy HH:mm'),'manifestation/show?id='.$manif->id) ?>
+<?php endforeach ?>
+<?php sort($manifs) ?>
+<?php echo implode(', ',$manifs) ?>
