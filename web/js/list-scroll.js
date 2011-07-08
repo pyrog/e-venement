9c77a2f9c0fe6dc6c97ca486721e1bc9926ff1b4
beta <betaglop@users.noreply.github.com>
2011-07-08 20:31:47 +0000

    (global) adding a pagination feature which adds through an ajax request the rest of the listing called in the next page

---
+++ web/js/list-scroll.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  list_scroll();
+});
+function list_scroll()
+{
+  $('.sf_admin_pagination .ui-icon-seek-next').parent().unbind().click(function(){
+    $.get($(this).attr('href'),function(data){
+      $('.sf_admin_list > table > tbody').append($(data).find('.sf_admin_list > table > tbody tr.sf_admin_row'));
+      $('#sf_admin_pager')
+        .replaceWith($(data).find('#sf_admin_pager'));
+      list_scroll();
+    });
+    return false;
+  });
+}
