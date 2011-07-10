331faf5e53c601bb39fe9d5d5c7a8fb3d0e3664d
beta <betaglop@users.noreply.github.com>
2011-07-10 21:11:15 +0000

    (stats) nothing really

--- apps/stats/modules/prices/templates/indexSuccess.php
+++ apps/stats/modules/prices/templates/indexSuccess.php
@@ -1,5 +1,6 @@
 <?php include_partial('chart_all') ?>
 <?php include_partial('chart_printed') ?>
+<div class="clear"></div>
 <?php include_partial('chart_ordered') ?>
 <?php include_partial('chart_asked') ?>
 <div class="clear"></div>
