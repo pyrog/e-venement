f16a553e794c5b4efd640de4437ac47721177e27
beta <betaglop@users.noreply.github.com>
2011-07-10 13:57:36 +0000

    (stats) adding a recording feature on charts

--- apps/stats/modules/attendance/actions/actions.class.php
+++ apps/stats/modules/attendance/actions/actions.class.php
@@ -19,23 +19,45 @@ class attendanceActions extends sfActions
   {
   }
   
+  public function executeCsv(sfWebRequest $request)
+  {
+    $this->lines = $this->getManifs('array');
+    foreach ( $this->lines as $key => $line )
+      $this->lines[$key]['free'] = $line['gauge']-$line['printed']-$line['asked']-$line['ordered'];
+    
+    $params = OptionCsvForm::getDBOptions();
+    $this->options = array(
+      'ms' => isset($params['option']['ms']),
+      'fields' => array('event_name','happens_at','location_name','location_city','gauge','printed','ordered','asked','free'),
+      'tunnel' => false,
+      'noheader' => false,
+    );
+    
+    $this->outstream = 'php://output';
+    $this->delimiter = $this->options['ms'] ? ';' : ',';
+    $this->enclosure = '"';
+    $this->charset   = sfContext::getInstance()->getConfiguration()->charset;
+    
+    sfConfig::set('sf_escaping_strategy', false);
+    sfConfig::set('sf_charset', $this->options['ms'] ? $this->charset['ms'] : $this->charset['db']);
+    
+    if ( !$request->hasParameter('debug') )
+    {
+      sfConfig::set('sf_web_debug', false);
+      $this->getResponse()->setContentType('text/comma-separated-values');
+      $this->getResponse()->sendHttpHeaders();
+    }
+    else
+      $this->setLayout(true);
+  }
+  
   public function executeData(sfWebRequest $request)
   {
     sfContext::getInstance()->getConfiguration()->loadHelpers(array('I18N','Date','CrossAppLink'));
+    $manifs = $this->getManifs();
     
-    $q = Doctrine::getTable('Manifestation')->createQuery('m')
-      ->select('*')
-      ->addSelect('(SELECT sum(gg.value) FROM gauge gg WHERE m.id = gg.manifestation_id) AS gauge')
-      ->addSelect('(SELECT sum(tt.printed AND duplicate IS NULL) FROM ticket tt WHERE m.id = tt.manifestation_id AND tt.id NOT IN (SELECT ttt.cancelling FROM ticket ttt WHERE ttt.cancelling IS NOT NULL AND ttt.manifestation_id = m.id AND ttt.printed = TRUE)) AS printed')
-      ->addSelect('(SELECT sum(NOT tt2.printed AND duplicate IS NULL) FROM ticket tt2 WHERE m.id = tt2.manifestation_id AND tt2.id NOT IN (SELECT ttt2.cancelling FROM ticket ttt2 WHERE ttt2.cancelling IS NOT NULL AND ttt2.manifestation_id = m.id) AND tt2.transaction_id IN (SELECT oo.transaction_id FROM order oo)) AS ordered')
-      ->addSelect('(SELECT sum(NOT tt3.printed AND duplicate IS NULL) FROM ticket tt3 WHERE m.id = tt3.manifestation_id AND tt3.id NOT IN (SELECT ttt3.cancelling FROM ticket ttt3 WHERE ttt3.cancelling IS NOT NULL AND ttt3.manifestation_id = m.id) AND tt3.transaction_id NOT IN (SELECT oo3.transaction_id FROM order oo3)) AS asked')
-      ->andWhere('m.happens_at < ?',date('Y-m-d',strtotime('+ 2 month')))
-      ->andWhere('m.happens_at >= ?',date('Y-m-d',strtotime('- 1 month')))
-      ->orderBy('m.happens_at, e.name');
-    $manifs = $q->execute();
-    
-    //$bar0 = new stBarOutline( 40, '#7cec78', '#17b912' );
-    //$bar0->key( __('Gauge'), 10 );
+    $bar0 = new stBarOutline( 40, '#7cec78', '#17b912' );
+    $bar0->key( __('Free tickets'), 10 );
     $bar1 = new stBarOutline( 40, '#789aec', '#1245b9' );
     $bar1->key( __('Asked tickets'), 10 );
     $bar2 = new stBarOutline( 40, '#eca478', '#fe8134' );
@@ -44,13 +66,17 @@ class attendanceActions extends sfActions
     $bar3->key( __('Printed tickets'), 10 );
     
     //Passing the random data to bar chart
-    $names = array();
+    $names = $max = array();
     $bar0->data = $bar3->data = array();
     foreach ( $manifs as $manif )
     {
       $names[] = $manif->Event.' @ '.format_date($manif->happens_at);
       
-      //$bar0->data[] = $manif->gauge;
+      $max[] = 100 * ($manif->gauge-$manif->asked-$manif->ordered-$manif->printed)/($manif->gauge != 0 ? $manif->gauge : 1);
+      $max[] = 100 * $manif->asked/($manif->gauge != 0 ? $manif->gauge : 1);
+      $max[] = 100 * $manif->ordered/($manif->gauge != 0 ? $manif->gauge : 1);
+      $max[] = 100 * $manif->printed/($manif->gauge != 0 ? $manif->gauge : 1);
+      $bar0->add_link(100 * ($manif->gauge-$manif->asked-$manif->ordered-$manif->printed)/($manif->gauge != 0 ? $manif->gauge : 1),cross_app_url_for('event','manifestation/show?id='.$manif->id,true));
       $bar1->add_link(100 * $manif->asked/($manif->gauge != 0 ? $manif->gauge : 1),cross_app_url_for('event','manifestation/show?id='.$manif->id,true));
       $bar2->add_link(100 * $manif->ordered/($manif->gauge != 0 ? $manif->gauge : 1),cross_app_url_for('event','manifestation/show?id='.$manif->id,true));
       $bar3->add_link(100 * $manif->printed/($manif->gauge != 0 ? $manif->gauge : 1),cross_app_url_for('event','manifestation/show?id='.$manif->id,true));
@@ -58,17 +84,18 @@ class attendanceActions extends sfActions
     
     //Creating a stGraph object
     $g = new stGraph();
-    $g->title( __('Gauge filling'), '{font-size: 20px;}' );
+    //$g->title( __('Gauge filling'), '{font-size: 20px;}' );
     $g->bg_colour = '#E4F5FC';
+    $g->bg_colour = '#FFFFFF';
     $g->set_inner_background( '#E3F0FD', '#CBD7E6', 90 );
     $g->x_axis_colour( '#8499A4', '#E4F5FC' );
     $g->y_axis_colour( '#8499A4', '#E4F5FC' );
  
     //Pass stBarOutline object i.e. $bar to graph
-    //$g->data_sets[] = $bar0;
     $g->data_sets[] = $bar1;
     $g->data_sets[] = $bar2;
     $g->data_sets[] = $bar3;
+    $g->data_sets[] = $bar0;
  
     //Setting labels for X-Axis
     $g->set_x_labels($names);
@@ -83,11 +110,28 @@ class attendanceActions extends sfActions
     //set maximum value for y-axis
     //we can fix the value as 20, 10 etc.
     //but its better to use max of data
-    $g->set_y_max(100);
+    $max = ceil(max($max)/25)*25;
+    $g->set_y_max($max);
     $g->y_label_steps( 4 );
     $g->set_y_legend( __('Percentage on gauge'), 12, '#18A6FF' );
     echo $g->render();
  
     return sfView::NONE;
+  }
+  
+  protected function getManifs($type = NULL)
+  {
+    $q = Doctrine::getTable('Manifestation')->createQuery('m')
+      ->select('m.id, m.happens_at, e.name AS event_name, l.name AS location_name, l.city AS location_city')
+      //->select('m.*')
+      ->addSelect('(SELECT sum(gg.value) FROM gauge gg WHERE m.id = gg.manifestation_id AND gg.workspace_id IN (\''.implode("','",array_keys($this->getUser()->getWorkspacesCredentials())).'\')) AS gauge')
+      ->addSelect('(SELECT sum(tt.printed AND duplicate IS NULL) FROM ticket tt WHERE m.id = tt.manifestation_id AND tt.id NOT IN (SELECT ttt.cancelling FROM ticket ttt WHERE ttt.cancelling IS NOT NULL AND ttt.manifestation_id = m.id AND ttt.printed = TRUE)) AS printed')
+      ->addSelect('(SELECT sum(NOT tt2.printed AND duplicate IS NULL) FROM ticket tt2 WHERE m.id = tt2.manifestation_id AND tt2.id NOT IN (SELECT ttt2.cancelling FROM ticket ttt2 WHERE ttt2.cancelling IS NOT NULL AND ttt2.manifestation_id = m.id) AND tt2.transaction_id IN (SELECT oo.transaction_id FROM order oo)) AS ordered')
+      ->addSelect('(SELECT sum(NOT tt3.printed AND duplicate IS NULL) FROM ticket tt3 WHERE m.id = tt3.manifestation_id AND tt3.id NOT IN (SELECT ttt3.cancelling FROM ticket ttt3 WHERE ttt3.cancelling IS NOT NULL AND ttt3.manifestation_id = m.id) AND tt3.transaction_id NOT IN (SELECT oo3.transaction_id FROM order oo3)) AS asked')
+      ->andWhere('m.happens_at < ?',date('Y-m-d',strtotime('+ 2 month')))
+      ->andWhere('m.happens_at >= ?',date('Y-m-d',strtotime('- 1 month')))
+      ->andWhereIn('e.meta_event_id',array_keys($this->getUser()->getMetaEventsCredentials()))
+      ->orderBy('m.happens_at, e.name');
+    return $type == 'array' ? $q->fetchArray() : $q->execute();
   }
 }
--- apps/stats/modules/attendance/templates/indexSuccess.php
+++ apps/stats/modules/attendance/templates/indexSuccess.php
@@ -1,1 +1,9 @@
-<?php stOfc::createChart(900, 530, 'attendance/data', false); ?>
+<div class="ui-widget ui-corner-all ui-widget-content">
+  <div class="ui-widget-header ui-corner-all fg-toolbar">
+    <h1><?php echo __('Gauge filling') ?></h1>
+  </div>
+  <div class="chart">
+    <?php stOfc::createChart(900, 530, 'attendance/data', false); ?>
+  </div>
+  <div class="actions"><?php include_partial('record') ?></div>
+</div>
