9dea59fedeb98f17f3a4b01f551191e35f4a2af5
beta <betaglop@users.noreply.github.com>
2011-07-10 11:42:55 +0000

    (global) deactivating the "next" button in objects lists during it's completing (ajax)

--- web/js/list-scroll.js
+++ web/js/list-scroll.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 function list_scroll()
 {
   $('.sf_admin_pagination .ui-icon-seek-next').parent().unbind().click(function(){
+    $('.sf_admin_pagination .ui-icon-seek-next').parent().unbind().click(function(){return false;});
     $.get($(this).attr('href'),function(data){
       $('.sf_admin_list > table > tbody').append($(data).find('.sf_admin_list > table > tbody tr.sf_admin_row'));
       $('#sf_admin_pager')
