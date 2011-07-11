25aa9fa3b30434eff91526d2f35cfb168e1e8b71
beta <betaglop@users.noreply.github.com>
2011-07-11 20:30:58 +0000

    (stats) adding the form for criterias

---
+++ lib/form/doctrine/StatsCriteriasForm.class.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Base project form.
+ * 
+ * @package    e-venement
+ * @subpackage form
+ * @author     Your name here 
+ * @version    SVN: $Id: BaseForm.class.php 20147 2009-07-13 11:46:57Z FabianLange $
+ */
+class StatsCriteriasForm extends BaseForm
+{
+  public function configure()
+  {
+    $this->widgetSchema['dates'] = new sfWidgetFormDateRange(array(
+      'from_date' => new liWidgetFormJQueryDateText(array('culture' => sfContext::getInstance()->getUser()->getCulture())),
+      'to_date'   => new liWidgetFormJQueryDateText(array('culture' => sfContext::getInstance()->getUser()->getCulture())),
+      'template'  => 'du %from_date%<br/> au %to_date%',
+    ));
+    $this->validatorSchema['dates'] = new sfValidatorDateRange(array(
+      'from_date' => new sfValidatorDate(array('required' => false)),
+      'to_date'   => new sfValidatorDate(array('required' => false)),
+      'required' => false,
+    ));
+
+    $this->widgetSchema['users'] = new sfWidgetFormDoctrineChoice(array(
+      'model'     => 'sfGuardUser',
+      'add_empty' => true,
+      'order_by'  => array('first_name, last_name',''),
+      'multiple'  => true,
+    ));
+    $this->validatorSchema['users'] = new sfValidatorDoctrineChoice(array(
+      'model' => 'sfGuardUser',
+      'multiple' => true,
+      'required' => false,
+    ));
+    
+    $this->widgetSchema->setNameFormat('criterias[%s]');
+    $this->disableCSRFProtection();
+  }
+}
