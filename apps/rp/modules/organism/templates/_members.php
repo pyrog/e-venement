154eda7efbe0d7f368d705114731b90ff32b2678
beta <betaglop@users.noreply.github.com>
2011-08-09 13:17:22 +0000

    (rp) simplifying organisms and contacts files (rp) linking orgs from contacts and reverse through the current action type

--- apps/rp/modules/organism/templates/_members.php
+++ apps/rp/modules/organism/templates/_members.php
@@ -24,9 +24,10 @@
 <div class="sf_admin_edit ui-widget ui-widget-content ui-corner-all members">
 <div class="ui-widget-header ui-corner-all fg-toolbar"><h2><?php echo __("Organism's members") ?></h2></div>
 <ul class="contacts">
+<?php $action = !$sf_user->hasCredential('pr-contact-edit') ? 'show' : sfContext::getInstance()->getActionName(); ?>
 <?php foreach ( $organism->Professionals as $professional ): ?>
 <li>
-  <a class="file" href="<?php echo url_for('contact/edit?id='.$professional->Contact['id']) ?>"><?php echo $professional->Contact ?></a>
+  <a class="file" href="<?php echo url_for('contact/'.$action.'?id='.$professional->Contact['id']) ?>"><?php echo $professional->Contact ?></a>
   <span class="professional"><?php echo $professional ?> (<?php echo $professional->ProfessionalType ?>)</span>
 </li>
 <?php endforeach ?>
