61647e85030a68560f4e2521ebe736f03e75a56e
beta <betaglop@users.noreply.github.com>
2011-08-10 11:29:27 +0000

    (rp) improving the details of ticketting informations on contact files

--- apps/rp/modules/contact/templates/_events_list.php
+++ apps/rp/modules/contact/templates/_events_list.php
@@ -2,19 +2,42 @@
 <ul class="sf_form_field_events_list">
 <?php foreach ($contact->Transactions as $transaction): ?>
 <?php if ( $transaction->Tickets->count() > 0 ): ?>
+  <?php
+    $manifs = array();
+    $prints = 0;
+    foreach ( $transaction->Tickets as $ticket )
+    {
+      if ( $ticket->printed )
+        $prints++;
+      $manifs[$ticket->manifestation_id] = cross_app_link_to($ticket->Manifestation->getShortName(),'event','manifestation/show?id='.$ticket->manifestation_id);
+    }
+  ?>
+  <?php if ( $prints > 0 || !is_null($transaction->Order->id) ): ?>
   <li>
-    #<?php echo cross_app_link_to($transaction,'tck','ticket/sell?id='.$transaction->id) ?>,
-    <?php echo format_date($transaction->created_at) ?>
-    <?php echo __('for %%i%% ticket(s) and',array('%%i%%' => $transaction->Tickets->count())) ?> 
-    <?php $sum = 0; foreach ( $transaction->Payments as $pay ) $sum += $pay->value; echo format_currency($sum,'€'); ?> 
-    <br/>
-    <?php
-      $manifs = array();
-      foreach ( $transaction->Tickets as $ticket )
-        $manifs[$ticket->manifestation_id] = cross_app_link_to($ticket->Manifestation->getShortName(),'event','manifestation/show?id='.$ticket->manifestation_id);
+    <p class="infos">
+      #<?php echo cross_app_link_to($transaction,'tck','ticket/sell?id='.$transaction->id) ?>,
+      <?php echo format_date($transaction->created_at) ?>
+      <?php echo __('for %%i%% ticket(s) and',array('%%i%%' => $transaction->Tickets->count())) ?> 
+      <?php $sum = 0; foreach ( $transaction->Payments as $pay ) $sum += $pay->value; echo format_currency($sum,'€'); ?> 
+      (<?php
+        if ( $prints < $transaction->Tickets->count() )
+        {
+          if ( !is_null($transaction->Order->id) )
+            echo __('Ordered');
+          else if ( $prints == 0 )
+            echo __('All demanded');
+          else
+            echo __('Partially printed');
+        }
+        else
+          echo __('All printed');
+      ?>)
+    </p>
+    <p class="manifs"><?php
       echo implode(', ',$manifs);
-    ?>
+    ?></p>
   </li>
+  <?php endif ?>
 <?php endif ?>
 <?php endforeach ?>
 </ul>
