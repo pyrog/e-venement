85c94980b1c01b0b2db7e9404acad33fb3bbba7d
beta <betaglop@users.noreply.github.com>
2011-08-11 10:22:52 +0000

    (rp) early modifications for filtering contact in a quick way on groups

--- apps/rp/modules/contact/templates/_list_header.php
+++ apps/rp/modules/contact/templates/_list_header.php
@@ -1,1 +1,13 @@
+<script type="text/javascript">
+  $(document).ready(function(){
+    $('#sf_admin_filter form').clone(true)
+      .attr('id','integrated-filter')
+      .appendTo('#sf_admin_header');
+    $('#sf_admin_header #integrated-filter tr.sf_admin_form_row:not(.sf_admin_filter_field_groups_list)').remove();
+    $('#sf_admin_header #integrated-filter .fieldset thead').remove();
+    $('#sf_admin_header #integrated-filter tfoot').remove();
+    $('#sf_admin_header #integrated-filter label').remove();
+    $('#sf_admin_header #integrated-filter .fieldset tbody:empty').parent().parent().parent().parent().remove();
+  });
+</script>
 <?php include_partial('global/list_header') ?>
