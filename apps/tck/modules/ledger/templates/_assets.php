6d69104ed3ed4ec55fba2c95705dd2b8c21cb1b2
beta <betaglop@users.noreply.github.com>
2011-08-12 13:00:33 +0000

    (tck) printing the ledgers without any background color which wastes ink

--- apps/tck/modules/ledger/templates/_assets.php
+++ apps/tck/modules/ledger/templates/_assets.php
@@ -1,6 +1,7 @@
   <?php use_javascript('/sfAdminThemejRollerPlugin/js/jquery.min.js', 'first') ?>
   <?php use_javascript('/sfAdminThemejRollerPlugin/js/jquery-ui.custom.min.js', 'first') ?>
   <?php use_stylesheet('/sfAdminThemejRollerPlugin/css/jquery/redmond/jquery-ui.custom.css') ?>
+  <?php use_stylesheet('print-ledger','',array('media' => 'print')) ?>
 
 <?php use_helper('Number','Date') ?>
 <?php use_helper('CrossAppLink') ?>
