3f2733cf5f9098756af940a6f44a6eedbddca345
beta <betaglop@users.noreply.github.com>
2011-08-17 19:30:03 +0000

    (tck) filtering orders to avoid closed transactions

--- lib/model/doctrine/tck/OrderTable.class.php
+++ lib/model/doctrine/tck/OrderTable.class.php
@@ -16,4 +16,22 @@ class OrderTable extends PluginOrderTable
     {
         return Doctrine_Core::getTable('Order');
     }
+    
+    public function doSelectIndex(Doctrine_Query $q)
+    {
+      $o = $q->getRootAlias();
+      $q->leftJoin("$o.Transaction t")
+        ->leftJoin('t.Tickets tck')
+        ->leftJoin('tck.Manifestation m')
+        ->leftJoin('m.Location l')
+        ->leftJoin('m.Event e')
+        ->leftJoin('m.Workspaces w')
+        ->leftJoin('e.MetaEvent me')
+        ->leftJoin('e.EventCategory ec')
+        ->leftJoin('t.Contact c')
+        ->leftJoin('t.Professional p')
+        ->leftJoin('p.Organism org')
+        ->andWhere('NOT t.closed');
+      return $q;
+    }
 }
