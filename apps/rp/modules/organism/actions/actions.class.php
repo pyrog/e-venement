7bb1708e6fb3c540bbbcaa7cadf3c4d89f796cd4
beta <betaglop@users.noreply.github.com>
2011-09-23 15:40:58 +0000

    (rp) removing the specific display of organisms in ajax requests

--- apps/rp/modules/organism/actions/actions.class.php
+++ apps/rp/modules/organism/actions/actions.class.php
@@ -121,7 +121,7 @@ class organismActions extends autoOrganismActions
 
     $organisms = array();
     foreach ( $request as $organism )
-      $organisms[$organism->id] = ((string) $organism).' ('.$organism->city.')';
+      $organisms[$organism->id] = (string) $organism;
     
     return $this->renderText(json_encode($organisms));
   }
