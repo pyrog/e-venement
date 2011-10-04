7117cb77c9d96b44b37c8ee2daa791053168a058
beta <betaglop@users.noreply.github.com>
2011-10-04 11:57:17 +0000

    (rp) correcting the link to organisms from contact's list

--- apps/rp/modules/contact/templates/_organisms_list.php
+++ apps/rp/modules/contact/templates/_organisms_list.php
@@ -2,6 +2,6 @@
   $action = !$sf_user->hasCredential('pr-organism-edit') ? 'show' : sfContext::getInstance()->getActionName();
   $orgs = array();
   foreach ( $contact->Professionals as $pro )
-    $orgs[] = link_to($pro->Organism,'organism/'.$action.'?id='.$pro->Organism->id);
+    $orgs[] = link_to($pro->Organism,'organism/show?id='.$pro->Organism->id);
   echo implode('<br/>',$orgs);
 ?>
