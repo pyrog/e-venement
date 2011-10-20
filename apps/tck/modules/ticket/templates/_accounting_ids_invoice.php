b2c66337b707cc0e904a8b7f5caf7a97c5166f8f
beta <betaglop@users.noreply.github.com>
2011-10-20 10:44:20 +0000

    (tck) adding a prefix on invoices generated (tck) correcting a false JS path

--- apps/tck/modules/ticket/templates/_accounting_ids_invoice.php
+++ apps/tck/modules/ticket/templates/_accounting_ids_invoice.php
@@ -1,1 +1,3 @@
-<p id="ids" class="invoice"><?php echo __('<span class="name">Invoice</span> #<span class="invoice_id">%%iid%%</span>, for transaction #<span class="transaction_id">%%tid%%</span>',array('%%iid%%' => $invoice->id, '%%tid%%' => $transaction->id)) ?></span></p>
+<p id="ids" class="invoice">
+  <?php echo __('<span class="name">Invoice</span> #%%prefix%%<span class="invoice_id">%%iid%%</span>, for transaction #<span class="transaction_id">%%tid%%</span>',array('%%iid%%' => $invoice->id, '%%tid%%' => $transaction->id, '%%prefix%%' => sfConfig::get('app_seller_invoice_prefix'))) ?>
+</p>
