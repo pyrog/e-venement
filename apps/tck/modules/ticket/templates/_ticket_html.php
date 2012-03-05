7cd603466540db1347e766fde1218c94c0757f08
beta <betaglop@users.noreply.github.com>
2012-03-05 15:02:13 +0000

    (tck) correcting tickets printing on max size on event's name

--- apps/tck/modules/ticket/templates/_ticket_html.php
+++ apps/tck/modules/ticket/templates/_ticket_html.php
@@ -1,9 +1,9 @@
 <?php use_helper('Date','Number') ?>
-<?php $maxsize = sfConfig::get('app_tickets_max_size') ?>
 <?php
-  $maxsize['event_name'] = !isset($maxsize['event_name']) || intval($maxsize['event_name']) > 0 ? intval($maxsize['event_name']) : 30;
-  $maxsize['place'] = !isset($maxsize['place']) || intval($maxsize['place']) > 0 ? intval($maxsize['place']) : 30;
-  $maxsize['event_nameshort'] = !isset($maxsize['event_shortname']) || intval($maxsize['event_shortname']) > 0 ? intval($maxsize['event_shortname']) : 30;
+	$maxsize = sfConfig::get('app_tickets_max_size');
+  $maxsize['event_name'] = isset($maxsize['event_name']) && intval($maxsize['event_name']) != 0 ? intval($maxsize['event_name']) : 30;
+  $maxsize['event_shortname'] = isset($maxsize['event_shortname']) && intval($maxsize['event_shortname']) != 0 ? intval($maxsize['event_shortname']) : 40;
+  $maxsize['place'] = isset($maxsize['place']) && intval($maxsize['place']) != 0 ? intval($maxsize['place']) : 30;
 ?>
 <div class="ticket">
   <div class="logo"><img src="" alt="" /></div>
@@ -20,8 +20,8 @@
       <span class="price"><?php echo format_currency($ticket->value,'€') ?></span>
     </p>
     <p class="price_name"><span class="description"><?php echo $ticket->Price->description ?></span><span class="name"><?php echo $ticket->price_name ?></span> <span class="price"><?php echo format_currency($ticket->value,'€') ?></span></p>
-    <p class="event"><?php echo strlen($buf = $ticket->Manifestation->Event) > $maxsize['event_name'] ? substr(nl2br($buf),0,$maxsize['event_name']).'...' : nl2br($buf) ?></p>
-    <p class="event-short"><?php echo strlen($buf = $ticket->Manifestation->Event->short_name) > $maxsize['event_shortname'] ? substr($buf,0,$maxsize['event_name']).'...' : $buf ?></p>
+    <p class="event"><?php echo strlen($buf = (string)$ticket->Manifestation->Event) > $maxsize['event_name'] ? substr(nl2br($buf),0,$maxsize['event_name']).'...' : nl2br($buf) ?></p>
+    <p class="event-short"><?php echo strlen($buf = $ticket->Manifestation->Event->short_name) > $maxsize['event_shortname'] ? substr($buf,0,$maxsize['event_shortname']).'...' : $buf ?></p>
     <p class="cie"><?php $creators = array(); $cpt = 0; foreach ( $ticket->Manifestation->Event->Companies as $company ) { if ( $cpt++ > 1 ) break; $creators[] .= $company; } echo implode(', ',$creators); ?></p>
     <p class="org"><?php $orgas = array(sfConfig::get('app_seller_name')); $cpt = 0; foreach ( $ticket->Manifestation->Organizers as $orga ) { if ( $cpt++ > 2 ) break; $orgas[] = $orga; } echo implode(', ',$orgas); ?></p>
     <p class="seat"><?php echo $ticket->numerotation ? __('Seat n°%%s%%',array('%%s%%' => $ticket->numerotation)) : '' ?></p>
