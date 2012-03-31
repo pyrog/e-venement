8773aaca6ecf45ac5179909813cd0793a6774f59
beta <betaglop@users.noreply.github.com>
2012-03-31 21:35:32 +0000

    (tck) now timeout after scanning tickets can be configured in the app.yml

--- apps/tck/modules/ticket/templates/failedSuccess.php
+++ apps/tck/modules/ticket/templates/failedSuccess.php
@@ -3,11 +3,13 @@
     <h1><?php echo __('Checkpoint failed !') ?></h1>
   </div>
   <p class="link ui-corner-all"><?php echo link_to(__('Try again...'),'ticket/control') ?></p>
+  <?php $delays = sfConfig::get('app_control_delays') ?>
+  <?php print_r($delays) ?>
   <script type="text/javascript">
     $(document).ready(function(){
       setTimeout(function(){
         document.location = $('#checkpoint .link a').attr('href');
-      },2500);
+      },<?php echo isset($delays['failure']) ? intval($delays['failure']) : '2.5' ?>*1000);
     });
   </script>
 </div>
--- apps/tck/modules/ticket/templates/passedSuccess.php
+++ apps/tck/modules/ticket/templates/passedSuccess.php
@@ -6,12 +6,13 @@
     <p><?php echo link_to(__('Get back for a new ticket...'),'ticket/control') ?></p>
     <?php if ( $comment ): ?><p class="ui-widget-content ui-corner-all comment"><?php echo $comment ?></p><?php endif ?>
   </div>
+  <?php $delays = sfConfig::get('app_control_delays') ?>
   <script type="text/javascript">
     $(document).ready(function(){
       if ( !$('.comment').html() )
       setTimeout(function(){
         document.location = $('#checkpoint .link a').attr('href');
-      },1500);
+      },<?php echo isset($delays['failure']) ? intval($delays['success']) : '1.5' ?>*1000);
     });
   </script>
 </div>
