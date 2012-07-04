f3b2bb3bcf61334d327a19abfb4d186a96f38ce9
beta <betaglop@users.noreply.github.com>
2012-07-04 11:07:12 +0000

    (event) improving the text gauge

--- apps/event/modules/manifestation/templates/_show_gauge_txt.php
+++ apps/event/modules/manifestation/templates/_show_gauge_txt.php
@@ -1,4 +1,5 @@
 <div class="sf_admin_form_row">
   <label><?php echo __('Gauge') ?>:</label>
+  <?php if ( !isset($manifestation) ) $manifestation = $form->getObject(); ?>
   <?php include_partial('list_gauge',array('manifestation' => $manifestation)) ?>
 </div>
