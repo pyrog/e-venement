b105d856e144df06febcb63fdd2ba48b1f6ce09a
beta <betaglop@users.noreply.github.com>
2012-07-06 11:21:53 +0000

    (grp) adding the professional module

---
+++ apps/grp/modules/professional/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/professionalGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/professionalGeneratorHelper.class.php';
+
+/**
+ * professional actions.
+ *
+ * @package    e-venement
+ * @subpackage professional
+ * @author     Baptiste SIMON <baptiste.simon AT e-glop.net>
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class professionalActions extends autoProfessionalActions
+{
+}
---
+++ apps/grp/modules/professional/templates/showSuccess.php
@@ -0,0 +1,49 @@
+<?php use_helper('I18N', 'Date', 'CrossAppLink') ?>
+<?php include_partial('assets') ?>
+<?php use_stylesheet('grp-event') ?>
+
+<div id="sf_admin_container" class="li_grp_professional sf_admin_show ui-widget ui-widget-content ui-corner-all">
+  <div class="fg-toolbar ui-widget-header ui-corner-all">
+    <h1><?php echo __('Contact file', array(), 'messages') ?></h1>
+  </div>
+
+  <div class="ui-helper-clearfix"></div>
+
+  <p class="contact">
+    <?php echo cross_app_link_to($professional->Contact,'rp','contact/show?id='.$professional->Contact->id) ?>
+    (<?php echo cross_app_link_to($professional->Organism,'rp','organism/show?id='.$professional->Organism->id) ?>
+    -
+    <?php echo $professional->name ?>)
+  </p>
+  
+  <div class="ui-widget ui-widget-content ui-corner-all">
+    <div class="fg-toolbar ui-widget-header ui-corner-all">
+      <h2><?php echo __('Events', array(), 'messages') ?></h2>
+    </div>
+    <?php $last_event_id = 0 ?>
+    <ul class="entries">
+      <?php foreach ( $professional->ContactEntries as $ce ): ?>
+      <?php foreach ( $ce->Entries as $entry ): ?>
+      <?php if ( $last_event_id != $entry->ManifestationEntry->Manifestation->Event->id ): ?>
+      <?php if ( $last_event_id != 0 ): ?></ul></li><?php endif ?>
+      <li class="event event-<?php echo $entry->ManifestationEntry->Manifestation->Event->id ?>">
+        <span class="entry_id"><?php echo link_to('#'.$entry->ManifestationEntry->Manifestation->Event->id,'event/edit?id='.$entry->ManifestationEntry->Manifestation->Event->id) ?>:</span>
+        <span class="event"><?php echo cross_app_link_to($entry->ManifestationEntry->Manifestation->Event,'event','event/edit?id='.$entry->ManifestationEntry->Manifestation->Event->id) ?></span>
+        <ul>
+      <?php endif ?>
+          <li>
+            <span class="manifestation_happens_at"><?php echo cross_app_link_to($entry->ManifestationEntry->Manifestation->getFormattedDate(),'event','manifestation/show?id='.$entry->ManifestationEntry->Manifestation->id) ?></span>
+            <?php foreach ( $entry->EntryTickets as $et ): ?>
+            <span class="tickets"><?php echo $et->quantity.' '.$et->Price ?></span>
+            <?php endforeach ?>
+            <?php echo $ce->transaction_id ? '<a class="transpose" href="'.cross_app_url_for('tck','ticket/sell?id='.$ce->transaction_id).'">&gt;&gt;</span>' : '' ?>
+          </li>
+      <?php $last_event_id = $entry->ManifestationEntry->Manifestation->Event->id ?>
+      <?php endforeach ?>
+      <?php endforeach ?>
+      </li>
+    </ul>
+  </div>
+
+  <?php include_partial('themeswitcher') ?>
+</div>
