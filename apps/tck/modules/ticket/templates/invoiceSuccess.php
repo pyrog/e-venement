18e5ebbbcf487cbcffed85998bf3fa1a88b10448
beta <betaglop@users.noreply.github.com>
2012-07-29 11:57:44 +0000

    (tck) improving invoice generation

--- apps/tck/modules/ticket/templates/invoiceSuccess.php
+++ apps/tck/modules/ticket/templates/invoiceSuccess.php
@@ -1,7 +1,7 @@
 <?php include_partial('accounting_assets',array('modifiable' => true)) ?>
 <?php include_partial('accounting_date') ?>
 <?php include_partial('accounting_type_invoice') ?>
-<?php include_partial('accounting_seller') ?>
+<?php include_partial('accounting_seller',array('transaction' => $transaction)) ?>
 <?php include_partial('accounting_customer',array('transaction' => $transaction)) ?>
 <?php include_partial('accounting_ids_invoice',array('transaction' => $transaction,'invoice' => $invoice)) ?>
 <?php include_partial('accounting_lines',array('transaction' => $transaction,'tickets' => $tickets,'nocancel' => $nocancel,)) ?>
