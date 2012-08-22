9bb0e553784e6c5ab9c97c4a5c64f9993402e951
beta <betaglop@users.noreply.github.com>
2012-08-22 13:39:58 +0000

    (default) ordering stuff in user profiles

--- lib/form/doctrine/sfDoctrineGuardPlugin/liGuardUserAdminForm.class.php
+++ lib/form/doctrine/sfDoctrineGuardPlugin/liGuardUserAdminForm.class.php
@@ -13,7 +13,15 @@ class liGuardUserAdminForm extends sfGuardUserAdminForm
   public function configure()
   {
     parent::configure();
-    $this->widgetSchema   ['workspaces_list']->setOption('query',Doctrine::getTable('Workspace')->createQuery('ws',true));
-    $this->validatorSchema['workspaces_list']->setOption('query',Doctrine::getTable('Workspace')->createQuery('ws',true));
+    
+    $this->widgetSchema['groups_list']->setOption('order_by',array('name',''));
+    $this->widgetSchema['groups_list']->setOption('expanded',true);
+    
+    $this->widgetSchema['meta_events_list']->setOption('order_by',array('name',''));
+    $this->widgetSchema   ['workspaces_list']->setOption('query',$q = Doctrine::getTable('Workspace')->createQuery('ws',true)->orderBy('name'));
+    $this->validatorSchema['workspaces_list']->setOption('query',$q);
+
+    $this->widgetSchema['prices_list']->setOption('order_by',array('name',''));
+    $this->widgetSchema['prices_list']->setOption('expanded',true);
   }
 }
