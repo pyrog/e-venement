41ef10f745cf68003fe573315ade2010b76d0af0
beta <betaglop@users.noreply.github.com>
2012-09-10 09:08:32 +0000

    (rp) making the organism's URL clickable

---
+++ apps/rp/modules/organism/templates/_show_url.php
@@ -0,0 +1,6 @@
+<?php $url = $form->getObject()->get('url') ?>
+<div class="sf_admin_form_row">
+  <label><?php echo __('URL') ?>:</label>
+  <a href="mailto:<?php echo $url ?>"><?php echo $url ?></a>
+</div>
+    
