ea9188469ea7c92fd0dec33af1ae19840c1ec262
beta <betaglop@users.noreply.github.com>
2012-09-10 09:47:23 +0000

    (global) expanding choices lists in users' setting (the ticketting part)

--- lib/form/doctrine/sfDoctrineGuardPlugin/liGuardUserAdminForm.class.php
+++ lib/form/doctrine/sfDoctrineGuardPlugin/liGuardUserAdminForm.class.php
@@ -18,7 +18,10 @@ class liGuardUserAdminForm extends sfGuardUserAdminForm
     $this->widgetSchema['groups_list']->setOption('expanded',true);
     
     $this->widgetSchema['meta_events_list']->setOption('order_by',array('name',''));
+    $this->widgetSchema['meta_events_list']->setOption('expanded',true);
+    
     $this->widgetSchema   ['workspaces_list']->setOption('query',$q = Doctrine::getTable('Workspace')->createQuery('ws',true)->orderBy('name'));
+    $this->widgetSchema   ['workspaces_list']->setOption('expanded',true);
     $this->validatorSchema['workspaces_list']->setOption('query',$q);
 
     $this->widgetSchema['prices_list']->setOption('order_by',array('name',''));
