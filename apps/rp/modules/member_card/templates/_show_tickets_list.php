53d67c99d19d5fc46035804f77dddc50eae27fc4
beta <betaglop@users.noreply.github.com>
2012-09-24 14:36:47 +0000

    (rp) removing editting member cards through the list (rp) removing duplicates from member cards' list

--- apps/rp/modules/member_card/templates/_show_tickets_list.php
+++ apps/rp/modules/member_card/templates/_show_tickets_list.php
@@ -5,12 +5,14 @@
   <table class="tickets_list ui-widget ui-corner-all ui-widget-content">
   <tbody>
   <?php foreach ( $member_card->Tickets as $ticket ): ?>
+  <?php if ( is_null($ticket->duplicate) ): ?>
     <tr>
       <td><?php echo cross_app_link_to('#'.$ticket->transaction_id,'tck','ticket/sell?id='.$ticket->transaction_id) ?></td>
       <td><?php echo $ticket->price_name ?></td>
       <td><?php echo format_currency($ticket->value,'€') ?></td>
       <td><?php echo cross_app_link_to($ticket->Manifestation,'event','manifestation/show?id='.$ticket->manifestation_id) ?></td>
     </tr>
+  <?php endif ?>
   <?php endforeach ?>
   </tbody>
   </table>
