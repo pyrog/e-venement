85967b08a5f10ce880da3528b51a31129f208058
beta <betaglop@users.noreply.github.com>
2012-09-27 12:48:01 +0000

    (rp) adding the ticket id on member card content

--- apps/rp/modules/member_card/templates/_show_tickets_list.php
+++ apps/rp/modules/member_card/templates/_show_tickets_list.php
@@ -7,10 +7,11 @@
   <?php foreach ( $member_card->Tickets as $ticket ): ?>
   <?php if ( is_null($ticket->duplicate) ): ?>
     <tr>
-      <td><?php echo cross_app_link_to('#'.$ticket->transaction_id,'tck','ticket/sell?id='.$ticket->transaction_id) ?></td>
-      <td><?php echo $ticket->price_name ?></td>
-      <td><?php echo format_currency($ticket->value,'€') ?></td>
-      <td><?php echo cross_app_link_to($ticket->Manifestation,'event','manifestation/show?id='.$ticket->manifestation_id) ?></td>
+      <td class="transation_id"><?php echo cross_app_link_to('#'.$ticket->transaction_id,'tck','ticket/sell?id='.$ticket->transaction_id) ?></td>
+      <td class="ticket_id"><?php echo '#'.$ticket->id ?></td>
+      <td class="price_name"><?php echo $ticket->price_name ?></td>
+      <td class="ticket_value"><?php echo format_currency($ticket->value,'€') ?></td>
+      <td class="ticket_manifestation"><?php echo cross_app_link_to($ticket->Manifestation,'event','manifestation/show?id='.$ticket->manifestation_id) ?></td>
     </tr>
   <?php endif ?>
   <?php endforeach ?>
