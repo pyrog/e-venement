564b1f5727c3a5529f5c710dc4001603e160f0f9
beta <betaglop@users.noreply.github.com>
2012-10-03 15:57:41 +0000

    (rp) correcting a HTML bug

--- apps/rp/modules/member_card/templates/_show_actions.php
+++ apps/rp/modules/member_card/templates/_show_actions.php
@@ -1,6 +1,6 @@
-<ul class="sf_admin_actions_form" style="margin-left: 10px; float: none;">
+<div class="sf_admin_actions_form" style="margin-left: 10px; float: none;">
   <a class="fg-button ui-state-default fg-button-icon-left" href="<?php echo url_for('contact/card?id='.$form->getObject()->contact_id) ?>">
     <span class="ui-icon ui-icon-arrowreturnthick-1-w"></span>
     <?php echo __("Back to list",array(),'sf_admin') ?>
   </a>
-</ul>
+</div>
