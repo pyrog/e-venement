caea9fefa103d13d785f35ed5ea920a6504a5945
beta <betaglop@users.noreply.github.com>
2012-10-05 08:04:23 +0000

    (grp) adding a filter for professionals on contact's name and organism's name

--- lib/filter/doctrine/ProfessionalFormFilter.class.php
+++ lib/filter/doctrine/ProfessionalFormFilter.class.php
@@ -13,5 +13,54 @@ class ProfessionalFormFilter extends BaseProfessionalFormFilter
   public function configure()
   {
     $this->widgetSchema['professional_type_id']->setOption('order_by',array('name',''));
+    
+    $this->widgetSchema['contact_name'] = new sfWidgetFormInput(array(
+    ));
+    $this->validatorSchema['contact_name'] = new sfValidatorString(array(
+      'required' => false,
+    ));
+    
+    $this->widgetSchema['organism_name'] = new sfWidgetFormInput(array(
+    ));
+    $this->validatorSchema['organism_name'] = new sfValidatorString(array(
+      'required' => false,
+    ));
+  }
+  
+  public function getFields()
+  {
+    return array_merge(
+      array(
+        'contact_name' => 'Contact name',
+        'organism_name' => 'Organism name',
+      ),
+      parent::getFields()
+    );
+  }
+  
+  public function addContactNameColumnQuery(Doctrine_Query $query, $field, $value)
+  {
+    $fieldName = $this->getFieldName($field);
+    
+    if ( $value )
+    {
+      $a = $query->getRootAlias();
+      $query->addWhere('LOWER(c.name) LIKE ?',strtolower($value).'%');
+    }
+    
+    return $query;
+  }
+  
+  public function addOrganismNameColumnQuery(Doctrine_Query $query, $field, $value)
+  {
+    $fieldName = $this->getFieldName($field);
+    
+    if ( $value )
+    {
+      $a = $query->getRootAlias();
+      $query->addWhere('LOWER(o.name) LIKE ?',strtolower($value).'%');
+    }
+    
+    return $query;
   }
 }
