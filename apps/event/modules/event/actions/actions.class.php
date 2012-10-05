87bd455980e8f1bb2690815a067089079e471735
beta <betaglop@users.noreply.github.com>
2012-10-05 09:17:42 +0000

    (event) correcting a mistake in event ordering... the config param was reported with an error

--- apps/event/modules/event/actions/actions.class.php
+++ apps/event/modules/event/actions/actions.class.php
@@ -24,7 +24,7 @@ class eventActions extends autoEventActions
         ->select('*')
         ->addSelect("(SELECT min(m2.happens_at) FROM manifestation m2 WHERE m2.event_id = $a.id) AS min_happens_at")
         ->addSelect("(SELECT (CASE WHEN max(m3.happens_at) IS NULL THEN false ELSE max(m3.happens_at) > now() END) FROM manifestation m3 WHERE m3.event_id = $a.id) AS now")
-        ->orderby("(SELECT min(m4.happens_at) FROM manifestation m4 WHERE m4.event_id = $a.id) ".(sfConfig::get('app_listing_event_manif') != 'ASC' ? 'DESC' : 'ASC').", $a.name");
+        ->orderby("(SELECT min(m4.happens_at) FROM manifestation m4 WHERE m4.event_id = $a.id) ".(sfConfig::get('app_listing_manif_date') != 'ASC' ? 'DESC' : 'ASC').", $a.name");
     }
   }
   
