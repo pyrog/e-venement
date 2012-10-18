1c5fd5de49abccfe7ecceb7af01891a4f2826c56
beta <betaglop@users.noreply.github.com>
2012-10-18 15:25:54 +0000

    (mc) performing the member cards' list

--- apps/rp/modules/member_card/templates/_show_value.php
+++ apps/rp/modules/member_card/templates/_show_value.php
@@ -1,5 +1,5 @@
 <?php use_helper('Number') ?>
 <div class="sf_admin_form_row">
   <label><?php echo __('Value') ?>:</label>
-  <?php echo format_currency($member_card->getValue(),'€') ?>
+  <?php include_partial('value',array('member_card' => $member_card)) ?>
 </div>
