cca477664258513bb7ed7d82322bdc595977866b
beta <betaglop@users.noreply.github.com>
2012-10-24 09:46:53 +0000

    (event) forgotten files for workspace ordering

---
+++ lib/form/doctrine/GroupOrganismForm.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * GroupOrganism form.
+ *
+ * @package    symfony
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class GroupOrganismForm extends BaseGroupOrganismForm
+{
+  /**
+   * @see GroupDetailForm
+   */
+  public function configure()
+  {
+    parent::configure();
+  }
+}
