e05e6af8eccbdb74a5cae287edc096cfb18e8d6c
beta <betaglop@users.noreply.github.com>
2012-10-24 09:38:04 +0000

    (event) adding a tool to order workspaces in the ticketting process

---
+++ apps/event/modules/workspace_user_ordering/lib/workspace_user_orderingGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * workspace_user_ordering module helper.
+ *
+ * @package    symfony
+ * @subpackage workspace_user_ordering
+ * @author     Your name here
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class workspace_user_orderingGeneratorHelper extends BaseWorkspace_user_orderingGeneratorHelper
+{
+}
