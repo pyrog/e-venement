fdf181836925a83ab1c85e00b95a1731738ffc58
beta <betaglop@users.noreply.github.com>
2012-11-16 18:05:11 +0000

    (tck) correcting stuff about updated_at (tickets where updated when a transaction was closed)

--- lib/model/doctrine/tck/Traceable.class.php
+++ lib/model/doctrine/tck/Traceable.class.php
@@ -15,9 +15,10 @@ class Traceable extends PluginTraceable
   public function preSave($event)
   {
     if ( sfContext::hasInstance() && sfContext::getInstance()->getUser()->getId() && $this->isModified() )
+    {
+      $this->updated_at = date('Y-m-d H:i:s');
       $this->sf_guard_user_id = sfContext::getInstance()->getUser()->getId();
-    $this->updated_at = date('Y-m-d H:i:s');
-    $this->sf_guard_user_id = sfContext::getInstance()->getUser()->getId();
+    }
     parent::preSave($event);
   }
   
