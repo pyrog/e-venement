0a685638fa8e629e664a221570224da9b08bc115
beta <betaglop@users.noreply.github.com>
2012-11-16 13:16:54 +0000

    (event) adding a missing template

---
+++ apps/event/modules/gauge/templates/_batch_edit_td_rowspan.php
@@ -0,0 +1,5 @@
+<td class="rowspan gauge-transferts form-list-option" rowspan="<?php echo $pager->getResults()->count() ?>">
+  <p>
+    <span class="ui-icon ui-icon-arrow-4-diag" title="<?php echo __('Click to chain gauges from top to bottom, activating transferts') ?>"><?php echo __('Click to unchain gauges, deactivating transferts') ?></span>
+  </p>
+</td>
