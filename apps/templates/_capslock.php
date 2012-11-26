e8911bfbde251f104a4878c538a642a55439993a
beta <betaglop@users.noreply.github.com>
2012-11-26 17:56:21 +0000

    a forgottent partial

---
+++ apps/templates/_capslock.php
@@ -0,0 +1,5 @@
+<?php use_stylesheet('jquery.capsalerttipsy.css') ?>
+<div class="tipsy tipsy-w">
+  <div class="tipsy-arrow tipsy-arrow-w" style="position: static; margin-bottom: -15px; margin-left: -5px;"></div>
+  <div class="tipsy-inner"><?php echo __('WARNING: CAPS Lock has to be on.',null,'sf_admin') ?></div>
+</div>
