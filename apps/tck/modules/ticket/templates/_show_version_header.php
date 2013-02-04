752653ad9d4023214d2143bc7d7dacbc0b31d80f
beta <betaglop@users.noreply.github.com>
2013-02-04 16:58:00 +0000

    (tck) i18n

--- apps/tck/modules/ticket/templates/_show_version_header.php
+++ apps/tck/modules/ticket/templates/_show_version_header.php
@@ -1,4 +1,4 @@
 <td class="version"><?php echo __('Version') ?></td>
-<td class="user"><?php echo __('Updated at',null,'sf_admin') ?></td>
+<td class="user"><?php echo __('Updated at') ?></td>
 <td class="price_name"><?php echo __('Price name') ?></td>
 <td class="printed"><?php echo __('Printed or integrated') ?></td>
