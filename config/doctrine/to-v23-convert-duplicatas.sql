70982515afbc99c2419f3fe46baa92cc58d4a06c
beta <betaglop@users.noreply.github.com>
2013-02-19 16:02:28 +0000

    (tck) correcting the SQL migration script

--- config/doctrine/to-v23-convert-duplicatas.sql
+++ config/doctrine/to-v23-convert-duplicatas.sql
@@ -6,6 +6,6 @@ WHERE id IN (SELECT duplicate FROM ticket)
 UPDATE ticket t
 SET cancelling = (SELECT duplicating FROM ticket t2 WHERE t2.id = t.cancelling)
 WHERE cancelling IS NOT NULL
-  AND cancelling IN (SELECT id FROM ticket WHERE duplicating IS NOT NULL);
-WHERE id IN (SELECT duplicate FROM ticket)
+  AND cancelling IN (SELECT id FROM ticket WHERE duplicating IS NOT NULL)
+  AND id IN (SELECT duplicate FROM ticket)
   AND duplicating IS NULL;
