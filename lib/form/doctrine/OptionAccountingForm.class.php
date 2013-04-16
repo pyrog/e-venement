bd13d88b09d923690eb3276c7e6590cb3a46a7ad
beta <betaglop@users.noreply.github.com>
2013-04-16 00:45:17 +0000

    (tck) translating and improving the OptionAccounting form

--- lib/form/doctrine/OptionAccountingForm.class.php
+++ lib/form/doctrine/OptionAccountingForm.class.php
@@ -21,22 +21,22 @@ class OptionAccountingForm extends BaseOptionAccountingForm
     parent::configure();
     
     $this->widgets = array('' => array(
-      'file'                  => array('label' => 'File number',              'type' => 'integer', 'options' => array('min' => 0, 'max' => 9999)),
-      'ja_code'               => array('label' => 'JA code',                  'type' => 'integer', 'options' => array('min' => 0, 'max' => 99)),
-      'acc_section_analytic'  => array('label' => 'Analytic section',         'type' => 'integer', 'options' => array('min' => 0, 'max' => 999)),
-      'acc_section_other'     => array('label' => 'Other section',            'type' => 'integer', 'options' => array('min' => 0, 'max' => 999)),
+      'file'                  => array('label' => 'File number',              'type' => 'string', 'options' => array('min_length' => 4, 'max_length' => 3)),
+      'ja_code'               => array('label' => 'JA code',                  'type' => 'string', 'options' => array('min_length' => 2, 'max_length' => 2)),
+      'acc_section_analytic'  => array('label' => 'Analytic section',         'type' => 'string', 'options' => array('min_length' => 1, 'max_length' => 3)),
+      'acc_section_other'     => array('label' => 'Other section',            'type' => 'string', 'options' => array('min_length' => 1, 'max_length' => 3)),
       'code_doc_invoice'      => array('label' => 'Invoice document code',    'type' => 'string', 'default' => 'F', 'options' => array('min_length' => 1, 'max_length' => 1)),
       'code_doc_credit_side'  => array('label' => 'Credit side code',         'type' => 'string', 'default' => 'A', 'options' => array('max_length' => 1)), // un "avoir" comptable
       'code_debit'            => array('label' => 'Debit code',               'type' => 'string', 'default' => 'D', 'options' => array('min_length' => 1, 'max_length' => 1)),
       'code_credit'           => array('label' => 'Credit code',              'type' => 'string', 'default' => 'C', 'options' => array('min_length' => 1, 'max_length' => 1)),
-      'analytic_account'      => array('label' => 'Analytic account',         'type' => 'integer', 'options' => array('min' => 0, 'max' => 999999999999)),
+      'analytic_account'      => array('label' => 'Analytic account number',  'type' => 'string', 'options' => array('min_length' => 1, 'max_length' => 12)),
       'rec_code_transaction'  => array('label' => 'Transaction record code',  'type' => 'string', 'default' => 'T', 'options' => array('min_length' => 1, 'max_length' => 1)),
       'rec_code_imputation'   => array('label' => 'Imputation record code',   'type' => 'string', 'default' => 'I', 'options' => array('min_length' => 1, 'max_length' => 1)),
       'rec_code_expiration'   => array('label' => 'Expiration record code',   'type' => 'string', 'default' => 'E', 'options' => array('min_length' => 1, 'max_length' => 1)),
       'rec_code_analytic'     => array('label' => 'Analytic record code',     'type' => 'string', 'default' => 'Y', 'options' => array('min_length' => 1, 'max_length' => 1)),
       'incomes_acc_generic'   => array('label' => 'Generic account for incomes', 'type' => 'string', 'options' => array('min_length' => 1, 'max_length' => 10)),
-      'incomes_acc_vat'       => array('label' => 'Account for VAT',          'type' => 'string', 'options' => array('min_length' => 1, 'max_length' => 10)),
-      'currency'              => array('label' => 'Currency',                 'type' => 'string', 'default' => 'EUR', 'options' => array('max_length' => 3, 'min_length' => 1)),
+      'incomes_acc_vat'       => array('label' => 'Account for taxes',        'type' => 'string', 'options' => array('min_length' => 1, 'max_length' => 10)),
+      'currency'              => array('label' => 'Currency',                 'type' => 'string', 'default' => 'EUR', 'options' => array('max_length' => 3, 'min_length' => 3)),
     ));
     
     foreach ( $this->widgets as $fsname => $fieldset )
