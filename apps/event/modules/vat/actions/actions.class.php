77d8d79fc5313823f44b685a40eed70137e72d65
beta <betaglop@users.noreply.github.com>
2013-04-16 12:06:29 +0000

    (tck) adding the admin module for taxes

---
+++ apps/event/modules/vat/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/vatGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/vatGeneratorHelper.class.php';
+
+/**
+ * vat actions.
+ *
+ * @package    e-venement
+ * @subpackage vat
+ * @author     Baptiste SIMON <baptiste.simon AT e-glop.net>
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class vatActions extends autoVatActions
+{
+}
---
+++ apps/event/modules/vat/lib/vatGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * vat module helper.
+ *
+ * @package    e-venement
+ * @subpackage vat
+ * @author     Baptiste SIMON <baptiste.simon AT e-glop.net>
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class vatGeneratorHelper extends BaseVatGeneratorHelper
+{
+}
