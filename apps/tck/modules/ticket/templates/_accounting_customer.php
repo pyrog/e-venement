5d6bfcc8aafebabc7871ffb2ad5f9470cc0e3366
beta <betaglop@users.noreply.github.com>
2013-04-22 09:55:51 +0000

    (tck) adding new lines in the customer's address on invoices/orders

--- apps/tck/modules/ticket/templates/_accounting_customer.php
+++ apps/tck/modules/ticket/templates/_accounting_customer.php
@@ -1,7 +1,7 @@
 <div id="customer">
   <p class="name inline-modifiable"><?php echo $transaction->Contact ?></p>
   <p class="orgname inline-modifiable"><?php echo $transaction->Professional->Organism->name ?></p>
-  <p class="address inline-modifiable"><?php echo ucwords(strtolower($transaction->professional_id ? $transaction->Professional->Organism->address : $transaction->Contact->address)) ?></p>
+  <p class="address inline-modifiable"><?php echo nl2br(ucwords(strtolower($transaction->professional_id ? $transaction->Professional->Organism->address : $transaction->Contact->address))) ?></p>
   <p class="postalcode inline-modifiable"><?php echo $transaction->professional_id ? $transaction->Professional->Organism->postalcode : $transaction->Contact->postalcode ?></p>
   <p class="city inline-modifiable"><?php echo strtoupper($transaction->professional_id ? $transaction->Professional->Organism->city : $transaction->Contact->city) ?></p>
   <p class="country inline-modifiable"><?php echo strtoupper($transaction->professional_id ? $transaction->Professional->Organism->country : $transaction->Contact->country) ?></p>
