3b8599a8e69981b747410c70a9758e93b866982e
beta <betaglop@users.noreply.github.com>
2013-05-07 16:36:02 +0000

    (tck) adding a filter on "id" for invoices (tck) adding the invoice's id column in the debts list

--- apps/tck/modules/invoice/templates/_id.php
+++ apps/tck/modules/invoice/templates/_id.php
@@ -1,3 +1,3 @@
 #<?php echo $sf_user->hasCredential('tck-transaction')
-  ? link_to($invoice->id,'ticket/invoice?id='.$invoice->transaction_id,array('target' => '_blank'))
-  : $invoice->id ?>
+  ? link_to(sfConfig::get('app_seller_invoice_prefix').$invoice->id,'ticket/invoice?id='.$invoice->transaction_id,array('target' => '_blank'))
+  : sfConfig::get('app_seller_invoice_prefix').$invoice->id ?>
