d1aebe7339fcf36765fd6569823442fe864fbd62
beta <betaglop@users.noreply.github.com>
2013-05-29 11:19:27 +0000

    (tck) adding a script which aims to correct some cancellations which has been let on duplicatas... it had an impact on some widgets of the manifestation ledger in particular. (has been corrected in source since novembre 2012) -IMPORT FROM BRANCH v2.4-

---
+++ bin/update-cancels-duplicatas.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DB=$1
+
+[ -z "$DB" ] && echo "Please, precise your DB as the first parameter" && exit 1
+[ -z "$2" ] && echo 'We will only proceed to tests, if you want to update the DB, please precise "notest" as the last parameter'
+
+if [ "$2" != 'notest' ]; then
+  echo "select t.id, t.cancelling, t2.duplicating, e.name, m.happens_at from ticket t2, ticket t left join manifestation m on m.id = t.manifestation_id left join event e on e.id = m.event_id where t.cancelling = t2.id and t2.duplicating is not null and t.cancelling is not null order by happens_at;" | psql $DB;
+  exit 0;
+fi
+
+CPT=0
+CONTINUE=true
+while $CONTINUE; do
+  TEST=`echo 'update ticket t set cancelling = t2.duplicating from ticket t2 where t.cancelling = t2.id and t2.duplicating is not null and t.cancelling is not null;' | psql $DB`
+  [ "$TEST" = 'UPDATE 0' ] && CONTINUE=false
+  let "CPT++"
+  echo iteration $CPT
+done
