af7ee2819faf21ce7d874a45d2d98beeed593158
beta <betaglop@users.noreply.github.com>
2013-05-29 11:11:02 +0000

    (tck) improving the script

--- bin/update-cancels-duplicatas.sh
+++ bin/update-cancels-duplicatas.sh
@@ -3,9 +3,9 @@
 DB=$1
 
 [ -z "$DB" ] && echo "Please, precise your DB as the first parameter" && exit 1
-[ -z "$2" ] && echo We will only proceed to tests, if you want to update the DB, please precise "no test" as the last parameter
+[ -z "$2" ] && echo 'We will only proceed to tests, if you want to update the DB, please precise "notest" as the last parameter'
 
-if [ -z "$2" ]; then
+if [ "$2" != 'notest' ]; then
   echo "select t.id, t.cancelling, t2.duplicating, e.name, m.happens_at from ticket t2, ticket t left join manifestation m on m.id = t.manifestation_id left join event e on e.id = m.event_id where t.cancelling = t2.id and t2.duplicating is not null and t.cancelling is not null order by happens_at;" | psql $DB;
   exit 0;
 fi
