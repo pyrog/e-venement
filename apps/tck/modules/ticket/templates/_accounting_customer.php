c6f7d6248cfa516413759eb02030d5fc16b54f83
FERRAND Bevlhay <drist-li@users.noreply.github.com>
2013-06-06 09:43:40 +0000

    Uncomment phonenumber for customer on accouting and order

--- apps/tck/modules/ticket/templates/_accounting_customer.php
+++ apps/tck/modules/ticket/templates/_accounting_customer.php
@@ -1,10 +1,10 @@
 <div id="customer">
   <p class="name inline-modifiable"><?php echo $transaction->Contact ?></p>
-  <!--<p class="phonenumber inline-modifiable"><?php
+  <p class="phonenumber inline-modifiable"><?php
     echo is_null($transaction->professional_id)
     ? $transaction->Contact->Phonenumbers[0]->number
     : ($transaction->Professional->contact_number ? $transaction->Professional->contact_number : $transaction->Professional->Organism->Phonenumbers[0]->number)
-  ?></p>-->
+  ?></p>
   <p class="orgname inline-modifiable"><?php echo $transaction->Professional->Organism->name ?></p>
   <p class="address inline-modifiable"><?php echo nl2br(ucwords(strtolower($transaction->professional_id ? $transaction->Professional->Organism->address : $transaction->Contact->address))) ?></p>
   <p class="postalcode inline-modifiable"><?php echo $transaction->professional_id ? $transaction->Professional->Organism->postalcode : $transaction->Contact->postalcode ?></p>
