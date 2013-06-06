4c8bff974fdf59f54203f7011533e72dcda885fe
beta <betaglop@users.noreply.github.com>
2013-06-06 11:22:29 +0000

    (tck) updating the SHELL script which controls and updates duplications / cancellations

--- bin/update-cancels-duplicatas.sh
+++ bin/update-cancels-duplicatas.sh
@@ -6,9 +6,22 @@ DB=$1
 [ -z "$2" ] && echo 'We will only proceed to tests, if you want to update the DB, please precise "notest" as the last parameter'
 
 if [ "$2" != 'notest' ]; then
+  echo "Problems with duplicatas:"
   echo "select t.id, t.cancelling, t2.duplicating, e.name, m.happens_at from ticket t2, ticket t left join manifestation m on m.id = t.manifestation_id left join event e on e.id = m.event_id where t.cancelling = t2.id and t2.duplicating is not null and t.cancelling is not null order by happens_at;" | psql $DB;
+  echo ""
+  echo "Problem with multi-cancelling:"
+  echo "select cancelling as ticket_id, count(id) AS nb, min(id) AS cancelling_min_id, max(id) AS cancelling_max_id from ticket t where cancelling is not null and duplicating is null and cancelling in (select cancelling from ticket where t.id != id and cancelling is not null and duplicating is null) group by cancelling;" | psql $DB
   exit 0;
 fi
+
+CPT=0
+CONTINUE=true
+while $CONTINUE; do
+  TEST=`echo 'update ticket t set cancelling = t2.duplicating from ticket t2 where t.cancelling = t2.id and t2.duplicating is not null and t.cancelling is not null;' | psql $DB`
+  [ "$TEST" = 'UPDATE 0' ] && CONTINUE=false
+  let "CPT++"
+  echo iteration $CPT
+done
 
 CPT=0
 CONTINUE=true
