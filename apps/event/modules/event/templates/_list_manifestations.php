679e6cd6be6cfe0d7d6c71f174017e6e437de905
beta <betaglop@users.noreply.github.com>
2013-06-14 11:35:12 +0000

    (event) some mak'up for the events list (about manifestations)

--- apps/event/modules/event/templates/_list_manifestations.php
+++ apps/event/modules/event/templates/_list_manifestations.php
@@ -5,7 +5,7 @@
       link_to(format_date($manif->happens_at,'EEE d MMM yyyy HH:mm'),
         'manifestation/show?id='.$manif->id,
         array('title' => format_date(strtotime($manif->happens_at) + strtotime($manif->duration_h_r) - strtotime('0:00'), 'EEE d MMM yyyy HH:mm').' @ '.$manif->Location, 'style' =>  $manif->Color ? 'background-color: #'.$manif->Color->color.';' : '')
-      ).' <a title="'.__('Sell').'" href="'.cross_app_url_for('tck','ticket/sell#manif-'.$manif->id).'"><span class="ui-icon-li ui-icon ui-icon-microplus"></span></a>'
+      ).' <a class="sell" title="'.__('Sell').'" href="'.cross_app_url_for('tck','ticket/sell#manif-'.$manif->id).'"><span class="ui-icon-li ui-icon ui-icon-microplus"></span></a>'
   ?>
 <?php endforeach ?>
 <?php ksort($manifs) ?>
