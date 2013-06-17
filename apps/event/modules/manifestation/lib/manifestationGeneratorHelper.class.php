814df144103b9f2bfbc4a60e58843b91bbba1aa8
beta <betaglop@users.noreply.github.com>
2013-06-17 10:38:03 +0000

    (event) removing a PHP warning

--- apps/event/modules/manifestation/lib/manifestationGeneratorHelper.class.php
+++ apps/event/modules/manifestation/lib/manifestationGeneratorHelper.class.php
@@ -22,7 +22,8 @@ class manifestationGeneratorHelper extends BaseManifestationGeneratorHelper
   {
     if (!key_exists('ui-icon', $params)) $params['ui-icon'] = '';
     $params['params'] = UIHelper::addClasses($params, '');
-    $params['ui-icon'] = $this->getIcon($params['extra-icon'], $params);
+    if ( isset($params['extra-icon']) )
+      $params['ui-icon'] = $this->getIcon($params['extra-icon'], $params);
     return '<li class="sf_admin_action_'.$params['action'].'">'
       .($params['app']
         ? cross_app_link_to(UIHelper::addIcon($params) . __($params['label']), $params['app'], ($params['module'] ? $params['module'] : sfContext::getInstance()->getModuleName()).'/'.$params['action'], false, '', false, 'class="fg-button ui-state-default fg-button-icon-left"')
