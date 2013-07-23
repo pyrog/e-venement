76c655d15654fc6fe61eff90268df32b4fdb5bc7
beta <betaglop@users.noreply.github.com>
2013-07-23 11:15:08 +0000

    (stats) optimizing the structure of displaid criterias

--- apps/stats/modules/prices/templates/indexSuccess.php
+++ apps/stats/modules/prices/templates/indexSuccess.php
@@ -6,30 +6,9 @@
     <?php include_partial('attendance/filters_buttons') ?>
     <h1><?php echo __('Tickets by price',null,'menu') ?></h1>
   </div>
-  <?php $criterias = $sf_user->getAttribute('stats.criterias',array(),'admin_module') ?>
-  <div class="ui-widget-content show-criterias ui-corner-all">
-    <ul>
-      <?php if ( isset($criterias['dates']['from']) && isset($criterias['dates']['to']) ): ?>
-      <li class="dates">
-        <?php echo __('To %%to%% from %%from%%',array(
-          '%%from%%'  => format_date($criterias['dates']['from']),
-          '%%to%%'    => format_date($criterias['dates']['to']),
-        )) ?>
-      </li>
-      <?php endif ?>
-      <?php if ( count($criterias['workspaces_list']) > 0 ): ?>
-      <li class="workspaces"><?php echo __('%%nb%% workspace(s)', array( '%%nb%%' => count($criterias['workspaces_list']) )) ?></li>
-      <?php endif ?>
-      <?php if ( count($criterias['meta_events_list']) > 0 ): ?>
-      <li class="workspaces"><?php echo __('%%nb%% meta-event(s)',array( '%%nb%%' => count($criterias['meta_events_list']) )) ?></li>
-      <?php endif ?>
-      <?php if ( count($criterias['users_list']) > 0 ): ?>
-      <li class="workspaces"><?php echo __('%%nb%% user(s)',array( '%%nb%%' => count($criterias['users_list']) )) ?></li>
-      <?php endif ?>
-    </ul>
-  </div>
-<?php include_partial('chart_ordered') ?>
+<?php include_partial('show_criterias') ?>
 <?php include_partial('chart_printed') ?>
+<?php include_partial('chart_ordered') ?>
 <div class="clear"></div>
 <?php if ( sfConfig::get('project_count_demands',false) ): ?>
 <?php include_partial('chart_asked') ?>
