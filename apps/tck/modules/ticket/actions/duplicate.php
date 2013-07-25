8cc8d20e6a0b52c21c8d4aeb51ff421bbc60ddee
beta <betaglop@users.noreply.github.com>
2013-07-25 17:53:27 +0000

    (tck) rejecting duplications when a ticket's transaction has been closed already

--- apps/tck/modules/ticket/actions/duplicate.php
+++ apps/tck/modules/ticket/actions/duplicate.php
@@ -50,6 +50,11 @@
         $this->getUser()->setFlash('error',__("Can't find the ticket #%%i%% in database...",array('%%i%%' => $ticket->id)));
         $this->redirect('ticket/cancel');
       }
+      if ( !$ticket->Transaction->closed )
+      {
+        $this->getUser()->setFlash('error',__("Can't duplicate the ticket #%%i%% because its transaction has been closed already.",array('%%i%%' => $ticket->id)));
+        $this->redirect('ticket/sell?id='.$ticket->transaction_id);
+      }
       if ( !$ticket->printed )
       {
         $this->getUser()->setFlash('error',__("Can't duplicate the ticket #%%i%% because it was not yet printed... Just try to print it",array('%%i%%' => $ticket->id)));
