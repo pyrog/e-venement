5aff765cc630212aa7581bc95edff4da9dcf5d33
beta <betaglop@users.noreply.github.com>
2013-08-29 07:33:49 +0000

    (pub) adding the text about the reasons why the system is closed -IMPORT FROM v2.5-

--- apps/pub/config/pubConfiguration.class.php
+++ apps/pub/config/pubConfiguration.class.php
@@ -9,6 +9,6 @@ class pubConfiguration extends sfApplicationConfiguration
   public function shut()
   {
     if ( !sfConfig::get('app_open',false) )
-      die($this->getEnvironment() == 'dev' ? 'This application is not opened' : '');
+      die($this->getEnvironment() == 'dev' ? 'This application is not opened' : sfConfig::get('app_texts_when_closed',''));
   }
 }
