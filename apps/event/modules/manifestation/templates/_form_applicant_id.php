731c952035922ac000f8c8992b816594faeaa2f1
beta <betaglop@users.noreply.github.com>
2013-09-03 15:29:50 +0000

    (event) correcting a bad condition to show a manifestation's applicant

--- apps/event/modules/manifestation/templates/_form_applicant_id.php
+++ apps/event/modules/manifestation/templates/_form_applicant_id.php
@@ -1,5 +1,5 @@
 <div class="sf_admin_form_row sf_admin_form_field_applicant">
-  <?php if ( $form->getObject()->Applicant ): ?>
+  <?php if ( !$form->getObject()->Applicant->isNew() ): ?>
   <div class="label ui-helper-clearfix"><label><?php echo __('The applicant') ?>:</label></div>
   <?php echo cross_app_link_to($form->getObject()->Applicant, 'rp', 'contact/show?id='.$form->getObject()->Applicant->id) ?>
   <?php endif ?>
