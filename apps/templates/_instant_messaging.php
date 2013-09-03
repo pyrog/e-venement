89a6693f4f2d7561ea5753d53d2b38ffdb2729e2
beta <betaglop@users.noreply.github.com>
2013-09-03 09:59:33 +0000

    (global) progressing in e-venement IM integration

--- apps/templates/_instant_messaging.php
+++ apps/templates/_instant_messaging.php
@@ -4,6 +4,7 @@
       jQuery.ajaxSetup({cache: true});
       jQuery.getScript("/liJappixPlugin/php/get.php?l=fr&t=js&g=mini.xml", function() {
         MINI_GROUPCHATS = ["<?php echo implode('", "',sfConfig::get('project_messaging_chatrooms',array())) ?>"];
+        MINI_SUGGEST_GROUPCHATS = ["<?php echo implode('", "',sfConfig::get('project_messaging_chatrooms',array())) ?>"];
         launchMini(true, false, "<?php echo $jabber_id[1] ?>", "<?php echo $jabber_id[0] ?>", "<?php echo $sf_user->getJabber(0)->password ?>");
       });
     </script>
