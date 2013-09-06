c946d158227b709a1c5c0014711b45ddbdc7551a
beta <betaglop@users.noreply.github.com>
2013-09-06 11:00:44 +0000

    (rp) restricting access to groups to real credentials + permissions, to avoid exceptions after a graphic feeling of permission

--- lib/form/doctrine/ContactForm.class.php
+++ lib/form/doctrine/ContactForm.class.php
@@ -38,14 +38,6 @@ class ContactForm extends BaseContactForm
     ));
     
     $q = Doctrine::getTable('Group')->createQuery('g');
-    if ( sfContext::hasInstance() )
-    {
-      $q->where('(TRUE')
-        ->andWhere('g.sf_guard_user_id = ?',sfContext::getInstance()->getUser()->getId());
-      if ( sfContext::getInstance()->getUser()->hasCredential('pr-group-common') )
-        $q->orWhere('g.sf_guard_user_id IS NULL');
-      $q->andWhere('TRUE)');
-    }
     $this->widgetSchema   ['groups_list']
       ->setOption('order_by', array('u.id IS NULL DESC, u.username, name',''))
       ->setOption('query', $q);
