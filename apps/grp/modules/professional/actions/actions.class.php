1ae9cf5d34b4af87bdd11dd18f59382a33a06ebc
beta <betaglop@users.noreply.github.com>
2013-09-13 15:15:10 +0000

    (grp) removing contacts' history if current user doesn't have the credentials to show them (through meta events) -IMPORT FROM v2.5-

--- apps/grp/modules/professional/actions/actions.class.php
+++ apps/grp/modules/professional/actions/actions.class.php
@@ -1,4 +1,27 @@
 <?php
+/**********************************************************************************
+*
+*	    This file is part of e-venement.
+*
+*    e-venement is free software; you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation; either version 2 of the License.
+*
+*    e-venement is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with e-venement; if not, write to the Free Software
+*    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*
+*    Copyright (c) 2006-2013 Baptiste SIMON <baptiste.simon AT e-glop.net>
+*    Copyright (c) 2006-2013 Libre Informatique [http://www.libre-informatique.fr/]
+*
+***********************************************************************************/
+?>
+<?php
 
 require_once dirname(__FILE__).'/../lib/professionalGeneratorConfiguration.class.php';
 require_once dirname(__FILE__).'/../lib/professionalGeneratorHelper.class.php';
@@ -13,4 +36,26 @@ require_once dirname(__FILE__).'/../lib/professionalGeneratorHelper.class.php';
  */
 class professionalActions extends autoProfessionalActions
 {
+  public function executeShow(sfWebRequest $request)
+  {
+    $table = Doctrine::getTable('Professional');
+    $this->professional = $table->doSelectOnlyGrp($table->createQuery('p'))
+      ->andWhere('p.id = ?', $request->getParameter('id'))
+      ->fetchOne();
+    $this->forward404Unless($this->professional);
+    $this->form = $this->configuration->getForm($this->professional);
+  }
+  
+  public function executeEdit(sfWebRequest $request)
+  { $this->redirect('professional/show?id='.$request->getParameter('id')); }
+  public function executeUpdate(sfWebRequest $request)
+  { throw new liEvenementException('This action is not implemented.'); }
+  public function executeNew(sfWebRequest $request)
+  { throw new liEvenementException('This action is not implemented.'); }
+  public function executeCreate(sfWebRequest $request)
+  { throw new liEvenementException('This action is not implemented.'); }
+  public function executeDelete(sfWebRequest $request)
+  { throw new liEvenementException('This action is not implemented.'); }
+  public function executeBatchDelete(sfWebRequest $request)
+  { throw new liEvenementException('This action is not implemented.'); }
 }
