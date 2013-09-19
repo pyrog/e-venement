429b12d0f8316b1d0224cd212cf776d91cddb702
beta <betaglop@users.noreply.github.com>
2013-09-19 11:26:42 +0000

    (event) improving resources conflicts display during edition

--- apps/event/modules/manifestation/templates/_form_resources_test.php
+++ apps/event/modules/manifestation/templates/_form_resources_test.php
@@ -8,6 +8,10 @@
       if ( $(this).is(':checked') )
         li_manifestation_check_resource(this);
     }).change();
+    $('.sf_admin_form input[name="manifestation[blocking]"]').change(function(){
+      $('.sf_admin_form input[name="manifestation[booking_list][]"]').change();
+      $('.sf_admin_form select[name="manifestation[location_id]"]').change();
+    });
     
     // for every change in reservation dates
     $('.sf_admin_form_field_happens_at, .sf_admin_form_field_ends_at, .sf_admin_form_field_reservation_begins_at, .sf_admin_form_field_reservation_ends_at').change(function(){
@@ -15,8 +19,23 @@
     });
   });
   
-  function li_manifestation_check_resource(elt)
+  function li_manifestation_check_resource(elt = NULL)
   {
+    // not a blocking booking
+    if ( $('input[name="manifestation[blocking]"]:checked').length == 0 )
+    {
+      $('.sf_admin_form_field_booking_list li.ui-state-error')
+        .removeClass('ui-state-error')
+        .find('.error.conflict').remove();
+      $('.sf_admin_form_field_location_id').each(function(){
+        if ( $(this).find('input[value=""]').length > 0 )
+          return;
+        $(this).removeClass('ui-state-error')
+          .find('.error.conflict').remove();
+      });
+      return;
+    }
+    
     var start = li_manifestation_datetime('reservation_begins_at').getTime()/1000;
     var stop  = li_manifestation_datetime('reservation_ends_at').getTime()/1000;
     var location_id = $(elt).val();
@@ -31,7 +50,18 @@
       only_blocking: true,
     }, function(data){
       if ( data.length > 0 )
+      {
+        $(elt).parent().find('.error.conflict').remove();
+        for ( i = 0 ; i < data.length ; i++ )
+        if ( parseInt(data[i].id)+'' === ''+data[i].id )
+        {
+          $(elt).parent().append($('<div class="error conflict"><a></a></div>')).find('.error.conflict a')
+            .html(data[i].title)
+            .prop('href', data[i].hackurl)
+            ;
+        }
         $(elt).parent().addClass('ui-state-error').addClass('ui-corner-all');
+      }
       else
         $(elt).parent().removeClass('ui-state-error');
     }, 'json');
