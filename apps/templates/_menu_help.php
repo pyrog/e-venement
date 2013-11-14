3100089b7ba59971027f901d09d476288e9064cf
beta <betaglop@users.noreply.github.com>
2013-11-14 16:00:33 +0000

    (global) changing the link to the documentation (global) adding a link in the help menu to the FAQ

--- apps/templates/_menu_help.php
+++ apps/templates/_menu_help.php
@@ -23,7 +23,8 @@
 ?>
       <li>
         <ul class="second">
-          <li><a href="http://www.libre-informatique.fr/sw/01-Billetterie/e-venement/Manuels" target="_blank"><?php echo __('Documentation',array(),'menu') ?></a></li>
+          <li><a href="http://www.e-venement.org/documentation/" target="_blank"><?php echo __('Documentation',array(),'menu') ?></a></li>
+          <li><a href="http://www.e-venement.org/documentation/faq/" target="_blank"><?php echo __('Frequently asked questions',array(),'menu') ?></a></li>
           <li><a href="<?php echo cross_app_url_for('default','about') ?>" class="fancybox"><?php echo __('About',array(),'menu') ?></a></li>
         </ul>
         <span class="title"><?php echo __('Help',array(),'menu') ?></span>
