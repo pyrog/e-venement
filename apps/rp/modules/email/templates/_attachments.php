6b1545ea8f499964d065a899e7becef0373397b2
beta <betaglop@users.noreply.github.com>
2013-11-14 16:30:10 +0000

    (rp) emailing: correcting a bad URI to attachments when any user wants to click on the link

--- apps/rp/modules/email/templates/_attachments.php
+++ apps/rp/modules/email/templates/_attachments.php
@@ -7,7 +7,7 @@
 <ul>
   <?php foreach ( $form->getObject()->Attachments as $att ): ?>
   <li class="attachment-<?php echo $att->id ?>">
-    <a href="<?php echo dirname($_SERVER['SCRIPT_NAME']).$att->getWebUri() ?>" target="_blank"><?php echo $att ?></a>
+    <a href="<?php echo $att->getWebUri() ?>" target="_blank"><?php echo $att ?></a>
     <a href="<?php echo url_for('email/deleteAttachment?id='.$form->getObject()->id.'&attachment_id='.$att->id) ?>" class="fg-button-mini fg-button ui-state-default fg-button-icon-left" onclick="javascript: $.get($(this).attr('href')); $(this).closest('li').remove(); return false;"><span class="ui-icon ui-icon-trash"></span>Supprimer</a>
   </li>
   <?php endforeach ?>
