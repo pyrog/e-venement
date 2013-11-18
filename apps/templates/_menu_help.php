d7b8ae4000f6cdfd0c8c88e9fd6fb53ad2c467ff
beta <betaglop@users.noreply.github.com>
2013-11-18 20:44:31 +0000

    (global) i18n on FAQ

--- apps/templates/_menu_help.php
+++ apps/templates/_menu_help.php
@@ -24,7 +24,7 @@
       <li>
         <ul class="second">
           <li><a href="http://www.e-venement.org/documentation/" target="_blank"><?php echo __('Documentation',array(),'menu') ?></a></li>
-          <li><a href="http://www.e-venement.org/documentation/faq/" target="_blank"><?php echo __('Frequently asked questions',array(),'menu') ?></a></li>
+          <li><a href="http://www.e-venement.org/documentation/faq/" target="_blank"><?php echo __('Frequently Asked Questions',array(),'menu') ?></a></li>
           <li><a href="<?php echo cross_app_url_for('default','about') ?>" class="fancybox"><?php echo __('About',array(),'menu') ?></a></li>
         </ul>
         <span class="title"><?php echo __('Help',array(),'menu') ?></span>
