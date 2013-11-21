c28920e1b40b811204b3ebeb6080b7aef02af59e
beta <betaglop@users.noreply.github.com>
2013-11-21 20:39:53 +0000

    (global) generizing the Picture use (event) adding the first base for graphical seated plans user-defined

---
+++ bin/migration-to-v27.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# preconditions
+[ -z "$1" ] && echo "You forgot to specify your DB name as the first parameter" && exit 1;
+[ -z "$2" ] && echo "You forgot to specify your user for DB access as the second parameter" && exit 2;
+[ ! -d "data/sql" ] && echo "cd to your project's root directory please" && exit 3;
+[ ! -z "$3" ] && echo "You'll do a restore-only procedure"
+
+DB=$1
+USER=$2
+
+echo "Are you sure you want to continue with those parameters :"
+echo Database: $DB
+echo User: $USER
+[ ! -z "$3" ] && echo "With a restore only procedure (no DB backup, no previous backup overwritting)"
+[ -z "$3" ] && echo "With a backup which is going to be written in data/sql/$DB-`date +%Y%m%d`.pgdump"
+echo ""
+echo "To continue press ENTER"
+echo "To cancel press CTRL+C NOW !!"
+read
+
+if [ -z "$3" ]; then
+
+# preliminary modifications & backup
+psql $DB <<EOF
+  DROP TABLE seating_plan;
+EOF
+
+rm -rf lib/*/doctrine/
+svn update
+
+echo "DUMPING DB..."
+pg_dump -Fc $DB > data/sql/$DB-`date +%Y%m%d`.pgdump && echo "DB dumped"
+
+echo ""
+echo To continue press ENTER
+read
+
+fi #end of "allow dumps" condition
+
+# recreation and data backup
+dropdb $DB && createdb $DB && \
+echo "GRANT ALL ON DATABASE $DB TO $USER" | psql $DB && \
+./symfony doctrine:build  --all --no-confirmation && \
+cat data/sql/$DB-`date +%Y%m%d`.pgdump | pg_restore --disable-triggers -Fc -d $DB -a
+cat config/doctrine/functions-pgsql.sql | psql $DB && \
+./symfony cc &> /dev/null
+echo ""
+
+[ ! -f apps/default/config/app.yml ] && cp apps/default/config/app.yml.template apps/default/config/app.yml
+
+# final data modifications
+echo ""
+echo "Doing something"
+psql $DB <<EOF
+EOF
+
+echo ""
+echo ""
+echo "Patching framework..."
+for elt in data/diff/*.diff
+do
+  patch -N -p0 < $elt
+done
+rm -f `find lib/vendor/ -iname '*.rej'`
+rm -f `find lib/vendor/ -iname '*.orig'`
+
+# final informations
+echo ""
+echo ""
+echo "Don't forget to configure those extra features :"
+echo "- Seated plans for your locations"
+
+echo ""
+echo "Don't forget to inform your users about those evolutions"
---
+++ lib/form/doctrine/SeatedPlanForm.class.php
@@ -0,0 +1,70 @@
+<?php
+/**********************************************************************************
+*
+*	    This file is part of e-venement.
+*
+*    e-venement is free software; you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation; either version 2 of the License.
+*
+*    e-venement is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with e-venement; if not, write to the Free Software
+*    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*
+*    Copyright (c) 2006-2013 Baptiste SIMON <baptiste.simon AT e-glop.net>
+*    Copyright (c) 2006-2013 Libre Informatique [http://www.libre-informatique.fr/]
+*
+***********************************************************************************/
+?>
+<?php
+
+/**
+ * SeatedPlan form.
+ *
+ * @package    e-venement
+ * @subpackage form
+ * @author     Baptiste SIMON <baptiste.simon AT e-glop.net>
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class SeatedPlanForm extends BaseSeatedPlanForm
+{
+  public function doSave($con = NULL)
+  {
+    $picform_name = 'Picture';
+    $file = $this->values[$picform_name]['content_file'];
+    unset($this->values[$picform_name]['content_file']);
+    
+    if (!( $file instanceof sfValidatedFile ))
+      unset($this->embeddedForms[$picform_name]);
+    else
+    {
+      // data translation
+      $this->values[$picform_name]['content']  = base64_encode(file_get_contents($file->getTempName()));
+      $this->values[$picform_name]['name']     = $file->getOriginalName();
+      $this->values[$picform_name]['type']     = $file->getType();
+      $this->values[$picform_name]['width']    = 1024;
+      $this->values[$picform_name]['height']   = 1200;
+    }
+    
+    return parent::doSave($con);
+  }
+  
+  public function configure()
+  {
+    // pictures & co
+    $this->embedRelation('Picture');
+    foreach ( array('name', 'type', 'version', 'height', 'width',) as $fieldName )
+      unset($this->widgetSchema['Picture'][$fieldName], $this->validatorSchema['Picture'][$fieldName]);
+    $this->validatorSchema['Picture']['content_file']->setOption('required',false);
+    unset($this->widgetSchema['picture_id'], $this->validatorSchema['picture_id']);
+    
+    $this->widgetSchema['location_id']
+      ->setOption('query', Doctrine::getTable('Location')->createQuery())
+      ->setOption('order_by', array('name',''));
+  }
+}
