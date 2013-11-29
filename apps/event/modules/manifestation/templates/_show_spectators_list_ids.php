c8e23d49bd792472a6774a96c783b3444ebaa918
beta <betaglop@users.noreply.github.com>
2013-11-29 17:53:03 +0000

    (event) adding the template that shows the numerotation & ids

---
+++ apps/event/modules/manifestation/templates/_show_spectators_list_ids.php
@@ -0,0 +1,10 @@
+<?php $tickets = $sf_data->getRaw('tickets') ?>
+<?php ksort($tickets) ?>
+<span class="tickets">
+  <?php foreach ( $tickets as $key => $id ) if ( $key != 'name' ): ?>
+    <?php echo $num.$id.PHP_EOL ?>
+    <?php if ( $show_workspaces ): ?>
+      <span class="workspace"><?php echo $ids['name'] ?></span>
+    <?php endif ?>
+  <?php endif ?>
+</span>
