0940cd4fe6ec427237580967453d142f3b650c79
beta <betaglop@users.noreply.github.com>
2013-11-29 10:38:06 +0000

    (event) adding a missing template

---
+++ apps/event/modules/seated_plan/templates/_list_nb_seats.php
@@ -0,0 +1,1 @@
+<?php echo Doctrine_Query::create()->from('SeatedPlan sp')->select('sp.id, count(s.id) nb_seats')->leftJoin('sp.Seats s')->andWhere('sp.id = ?',$seated_plan->id)->groupBy('sp.id')->fetchOne()->nb_seats ?>
