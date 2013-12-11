fb4dec79080f3fe08526de24be798d0b705945a2
beta <betaglop@users.noreply.github.com>
2013-12-11 15:33:17 +0000

    (rp) generizing the filters hierachical management (event) generizing the filters hierachical management (tck) preparing the menu for the new touchscreen ticketting process

--- apps/templates/_menu_ticketting.php
+++ apps/templates/_menu_ticketting.php
@@ -32,7 +32,7 @@
           <li><a href="<?php echo cross_app_url_for('tck','ticket/sell') ?>"><?php echo __('New transaction',array(),'menu') ?></a></li>
           <?php endif ?>
           <?php if ( $sf_user->hasCredential('tck-transaction') && $sf_user->hasCredential('tck-transaction-touchy') ): ?>
-          <li><a href="<?php echo cross_app_url_for('tck','ticket/touchscreen') ?>"><?php echo __('New transaction (touchscreens)',array(),'menu') ?></a></li>
+          <li><a href="<?php echo cross_app_url_for('tck','transaction/new') ?>"><?php echo __('New transaction (touchscreens)',array(),'menu') ?></a></li>
           <?php endif ?>
           <?php if ( $sf_user->hasCredential('tck-unblock') ): ?>
           <li><a href="<?php echo cross_app_url_for('tck','ticket/respawn') ?>"><?php echo __('Respawn a transaction',array(),'menu') ?></a></li>
