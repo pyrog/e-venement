b1ca1679d8f0b637e85ed16c2d7a371d6300bfc6
beta <betaglop@users.noreply.github.com>
2014-01-08 14:44:10 +0000

    (tck) reconverting a condition that was rewritten for debug purposes

--- apps/tck/modules/transaction/templates/_form_field_close.php
+++ apps/tck/modules/transaction/templates/_form_field_close.php
@@ -7,6 +7,6 @@
 <ul class="overbooking">
   <li class="msg block"><?php echo __('The blinking quantities mean that those gauges are or will be full.') ?><br/><?php echo PHP_EOL ?><?php echo __('You cannot proceed your action though.') ?></li>
   <li class="msg warn"><?php echo __('The blinking quantities mean that those gauges are or will be full.') ?><br/><?php echo PHP_EOL ?><?php echo __('Please confirm that you allow this overbooking.') ?></li>
-  <li class="type" data-type="<?php echo sfConfig::get('app_transaction_gauge_block') && $sf_user->hasCredential('tck-admin') ? 'block' : 'warn' ?>"></li>
+  <li class="type" data-type="<?php echo sfConfig::get('app_transaction_gauge_block') && !$sf_user->hasCredential('tck-admin') ? 'block' : 'warn' ?>"></li>
 </ul>
 </form>
