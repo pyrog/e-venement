5f530986a71be0b843afb8dd02cc4766743c0cc1
beta <betaglop@users.noreply.github.com>
2014-01-27 13:43:16 +0000

    (rp) going forward in the backport of PDF labels

--- apps/rp/modules/option_labels/templates/indexSuccess.php
+++ apps/rp/modules/option_labels/templates/indexSuccess.php
@@ -1,8 +1,11 @@
-<form action="<?php echo url_for('option_labels/update') ?>" method="post" class="sf_admin_form ui-widget-content ui-corner-all sf_admin_edit full-lines" id="sf_admin_container">
+<div class="sf_admin_form ui-widget-content ui-corner-all sf_admin_edit full-lines" id="sf_admin_container">
   <div class="fg-toolbar ui-widget-header ui-corner-all">
     <h1><?php echo __('Parameters for labels') ?></h1>
   </div>
+  <?php include_partial('form_header',array('form' => $form,)); ?>
   <?php include_partial('global/flashes') ?>
-  <?php include_partial('global/option_form',array('form' => $form,)); ?>
-  <?php include_partial('option_labels/form_save',array('form' => $form,)); ?>
-</form>
+  <form action="<?php echo url_for('option_labels/update') ?>" method="post" class="data">
+    <?php include_partial('global/option_form',array('form' => $form,)); ?>
+    <?php include_partial('option_labels/form_save',array('form' => $form,)); ?>
+  </form>
+</div>
