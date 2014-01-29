e28854d439fbf55f93ff7edbf894005a2b080384
beta <betaglop@users.noreply.github.com>
2014-01-29 12:07:50 +0000

    (event) optimizing the way seated plans are retrieved in the manifestations

--- apps/event/modules/manifestation/templates/_show_workspaces_list.php
+++ apps/event/modules/manifestation/templates/_show_workspaces_list.php
@@ -21,13 +21,10 @@
       <a href="<?php echo url_for('workspace/show?id='.$gauge->Workspace->id) ?>"><?php echo $gauge->Workspace ?></a>
       (<?php echo $gauge->online ? __('Online') : '' ?>)
       <a class="gauge-gfx" href="<?php echo cross_app_url_for('tck','ticket/gauge?id='.$form->getObject()->id.'&wsid='.$gauge->Workspace->id) ?>">gauge</a>
-      <?php if ( $gauge->Workspace->seated ): ?>
-        <?php $seated_plan = NULL; foreach ( $gauge->Workspace->SeatedPlans as $sp ) if ( $sp->location_id == $form->getObject()->location_id ) { $seated_plan = $sp; break; } ?>
-        <?php if ( $seated_plan ): ?>
-          <a class="picture seated-plan" href="<?php echo url_for('seated_plan/getSeats?id='.$seated_plan->id.'&gauge_id='.$gauge->id) ?>" style="background-color: <?php echo $seated_plan->background ?>;">
-            <?php echo $seated_plan->Picture->getHtmlTag(array('title' => $seated_plan->Picture)) ?>
-          </a>
-        <?php endif ?>
+      <?php if ( $gauge->Workspace->seated && $seated_plan = $form->getObject()->Location->getWorkspaceSeatedPlan($gauge->workspace_id) ): ?>
+      <a class="picture seated-plan" href="<?php echo url_for('seated_plan/getSeats?id='.$seated_plan->id.'&gauge_id='.$gauge->id) ?>" style="background-color: <?php echo $seated_plan->background ?>;">
+        <?php echo $seated_plan->Picture->getHtmlTag(array('title' => $seated_plan->Picture)) ?>
+      </a>
       <?php endif ?>
     </li>
     <?php endforeach ?>
