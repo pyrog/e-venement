415d9a8e4d784f58311a4945a4064e08cf3e62f1
beta <betaglop@users.noreply.github.com>
2014-01-30 18:15:14 +0000

    (tck) adding the ticket's seat in its log

--- apps/tck/modules/ticket/templates/_show_ticket_workspace.php
+++ apps/tck/modules/ticket/templates/_show_ticket_workspace.php
@@ -1,5 +1,5 @@
 <td><?php echo __('Workspace') ?></td>
 <td><?php echo $ticket->Gauge->Workspace ?></td>
-<td></td>
+<td title="<?php echo __('seat') ?>"><?php echo $ticket->numerotation ?></td>
 <td></td>
 
