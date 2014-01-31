9012aa61476d970a3a08e00399cb27d6e82cb172
beta <betaglop@users.noreply.github.com>
2014-01-31 17:32:21 +0000

    (tck) avoiding to seat cancelling tickets, because it is itself a non sens

--- apps/tck/modules/ticket/actions/seats-allocation.php
+++ apps/tck/modules/ticket/actions/seats-allocation.php
@@ -38,6 +38,7 @@
     ->leftJoin('g.Workspace ws')
     ->leftJoin('m.Event e')
     ->andWhere('t.id = ?',$request->getParameter('id',0))
+    ->andWhere('tck.cancelling IS NULL')
     ->andWhere('tck.id NOT IN (SELECT tt.duplicating FROM ticket tt  WHERE tt.duplicating IS NOT NULL AND tt.transaction_id = t.id)')
     ->andWhere('tck.id NOT IN (SELECT ttt.cancelling FROM ticket ttt LEFT JOIN ttt.Transaction tttr WHERE ttt.cancelling IS NOT NULL AND tttr.transaction_id = t.id)')
     ->andWhere('tck.gauge_id = ?',$request->getParameter('gauge_id',0))
