e038a89b98d493aa024c016b2f3e42732663870a
beta <betaglop@users.noreply.github.com>
2014-01-31 13:25:16 +0000

    (event) ordering data in the events creation form

--- lib/form/doctrine/EventForm.class.php
+++ lib/form/doctrine/EventForm.class.php
@@ -23,7 +23,10 @@ class EventForm extends BaseEventForm
     $this->widgetSchema['extraspec'] = new liWidgetFormTextareaTinyMCE($tinymce);
     $this->widgetSchema['name'] = new sfWidgetFormTextarea(array(), array('rows' => '1', 'cols' => 58));
     
-    $this->widgetSchema['meta_event_id']->setOption('query',EventFormFilter::addCredentialsQueryPart(Doctrine::getTable('MetaEvent')->createQuery('me')));
+    $this->widgetSchema['meta_event_id']
+      ->setOption('query',EventFormFilter::addCredentialsQueryPart(Doctrine::getTable('MetaEvent')->createQuery('me')))
+      ->setOption('order_by', array('name',''));
+    $this->widgetSchema['event_category_id']->setOption('order_by', array('name',''));
     $this->widgetSchema['companies_list'] = new cxWidgetFormDoctrineJQuerySelectMany(array(
       'model' => 'Organism',
       'url'   => cross_app_url_for('rp','organism/ajax'),
