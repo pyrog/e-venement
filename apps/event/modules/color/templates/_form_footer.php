f71f18c625bddc80fbc28e677492356582922834
beta <betaglop@users.noreply.github.com>
2014-01-31 11:21:22 +0000

    (event) idem

---
+++ apps/event/modules/color/templates/_form_footer.php
@@ -0,0 +1,7 @@
+<div id="colorpicker"></div>
+<script type="text/javascript">
+$(document).ready(function(){
+  $('#colorpicker').insertBefore('#color_color');
+  $('#colorpicker').farbtastic('#color_color');
+});
+</script>
