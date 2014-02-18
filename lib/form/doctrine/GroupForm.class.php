100e63903a5091ac4fa9a990e6f7c9c7e7b493cf
beta <betaglop@users.noreply.github.com>
2014-02-18 23:47:16 +0000

    (rp) adding a hack to avoid missing picture updates in groups -BACKPORT FROM v2.7-

--- lib/form/doctrine/GroupForm.class.php
+++ lib/form/doctrine/GroupForm.class.php
@@ -1,4 +1,27 @@
 <?php
+/**********************************************************************************
+*
+*	    This file is part of e-venement.
+*
+*    e-venement is free software; you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation; either version 2 of the License.
+*
+*    e-venement is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with e-venement; if not, write to the Free Software
+*    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*
+*    Copyright (c) 2006-2014 Baptiste SIMON <baptiste.simon AT e-glop.net>
+*    Copyright (c) 2006-2014 Libre Informatique [http://www.libre-informatique.fr/]
+*
+***********************************************************************************/
+?>
+<?php
 
 /**
  * Group form.
@@ -26,6 +49,9 @@ class GroupForm extends BaseGroupForm
       $this->values[$picform_name]['type']     = $file->getType();
       $this->values[$picform_name]['width']    = 24;
       $this->values[$picform_name]['height']   = 16;
+      
+      // hack to force root object update
+      $this->values['updated_at'] = date('Y-m-d H:i:s');
     }
     
     return parent::doSave($con);
