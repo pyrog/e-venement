943b7aa236c07cb798deffb7a9d2d0203f9451a0
beta <betaglop@users.noreply.github.com>
2014-02-25 19:25:58 +0000

    (rp) correcting stuff about the pro and the org's groups' pictos (event) adding the visual information of remarkable groups for professionals & organisms

--- apps/event/modules/manifestation/templates/_show_spectators_list_line.php
+++ apps/event/modules/manifestation/templates/_show_spectators_list_line.php
@@ -1,5 +1,9 @@
     <td class="name"><?php echo cross_app_link_to($transac->Contact,'rp','contact/show?id='.$transac->contact_id) ?> <span class="pictos"><?php if ( is_object($transac->Contact) ) echo $sf_data->getRaw('transac')->Contact->groups_picto ?></span></td>
-    <td class="organism"><?php if ( $contact['pro'] ) echo cross_app_link_to($contact['pro']->Organism,'rp','organism/show?id='.$contact['pro']->Organism->id) ?></td>
+    <td class="pro-groups"><?php echo $sf_data->getRaw('transac')->Professional->groups_picto ?></td>
+    <td class="organism">
+      <?php if ( $contact['pro'] ) echo cross_app_link_to($contact['pro']->Organism,'rp','organism/show?id='.$contact['pro']->Organism->id) ?>
+      <?php echo $sf_data->getRaw('transac')->Professional->Organism->groups_picto ?>
+    </td>
     <td class="tickets"><?php include_partial('show_spectators_list_tickets',array('tickets' => $ws, 'show_workspaces' => $show_workspaces)) ?></td>
     <td class="price"><?php echo format_currency($contact['value'][$wsid],'€') ?></td>
     <td class="accounting"><?php if ( $contact['transaction']->Invoice[0]->id ): ?>#<?php echo $contact['transaction']->Invoice[0]->id ?><?php else: ?>-<?php endif ?></td>
