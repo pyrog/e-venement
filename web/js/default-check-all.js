cad01fe8759839bdfc02e3f5b2442787050c7403
beta <betaglop@users.noreply.github.com>
2014-02-27 16:30:06 +0000

    (event) adding the minutes in the event shown in the calendar (default) adding a special checkbox to improve the selection of permissions

---
+++ web/js/default-check-all.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $('.sf_admin_form_row .checkbox_list').each(function(){
+    if ( $(this).find('input[type=checkbox]').length > 1 )
+    {
+      $('<input type="checkbox" name="all" value="all" />')
+        .click(function(){
+          $(this).closest('.sf_admin_form_row').find('.checkbox_list input[type=checkbox]').click();
+          return false;
+        })
+        .prependTo($(this).closest('.sf_admin_form_row').find('.label label'))
+      ;
+    }
+  });
+});
