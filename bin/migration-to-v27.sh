7d9b331e87134bb9cfdbf1a3d51214a535cd1632
beta <betaglop@users.noreply.github.com>
2014-02-27 17:59:36 +0000

    (bin) completing the warning at the end of the migration script

--- bin/migration-to-v27.sh
+++ bin/migration-to-v27.sh
@@ -126,6 +126,7 @@ echo ""
 echo "Don't forget to configure those extra features :"
 echo "- Seated plans for your locations"
 echo "- Change the apps/rp/config/factories.yml to replace sfMailer with liMailer"
+echo "- Activate the touchscreen GUI for selling ticket, through the apps/tck/config/app.yml option: design: tdp"
 echo "- Setup again the e-venement Messaging Network : http://[YOUR E-VENEMENT BASE ROOT]/liJappixPlugin"
 
 echo ""
