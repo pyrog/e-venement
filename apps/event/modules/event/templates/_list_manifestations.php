0e074ee06a39c63f65f37512e8c808a2fdd15eec
beta <betaglop@users.noreply.github.com>
2014-02-27 16:08:01 +0000

    (event) correcting the manifestations' ending dates in the events list -IMPORT FROM v2.6-

--- apps/event/modules/event/templates/_list_manifestations.php
+++ apps/event/modules/event/templates/_list_manifestations.php
@@ -4,7 +4,7 @@
     $manifs[$manif->happens_at.'-'.$manif->id] =
       link_to(format_date($manif->happens_at,'EEE d MMM yyyy HH:mm'),
         'manifestation/show?id='.$manif->id,
-        array('title' => '-> '.format_date(strtotime($manif->happens_at) + strtotime($manif->duration_h_r) - strtotime('0:00'), 'EEE d MMM yyyy HH:mm').' @ '.$manif->Location, 'style' => 'background-color: '.$manif->Color.';')
+        array('title' => '-> '.format_date(strtotime($manif->ends_at), 'EEE d MMM yyyy HH:mm').' @ '.$manif->Location, 'style' => 'background-color: '.$manif->Color.';')
       ).' <a class="sell" title="'.__('Sell').'" href="'.cross_app_url_for('tck','ticket/sell#manif-'.$manif->id).'"><span class="ui-icon-li ui-icon ui-icon-microplus"></span></a>'
   ?>
 <?php endforeach ?>
