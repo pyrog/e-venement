78826ffaa9ca4f1c686d2d9b07872d92878662f0
beta <betaglop@users.noreply.github.com>
2014-03-07 10:37:42 +0000

    (global) improving the display of date ranges in filters (tck) improving the graphical rendering of payment methods in the new GUI

--- apps/tck/modules/transaction/templates/_form_field_payment_new.php
+++ apps/tck/modules/transaction/templates/_form_field_payment_new.php
@@ -4,9 +4,6 @@
   'method' => 'get',
 )) ?>
 <?php echo $form->renderHiddenFields() ?>
-<div class="field_payment_method_id field">
-<?php echo $form['payment_method_id'] ?>
-</div>
 <p class="field_created_at field">
 <?php echo $form['created_at']->renderLabel() ?>
 <?php echo $form['created_at'] ?>
@@ -15,6 +12,9 @@
 <?php echo $form['value']->renderLabel() ?>
 <?php echo $form['value']->render(array('class' => 'for-board')) ?>
 </p>
+<div class="field_payment_method_id field">
+<?php echo $form['payment_method_id'] ?>
+</div>
 <p class="submit">
 <button name="s" value="" class="ui-widget-content ui-state-default ui-corner-all ui-widget fg-button"><?php echo __('Add') ?></button>
 </p>
