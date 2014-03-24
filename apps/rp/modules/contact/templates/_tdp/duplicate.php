305af44a794ea9a2d009b7dc24befbead730f8cb
beta <betaglop@users.noreply.github.com>
2014-03-24 09:39:47 +0000

    (rp) adding a missing template for contacts' duplication

---
+++ apps/rp/modules/contact/templates/_tdp/duplicate.php
@@ -0,0 +1,4 @@
+<span class="duplicate">
+  <input id="_duplicate" type="checkbox" class="duplicate" title="<?php echo __('Duplicate') ?>" name="_duplicate" value="+" onchange="javascript: this.form.submit();" />
+  <label for="_duplicate">+</label>
+<span>
