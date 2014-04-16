fff850966f4c81886bef7acb2755c0f50ed87ec4
beta <betaglop@users.noreply.github.com>
2014-04-16 13:23:50 +0000

    (tck) adding contact phonenumbers over pop-up on order list -IMPORT FROM v2.6 (drist)-

--- apps/tck/modules/order/templates/_contact.php
+++ apps/tck/modules/order/templates/_contact.php
@@ -1,5 +1,10 @@
 <?php if ($order->Transaction->professional_id): ?>
 <?php echo cross_app_link_to($order->Transaction->Professional->Organism,'rp','organism/show?id='.$order->Transaction->Professional->Organism->id).' ('.cross_app_link_to($order->Transaction->Contact,'rp','contact/show?id='.$order->Transaction->Contact).')' ?>
 <?php else: ?>
-<?php echo cross_app_link_to($order->Transaction->Contact,'rp','contact/show?id='.$order->Transaction->Contact->id) ?>
+<?php
+  $arr = array();
+  foreach ( $order->Transaction->Contact->Phonenumbers as $phone )
+    $arr[] = (string)$phone;
+?>
+<?php echo cross_app_link_to($order->Transaction->Contact,'rp','contact/show?id='.$order->Transaction->Contact->id,false,null,false,'title="'.implode(', ', $arr).'"') ?>
 <?php endif ?>
