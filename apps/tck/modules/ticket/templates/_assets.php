a74d7ecd47a5c53bb4fbfba20c314e4917808503
beta <betaglop@users.noreply.github.com>
2014-04-23 07:51:56 +0000

    (tck) correcting an obvious bug on vcards (using a CSS instead of a JS)

--- apps/tck/modules/ticket/templates/_assets.php
+++ apps/tck/modules/ticket/templates/_assets.php
@@ -1,11 +1,11 @@
 <?php
-  use_stylesheet('vcard');
   use_stylesheet('ticket','',array('media' => 'all'));
   use_stylesheet('gauge');
   use_stylesheet('/sfFormExtraPlugin/css/jquery.autocompleter.css');
 ?>
 <?php
   use_javascript('/sfFormExtraPlugin/js/jquery.autocompleter.js');
+  use_javascript('vcard');
   use_javascript('ticket');
 ?>
 <?php use_helper('CrossAppLink') ?>
