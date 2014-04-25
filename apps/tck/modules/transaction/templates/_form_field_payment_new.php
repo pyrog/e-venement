6825fcfd66c5e662654d49530a6bc954bb2c1198
FERRAND Bevlhay <drist-li@users.noreply.github.com>
2014-04-25 15:01:05 +0000

    (tck) Adding credential condition for displaying payement type

--- apps/tck/modules/transaction/templates/_form_field_payment_new.php
+++ apps/tck/modules/transaction/templates/_form_field_payment_new.php
@@ -12,9 +12,11 @@
 <?php echo $form['value']->renderLabel() ?>
 <?php echo $form['value']->render(array('class' => 'for-board')) ?>
 </p>
+<?php if ( $sf_user->hasCredential('tck-payement') ): ?>
 <div class="field_payment_method_id field">
 <?php echo $form['payment_method_id'] ?>
 </div>
+<?php endif ?>
 <p class="submit">
 <button name="s" value="" class="ui-widget-content ui-state-default ui-corner-all ui-widget fg-button"><?php echo __('Add') ?></button>
 </p>
