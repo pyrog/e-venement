a75cf8c4c9a8ec025f7c316ba0fee7e3eeb0479a
beta <betaglop@users.noreply.github.com>
2014-05-14 13:57:14 +0000

    (global) the liJappixPlugin has evoluated, and this commit implements this evolution

--- apps/templates/_instant_messaging.php
+++ apps/templates/_instant_messaging.php
@@ -2,7 +2,7 @@
   <?php $jabber_id = explode('@',$sf_user->getJabber(0)->jabber_id) ?>
     <script type="text/javascript">
       jQuery.ajaxSetup({cache: true});
-      jQuery.getScript("<?php echo public_path('/liJappixPlugin/php/get.php?l=fr&t=js&g=mini.xml') ?>", function() {
+      jQuery.getScript("<?php echo public_path('/liJappixPlugin/server/get.php?l=fr&t=js&g=mini.xml') ?>", function() {
         MINI_GROUPCHATS = ["<?php echo implode('", "',sfConfig::get('project_messaging_chatrooms',array())) ?>"];
         MINI_SUGGEST_GROUPCHATS = ["<?php echo implode('", "',sfConfig::get('project_messaging_chatrooms',array())) ?>"];
         launchMini(true, false, "<?php echo $jabber_id[1] ?>", "<?php echo $jabber_id[0] ?>", "<?php echo $sf_user->getJabber(0)->password ?>");
