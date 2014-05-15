4176839c9bd677922f80c509ea519eec7de05abf
beta <betaglop@users.noreply.github.com>
2014-05-15 08:56:55 +0000

    (event) adding a column in the manifestations list, for the comments on bookings (event) displaying the resources w/ comments in italic

--- apps/event/modules/manifestation/templates/_list_booking_list.php
+++ apps/event/modules/manifestation/templates/_list_booking_list.php
@@ -3,7 +3,8 @@
       <li class="empty"></li>
     <?php else: ?>
     <?php foreach ( $manifestation->Booking as $location ): ?>
-    <li title="<?php $h2t = new HtmlToText($location->description); echo $h2t->get_text(); ?>">
+    <?php $h2t = new HtmlToText($location->description); ?>
+    <li class="<?php if ( trim($h2t->get_text()) ): ?>with-comment<?php endif ?>" title="<?php echo $h2t->get_text(); ?>">
       <a href="<?php echo url_for('location/show?id='.$location->id) ?>"><?php echo $location ?></a>
     </li>
     <?php endforeach ?>
