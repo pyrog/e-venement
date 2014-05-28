ef9612f8a1ba8ffddc40e57a6adb8a6d55b98976
beta <betaglop@users.noreply.github.com>
2014-05-28 09:25:23 +0000

    (rp) correcting the security credentials for filters

--- lib/filter/doctrine/ContactFormFilter.class.php
+++ lib/filter/doctrine/ContactFormFilter.class.php
@@ -158,8 +158,7 @@ class ContactFormFilter extends BaseContactFormFilter
     $this->widgetSchema   ['workspaces_list'] = new sfWidgetFormDoctrineChoice(array(
       'model' => 'Workspace',
       'query' => Doctrine::getTable('Workspace')->createQuery('ws')
-        ->leftJoin('ws.Users u')
-        ->andWhere('ws.id = ?',sfContext::getInstance()->getUser()->getId()),
+        ->andWhereIn('ws.id', array_keys(sfContext::getInstance()->getUser()->getWorkspacesCredentials())),
       'order_by' => array('name',''),
       'multiple' => true,
     ));
