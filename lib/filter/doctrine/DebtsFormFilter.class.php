b47d17c25e8c01d44d44a66d19d79626520b1346
beta <betaglop@users.noreply.github.com>
2014-07-15 09:35:59 +0000

    (tck) improving the debts' filters, converting the "date" filter, to a range of dates ("from xxx to yyy")

--- lib/filter/doctrine/DebtsFormFilter.class.php
+++ lib/filter/doctrine/DebtsFormFilter.class.php
@@ -15,6 +15,7 @@ class DebtsFormFilter extends TransactionFormFilter
    */
   public function configure()
   {
+    sfContext::getInstance()->getConfiguration()->loadHelpers(array('I18N'));
     $this->widgetSchema   ['date'] = new liWidgetFormDateText(array('culture' => sfContext::getInstance()->getUser()->getCulture()));
     $this->validatorSchema['date'] = new sfValidatorDate(array(
       'required' => false,
@@ -27,6 +28,26 @@ class DebtsFormFilter extends TransactionFormFilter
       'required'  => false,
     ));
     
+    $this->widgetSchema   ['date'] = new sfWidgetFormFilterDate(array(
+      'from_date' => new liWidgetFormJQueryDateText(array('culture' => sfContext::getInstance()->getUser()->getCulture())),
+      'to_date'   => new liWidgetFormJQueryDateText(array('culture' => sfContext::getInstance()->getUser()->getCulture())),
+      'template'  => '<span class="from">'.__('From %from_date%').'</span> <span class="to">'.__('to %to_date%').'</span>',
+      'with_empty'=> false,
+    ));
+    $this->validatorSchema['date'] = new sfValidatorDateRange(array(
+      'from_date'     => new sfValidatorDate(array(
+        'required'    => false,
+        'date_output' => 'Y-m-d',
+        'with_time'   => false,
+      )),
+      'to_date'       => new sfValidatorDate(array(
+        'required'    => false,
+        'date_output' => 'Y-m-d',
+        'with_time'   => false,
+      )),
+      'required' => false,
+    ));
+    
     parent::configure();
   }
   
@@ -34,12 +55,12 @@ class DebtsFormFilter extends TransactionFormFilter
   {
     $a = $q->getRootAlias();
     
-    if ( $values )
+    if ( is_array($values) && $values )
     {
       TransactionTable::addDebtsListBaseSelect($q);
-      $q->addSelect('(SELECT SUM(tck.value)  FROM Ticket tck  WHERE '.TransactionTable::getDebtsListTicketsCondition('tck',$values).') AS outcomes')
-        ->addSelect("(SELECT SUM(pp.value)   FROM Payment pp  WHERE pp.transaction_id = t.id AND pp.created_at < '".$values."') AS incomes")
-        ->where('((SELECT (CASE WHEN COUNT(tck3.id) = 0 THEN 0 ELSE SUM(tck3.value) END) FROM Ticket tck3 WHERE '.TransactionTable::getDebtsListTicketsCondition('tck3',$values).') - (SELECT (CASE WHEN COUNT(p3.id) = 0 THEN 0 ELSE SUM(p3.value) END) FROM Payment p3 WHERE p3.transaction_id = t.id AND p3.created_at < ?)) != 0',$values); // not using andWhere is important to remove the "normal" condition before adding this one
+      $q->addSelect('(SELECT SUM(tck.value)  FROM Ticket tck  WHERE '.TransactionTable::getDebtsListTicketsCondition('tck', $values['to'], $values['from']).') AS outcomes')
+        ->addSelect("(SELECT SUM(pp.value)   FROM Payment pp  WHERE pp.transaction_id = t.id AND pp.created_at < '".$values['to']."' AND pp.created_at > '".$values['from']."') AS incomes")
+        ->andWhere('((SELECT (CASE WHEN COUNT(tck3.id) = 0 THEN 0 ELSE SUM(tck3.value) END) FROM Ticket tck3 WHERE '.TransactionTable::getDebtsListTicketsCondition('tck3', $values['to'], $values['from']).') - (SELECT (CASE WHEN COUNT(p3.id) = 0 THEN 0 ELSE SUM(p3.value) END) FROM Payment p3 WHERE p3.transaction_id = t.id AND p3.created_at < ? AND p3.created_at > ?)) != 0', array($values['to'], $values['from'])); // not using andWhere is important to remove the "normal" condition before adding this one
     }
     
     return $q;
