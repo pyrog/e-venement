d9de20288167ef5db480b7fa68e7ecf017bc51c8
beta <betaglop@users.noreply.github.com>
2014-07-16 13:39:07 +0000

    (grp) adding the events list on "by contact" list (grp) improving data extraction from the "by event" module (grp) adding an extraction feature that allows date-to-date extractions

---
+++ apps/grp/modules/professional/templates/_list_events.php
@@ -0,0 +1,18 @@
+<?php
+  $q = Doctrine::getTable('Event')->createQuery('e')
+    ->leftJoin('e.Manifestations m')
+    ->leftJoin('m.ManifestationEntries mme')
+    ->leftJoin('mme.Entries meee') // EntryElement
+    ->leftJoin('meee.ContactEntry ce')
+    ->andWhere('ce.professional_id = ?', $professional->id)
+    ->andWhere('meee.accepted = ?', true)
+    ->andWhereIn('e.meta_event_id', array_keys($sf_data->getRaw('sf_user')->getMetaEventsCredentials()))
+    ->select('e.*')
+    ->orderBy('e.name');
+  $events = $q->execute();
+?>
+<ul>
+<?php foreach ( $events as $event ): ?>
+  <li><?php echo cross_app_link_to($event, 'event', 'event/show?id='.$event->id) ?>
+<?php endforeach ?>
+</ul>
