4362767ef5314520d00104a7b6b33dc7bbacf069
beta <betaglop@users.noreply.github.com>
2014-07-21 10:59:44 +0000

    (default) making possible to setup the number of objects displaid, and the interval where to look for them

--- apps/default/modules/default/actions/actions.class.php
+++ apps/default/modules/default/actions/actions.class.php
@@ -17,25 +17,32 @@ class defaultActions extends sfActions
   */
   public function executeIndex(sfWebRequest $request)
   {
+    $interval = sfConfig::get('app_display_last_hours',24*30);
+    $max = sfConfig::get('app_display_max', 5);
+    
     $q = Doctrine::getTable('Email')->createQuery('e');
     $q->orderBy("e.updated_at DESC, e.created_at DESC")
-      ->limit(5);
+      ->andWhere("e.updated_at > NOW() - '$interval hours'::interval")
+      ->limit($max);
     $this->emails = $q->execute();
     
     $q = Doctrine::getTable('Contact')->createQuery('c');
     $q->orderBy("c.updated_at DESC, c.created_at DESC")
-      ->limit(5);
+      ->andWhere("c.updated_at > NOW() - '$interval hours'::interval")
+      ->limit($max);
     $this->contacts = $q->execute();
     
     $q = Doctrine::getTable('Organism')->createQuery('o');
     $q->orderBy("o.updated_at DESC, o.created_at DESC")
-      ->limit(5);
+      ->andWhere("o.updated_at > NOW() - '$interval hours'::interval")
+      ->limit($max);
     $this->organisms = $q->execute();
     
     $q = Doctrine::getTable('Manifestation')->createQuery('m');
     $q->orderBy("m.happens_at")
+      ->andWhere("m.updated_at > NOW() - '$interval hours'::interval")
       ->andWhere("m.happens_at > now()")
-      ->limit(5);
+      ->limit($max);
     $this->manifestations = $q->execute();
   }
   
