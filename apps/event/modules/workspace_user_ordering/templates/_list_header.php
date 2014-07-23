b892462f2efa575457d49ea4ee179db1fbc78fa6
beta <betaglop@users.noreply.github.com>
2014-07-23 07:43:36 +0000

    (event) adding templates for filters in calendar (event) adding a comment on workspace user ordering module (tck) adding the template for the "deposit" feature

---
+++ apps/event/modules/workspace_user_ordering/templates/_list_header.php
@@ -0,0 +1,6 @@
+<div class="ui-widget-content ui-corner-all">
+  <ul>
+    <li><?php echo __("If only one workspace's order is less than 0 (be careful, try to have only one workspace like this), then this workspace's gauges will be displaid alone in the mini-gauges, replacing the merging gauge.") ?></li>
+    <li><?php echo __("If only one workspace's order is more than 999, then the displaid mini-gauges will be based on profitability, in relation with the expected income and what is really being realized on every manifestation (only in the case where the expected income is given).") ?></li>
+  </ul>
+</div>
