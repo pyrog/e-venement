c28d701f2d6349036184d04c5bf55f78088958b0
beta <betaglop@users.noreply.github.com>
2014-07-25 11:26:06 +0000

    (grp) improving the averages of the professional module's list -IMPORT FROM v2.6-

--- apps/grp/modules/professional/templates/_nb_tickets_notfree.php
+++ apps/grp/modules/professional/templates/_nb_tickets_notfree.php
@@ -2,10 +2,12 @@
 <?php
   $q = Doctrine_Query::create()->from('EntryTickets et')
     ->select('et.*, ee.id, me.id')
+    ->leftJoin('et.Price p')
     ->leftJoin('et.EntryElement ee')
     ->leftJoin('ee.ContactEntry ce')
     ->leftJoin('ee.ManifestationEntry me')
     ->leftJoin('me.Manifestation m')
+    ->leftJoin('p.PriceManifestations pm ON p.id = pm.price_id AND m.id = pm.manifestation_id')
     ->leftJoin('m.Event e')
     ->andWhereIn('e.meta_event_id',array_keys($sf_data->getRaw('sf_user')->getMetaEventsCredentials()))
     ->andWhere('ce.professional_id = ?',$professional->id)
@@ -17,7 +19,13 @@
   foreach ( $q->execute() as $tickets )
   {
     $mids[$tickets->EntryElement->ManifestationEntry->id] = 1;
-    $nb += $tickets->quantity;
+    foreach ( $tickets->Price->PriceManifestations as $pm )
+    if ( $pm->manifestation_id == $tickets->EntryElement->ManifestationEntry->manifestation_id
+      && $pm->value > 0 )
+    {
+      $nb += $tickets->quantity;
+      break;
+    }
   }
   
   echo format_number(round($nb/count($mids),1));
