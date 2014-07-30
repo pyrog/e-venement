7e5382a0fc5413cab5abded9a1ca12b0c2049422
beta <betaglop@users.noreply.github.com>
2014-07-30 21:53:29 +0000

    (ws) applying the new feature for batchUpdateIndexes on tables that has I18n + Searchable behaviors (event) applying the new feature for batchUpdateIndexes on the Event table

--- lib/model/doctrine/packages/ws/PluginSurveyQuery.class.php
+++ lib/model/doctrine/packages/ws/PluginSurveyQuery.class.php
@@ -29,5 +29,9 @@ abstract class PluginSurveyQuery extends BaseSurveyQuery
     // adding a "lang" column to EventVersion to be able to record the changes in the i18n data
     $this->getTable()->getTemplate('Versionable')->getAuditLog()
       ->hasColumn('lang', 'string', 2, array('fixed' => true, 'primary' => true));
+    
+    // searchable
+    $tpl = $this->getTable()->getTemplate('Searchable');
+    $tpl->getListener()->set('Searchable', new Doctrine_Search_Listener_I18n($tpl->getPlugin()));
   }
 }
