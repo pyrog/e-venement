c72436c262cfc956c77967dfc069f4e8d13112b9
beta <betaglop@users.noreply.github.com>
2014-07-31 14:21:58 +0000

    (event) correcting some hidden bugs in manifestations "spectators" fieldset -BACKPORT FROM v2.8-

--- apps/event/modules/manifestation/templates/_show_spectators_list_line.php
+++ apps/event/modules/manifestation/templates/_show_spectators_list_line.php
@@ -1,22 +1,28 @@
     <?php
       $arr = array();
-      foreach ( $transac->Contact->YOBs as $yob )
-        $arr[] = (string)$yob;
-      foreach ( $transac->Contact->Phonenumbers as $pn )
-        $arr[] = (string)$pn;
+      if ( $transac->contact_id )
+      {
+        foreach ( $transac->Contact->YOBs as $yob )
+          $arr[] = (string)$yob;
+        foreach ( $transac->Contact->Phonenumbers as $pn )
+          $arr[] = (string)$pn;
+      }
     ?>
-    <td class="name" title="<?php echo implode(', ', $arr) ?>"><?php echo cross_app_link_to($transac->Contact,'rp','contact/show?id='.$transac->contact_id) ?> <span class="pictos"><?php if ( is_object($transac->Contact) ) echo $sf_data->getRaw('transac')->Contact->groups_picto ?></span></td>
-    <td class="pro-groups"><?php echo $sf_data->getRaw('transac')->Professional->groups_picto ?></td>
+    <td class="name" title="<?php echo implode(', ', $arr) ?>"><?php echo cross_app_link_to($transac->Contact,'rp','contact/show?id='.$transac->contact_id) ?> <span class="pictos"><?php if ( $transac->contact_id ) echo $sf_data->getRaw('transac')->Contact->groups_picto ?></span></td>
+    <td class="pro-groups"><?php echo $sf_data->getRaw('transac')->professional_id ? $sf_data->getRaw('transac')->Professional->groups_picto : '' ?></td>
     <?php
       $arr = array();
-      if ( trim($transac->Professional->contact_number) )
-        $arr[] = __('Direct phonenumber').': '.$transac->Professional->contact_number;
-      foreach ( $transac->Professional->Organism->Phonenumbers as $pn )
-        $arr[] = (string)$pn;
+      if ( $transac->professional_id )
+      {
+        if ( trim($transac->Professional->contact_number) )
+          $arr[] = __('Direct phonenumber').': '.$transac->Professional->contact_number;
+        foreach ( $transac->Professional->Organism->Phonenumbers as $pn )
+          $arr[] = (string)$pn;
+      }
     ?>
     <td class="organism" title="<?php echo implode(', ',$arr) ?>">
       <?php if ( $contact['pro'] ) echo cross_app_link_to($contact['pro']->Organism,'rp','organism/show?id='.$contact['pro']->Organism->id) ?>
-      <?php echo $sf_data->getRaw('transac')->Professional->Organism->groups_picto ?>
+      <?php echo $sf_data->getRaw('transac')->professional_id ? $sf_data->getRaw('transac')->Professional->Organism->groups_picto : '' ?>
     </td>
     <td class="tickets"><?php include_partial('show_spectators_list_tickets',array('tickets' => $ws, 'show_workspaces' => $show_workspaces)) ?></td>
     <td class="price"><?php echo format_currency($contact['value'][$wsid],'€') ?></td>
