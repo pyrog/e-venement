b50f6473e4204d31b5267454a7407ca123f1500d
beta <betaglop@users.noreply.github.com>
2014-08-07 17:58:58 +0000

    (srv) adding a credential control on menu

--- apps/templates/_menu_setup_online.php
+++ apps/templates/_menu_setup_online.php
@@ -30,7 +30,9 @@
       <?php if ( $sf_user->hasCredential('ws-group') ): ?>
         <li><a href="<?php echo cross_app_url_for('ws','auto_group') ?>"><?php echo __('Group association',array(),'menu') ?></a></li>
       <?php endif ?>
+      <?php if ( $sf_user->hasCredential('srv-access') ): ?>
         <li class="spaced"><?php echo cross_app_link_to(__('Surveys', null, 'menu'), 'srv', 'survey/index') ?></li>
+      <?php endif ?>
     </ul>
   </li>
 <?php endif ?>
