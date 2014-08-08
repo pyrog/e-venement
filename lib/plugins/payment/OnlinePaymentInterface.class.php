b96529ce8caac5a7d6df29eae00cbe79eed9507b
beta <betaglop@users.noreply.github.com>
2014-08-08 15:33:55 +0000

    (pub) documenting the OnlinePaymentInterface to give the needed information to other developpers (pub) reorganizing the OnlinePayment classes, to create autonomous plugins (pub) add README to the OnlinePayment plugins

--- lib/plugins/payment/OnlinePaymentInterface.class.php
+++ lib/plugins/payment/OnlinePaymentInterface.class.php
@@ -25,10 +25,53 @@
 
 interface OnlinePaymentInterface
 {
+  /**
+    * public "constructor"
+    *
+    * @param Transaction $transaction     the current transaction
+    * @return OnlinePaymentInterface      object, ready to use
+    **/
   public static function create(Transaction $transaction);
+  
+  /**
+    * Find back the transaction id before the object is created, based on the bank response
+    *
+    * @param sfWebRequest $parameters     the request object made out of the bank response
+    * @return integer                     the Transaction->id
+    **/
   public static function getTransactionIdByResponse(sfWebRequest $parameters);
+  
+  /**
+    * Deal with the bank response, records a BankPayment, and returns if the payment is validated or not
+    *
+    * @param sfWebRequest $parameters     the request object made out of the bank response
+    * @return array                       array('success' => false, 'amount' => $bank->amount/100) at least
+    **/
   public function response(sfWebRequest $parameters);
+  
+  /**
+    * Renders the bank request, it can be a <form> or a <a> HTML object
+    *   if you want your anchor to be directly followed, add it a "autofollow" class
+    *   if you want your form to be directly submitted, add it a "autosubmit" class
+    *
+    * @param array                        $attributes to add to the main HTML object generated
+    * @return string                      an HTML string to display as a bank request
+    **/
   public function render(array $attributes = array());
+  
+  /**
+    * Creates a BankPayment corresponding to the bank response, does not save it in the DB
+    *
+    * @param sfWebRequest $parameters     the request object made out of the bank response
+    * @return BankPayment                 the BankPayment freshly created
+    **/
   public function createBankPayment(sfWebRequest $request);
+  
+  /**
+    * Gives a string representation of the payment request
+    *   It is a standard way to call render() without a headache
+    *
+    * @return string                      an HTML string to display as a bank request
+    **/
   public function __toString();
 }
