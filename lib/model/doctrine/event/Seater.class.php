cc26e49d5e17181be239b9bede245d812b8356c6
beta <betaglop@users.noreply.github.com>
2014-08-18 16:34:42 +0000

    (event) improving the Seater

--- lib/model/doctrine/event/Seater.class.php
+++ lib/model/doctrine/event/Seater.class.php
@@ -29,24 +29,61 @@ class Seater
   
   public function __construct($gauge_id)
   {
-    $this->kept = new Doctrine_Collection(Doctrine::getTable('Seat'));
-    $this->done = new Doctrine_Collection(Doctrine::getTable('Seat'));
-    $this->query = Doctrine::getTable('Seat')->createQuery('s')
-      ->select('s.*, n.*')
-      ->leftJoin('s.SeatedPlan sp')
+    $this->kept = new Doctrine_Collection('Seat');
+    $this->done = new Doctrine_Collection('Seat');
+    $this->gauge_id = $gauge_id;
+    $this->seats = $this->createQuery()->execute();
+  }
+  
+  public function createQuery($alias = 's')
+  {
+    return Doctrine::getTable('Seat')->createQuery($alias)
+      ->select("$alias.*, n.*")
+      ->leftJoin("$alias.SeatedPlan sp")
       ->leftJoin('sp.Workspaces spw')
       ->leftJoin('spw.Gauge g')
-      ->andWhere('g.id = ?', $this->gauge_id = $gauge_id)
+      ->andWhere('g.id = ?', $this->gauge_id)
       ->leftJoin('g.Manifestation m')
       
-      ->leftJoin('s.Tickets tck WITH tck.manifestation_id = m.id')
+      ->leftJoin("$alias.Tickets tck WITH tck.manifestation_id = m.id")
       ->andWhere('tck.id IS NULL')
 
-      ->leftJoin('s.Neighbors n')
+      ->leftJoin("$alias.Neighbors n")
       
-      ->orderBy('s.rank, s.name')
+      ->orderBy("$alias.rank, $alias.name")
     ;
-    $this->seats = $this->query->execute();
+  }
+  
+  /**
+    * Find seats for $qty tickets
+    * @param $qty     integer             how many seats do you need
+    * @param $exclude Doctrine_Collection if you want to avoid looking for seats in that direction... Doctrine_Collection with the association Seat->id => Seat
+    * @return         Doctrine_Collection the Seats we have found
+    *
+    **/
+  public function findSeats($qty, Doctrine_Collection $exclude = NULL)
+  {
+    if ( !$exclude )
+      $exclude = new Doctrine_Collection('Seat');
+    $this->done = $exclude;
+    
+    foreach ( $this->seats as $seat )
+    {
+      $this->_findSeatsWalk($seat);
+      if ( $this->kept->count() >= intval($qty) )
+        break;
+      $this->kept = new Doctrine_Collection('Seat');
+    }
+    
+    $i = 0;
+    foreach ( $this->kept as $key => $seat )
+    {
+      if ( $i >= intval($qty) )
+        unset($this->kept[$key]);
+      $i++;
+    }
+    
+    return $this->kept;
   }
   
   /**
@@ -57,9 +94,11 @@ class Seater
     **/
   public function findOrphansWith($seats = NULL)
   {
+    $orphans = new Doctrine_Collection(Doctrine::getTable('Seat'));
+    
     if ( is_string($seats) )
     {
-      $this->kept = new Doctrine_Collection(Doctrine::getTable('Seat'));
+      $this->kept = new Doctrine_Collection('Seat');
       foreach ( $this->seats as $key => $seat )
       if ( $seat->name == $seats )
       {
@@ -73,30 +112,36 @@ class Seater
     else
       throw new liSeatedException('If you want to look for orphans, you have to tell correctly the Seater what to look for.');
     
+    try {
     foreach ( $this->kept as $seat )
-      $seats->merge($this->_findOrphansWithWalk($seat));
+      $orphans->merge($this->_findOrphansWithWalk($seat));
+    }
+    catch ( Exception $e )
+    {
+      echo $seats->getTable()->getTableName();
+      echo "\n";
+      echo $e->getMessage();
+    }
     
-    return $seats;
+    return $orphans;
   }
   
   protected function _findOrphansWithWalk(Seat $seat)
   {
-    $seats = new Doctrine_Collection(Doctrine::getTable('Seat'));
-    $impossible = true;
+    $seats = new Doctrine_Collection('Seat');
     
     foreach ( $seat->Neighbors as $n )
     {
       // check if the seat can be an orphan, at least
-      if ( isset($this->kept[$n->id])                           // if it's one of the selected seats
+      if ( in_array($n->id, $this->kept->getPrimaryKeys())      // if it's one of the selected seats
         || !in_array($n->id, $this->seats->getPrimaryKeys()) )  // if it's a seat that is already booked
         continue; // this neighbor is not a free seat, it cannot be an orphan
-      $impossible = false;
       
       $n = $this->seats[array_search($n->id, $this->seats->getPrimaryKeys())];
       $i = $n->Neighbors->count();
       foreach ( $n->Neighbors as $n2 )
       {
-        if ( isset($this->kept[$n2->id])                           // if it's one of the selected seats
+        if (  in_array($n2->id, $this->kept->getPrimaryKeys())     // if it's one of the selected seats
           || !in_array($n2->id, $this->seats->getPrimaryKeys()) )  // if it's a seat that is already booked
           $i--; // the neighbor of the neighbor is not a free seat, so the neighbor is an orphan on this side
       }
@@ -106,38 +151,6 @@ class Seater
     }
     
     return $seats;
-  }
-  
-  /**
-    * Find seats for $qty tickets
-    * @param $qty     integer             how many seats do you need
-    * @param $exclude Doctrine_Collection if you want to avoid looking for seats in that direction... Doctrine_Collection with the association Seat->id => Seat
-    * @return         Doctrine_Collection the Seats we have found
-    *
-    **/
-  public function findSeats($qty, Doctrine_Collection $exclude = NULL)
-  {
-    if ( !$exclude )
-      $exclude = new Doctrine_Collection(Doctrine::getTable('Seat'));
-    $this->done = $exclude;
-    
-    foreach ( $this->seats as $seat )
-    {
-      $this->_findSeatsWalk($seat);
-      if ( $this->kept->count() >= intval($qty) )
-        break;
-      $this->kept = new Doctrine_Collection(Doctrine::getTable('Seat'));
-    }
-    
-    $i = 0;
-    foreach ( $this->kept as $key => $seat )
-    {
-      if ( $i >= intval($qty) )
-        unset($this->kept[$key]);
-      $i++;
-    }
-    
-    return $this->kept;
   }
   
   protected function _findSeatsWalk(Seat $seat)
