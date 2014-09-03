28d367b6562e0ca195a14cbe174db940b622ac44
beta <betaglop@users.noreply.github.com>
2014-09-03 16:06:05 +0000

    (tck) changing the duplicate redirections to transaction (was ticket before) (pub) enlarging the events list max size (pub) starting to find a trick to override the P3P stupid IE's stuff (pub) displaying only the companie's name instead of (string)$cie method

--- web/pub_dev.php
+++ web/pub_dev.php
@@ -7,6 +7,7 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')))
   die('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
+header('P3P: CP="Topinambour"');
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('pub', 'dev', true);
