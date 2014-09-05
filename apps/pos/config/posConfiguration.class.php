58097b4604f6e10b34a621be185ba4deb0f13583
beta <betaglop@users.noreply.github.com>
2014-09-05 20:00:28 +0000

    (pos) going back directly to the product when a declination is created

--- apps/pos/config/posConfiguration.class.php
+++ apps/pos/config/posConfiguration.class.php
@@ -6,5 +6,15 @@ class posConfiguration extends sfApplicationConfiguration
   {
     parent::configure();
     sfConfig::set('sf_app_template_dir', sfConfig::get('sf_apps_dir') . '/templates');
+    $this->dispatcher->connect('admin.save_object', array($this, 'triggerDeclinationCreation'));
+  }
+  
+  public function triggerDeclinationCreation(sfEvent $event)
+  {
+    $params = $event->getParameters();
+    if (! $params['object'] instanceof ProductDeclination )
+      return;
+    
+    $event->getSubject()->redirect('product/edit?id='.$params['object']->product_id.'#sf_fieldset_declinations');
   }
 }
