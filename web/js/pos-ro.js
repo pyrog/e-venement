6377f363a5d760e50bb6e07b893a824099008787
beta <betaglop@users.noreply.github.com>
2014-09-08 13:51:41 +0000

    (pos) optimizing stuff about "read-only" mode for products

--- web/js/pos-ro.js
+++ web/js/pos-ro.js
@@ -5,8 +5,12 @@ if ( LI.posAfterRendering == undefined )
   LI.posAfterRendering = [];
 
 LI.posAfterRendering.push(function(){
-  $('.sf_admin_form').find('button, input, select').prop('readonly', true);
-  $('.sf_admin_form').find('.sf_admin_form_field_picture_del, .li-delete, .li-new-declination .fg-button').remove();
+  $('.sf_admin_form').find('button, input, select').prop('readonly', true).each(function(){
+    if ( !$(this).attr('readonly') )
+      $(this).prop('disabled', true);
+  });
+  $('.sf_admin_form').remove('.sf_admin_form_field_picture_del, .li-delete, .li-new-declination .fg-button')
+    .find('.sf_admin_actions_block li:not(.sf_admin_action_list)').css('visibility', 'hidden');
   $('.sf_admin_form form').submit(function(){ return false; });
   
   setTimeout(function(){
