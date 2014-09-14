6ee2d47de5ae0e9df9f725f8f4dc5ef6012483b4
beta <betaglop@users.noreply.github.com>
2014-09-14 18:44:03 +0000

    (event) adding shortcuts for seated plans (pos) ordering prices in the PricePOS list

---
+++ web/js/event-seated-plan-shortcuts.js
@@ -0,0 +1,43 @@
+/** all the keyboard shortcuts **/
+$(document).ready(function(){
+  $(document).keypress(function(e){
+    if ( $(':focus').length != 0 )
+      return;
+    if ( $('#sf_fieldset_plan').css('display') == 'none' )
+      return;
+    
+    // CTRL+[+] / CTRL+[-] : zoom-in / zoom-out
+    if ( (e.ctrlKey||e.metaKey) && e.key == '+' )
+    {
+      $('.gauge:first .magnify .magnify-in').click();
+      return false;
+    }
+    if ( (e.ctrlKey||e.metaKey) && e.key == '-' )
+    {
+      $('.gauge:first .magnify .magnify-out').click();
+      return false;
+    }
+    
+    // [+]/[-] : increase/decrease the size of seats
+    if ( e.key == '+' )
+    {
+      $('#seated_plan_seat_diameter').each(function(){
+        $(this).val(parseInt($(this).val(),10)+1);
+      });
+      return false;
+    }
+    if ( e.key == '-' )
+    {
+      $('#seated_plan_seat_diameter').each(function(){
+        $(this).val(parseInt($(this).val(),10)-1);
+      });
+      return false;
+    }
+    
+    if ( e.key == '*' )
+    {
+      $('.gauge .show-all input').click();
+      return false;
+    }
+  });
+});
