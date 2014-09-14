cc080dcc940ffeef8289ecf8237be65eb3847966
beta <betaglop@users.noreply.github.com>
2014-09-14 19:30:28 +0000

    (pos) differentiating filters of pos/price module from those of event/price module

--- apps/pos/modules/price/actions/actions.class.php
+++ apps/pos/modules/price/actions/actions.class.php
@@ -13,4 +13,43 @@ require_once dirname(__FILE__).'/../lib/priceGeneratorHelper.class.php';
  */
 class priceActions extends autoPriceActions
 {
+  /** differentiating filters from the event/price module **/
+  protected function getFilters()
+  {
+    return $this->getUser()->getAttribute('pos.price.filters', $this->configuration->getFilterDefaults(), 'admin_module');
+  }
+  protected function setFilters(array $filters)
+  {
+    return $this->getUser()->setAttribute('pos.price.filters', $filters, 'admin_module');
+  }
+  protected function setPage($page)
+  {
+    $this->getUser()->setAttribute('pos.price.page', $page, 'admin_module');
+  }
+
+  protected function getPage()
+  {
+    return $this->getUser()->getAttribute('pos.price.page', 1, 'admin_module');
+  }
+  protected function getSort()
+  {
+    if (!is_null($sort = $this->getUser()->getAttribute('pos.price.sort', null, 'admin_module')))
+    {
+      return $sort;
+    }
+
+    $this->setSort($this->configuration->getDefaultSort());
+
+    return $this->getUser()->getAttribute('pos.price.sort', null, 'admin_module');
+  }
+
+  protected function setSort(array $sort)
+  {
+    if (!is_null($sort[0]) && is_null($sort[1]))
+    {
+      $sort[1] = 'asc';
+    }
+
+    $this->getUser()->setAttribute('pos.price.sort', $sort, 'admin_module');
+  }
 }
