9f286ae9be6c04ebc59cdba5934d13f87ea5f7c0
beta <betaglop@users.noreply.github.com>
2014-09-14 19:52:52 +0000

    (event) ordering PriceGauge in a way that they can be found back

--- apps/event/modules/manifestation/templates/_widget_gauges_prices.php
+++ apps/event/modules/manifestation/templates/_widget_gauges_prices.php
@@ -1,6 +1,21 @@
 <?php use_javascript('helper') ?>
 <?php use_javascript('manifestation-price-gauges') ?>
 <?php use_javascript('jquery.nicescroll.min.js') ?>
+<?php
+  // ordering prices for every gauge in the same way
+  $order = array();
+  foreach ( $manifestation->Gauges as $gauge )
+  foreach ( $gauge->PriceGauges as $pg )
+  if (!( isset($order[$pg->price_id]) && $order[$pg->price_id] > $pg->value ))
+    $order[$pg->price_id] = $pg->value;
+  arsort($order);
+  $others = array();
+  foreach ( $sf_user->getGuardUser()->Prices as $price )
+  if (!( isset($order[$price->id]) ))
+    $others[$price->id] = $price->name;
+  asort($others);
+  $order = array_keys($order + $others);
+?>
 <table>
   <body>
     <?php foreach ( $manifestation->Gauges as $gauge ): ?>
@@ -19,15 +34,17 @@
     ?>
     <tr>
       <th><?php echo $gauge ?></th>
-      <?php foreach ( $prices as $price ): ?>
-      <?php $form = new PriceGaugeForm($price); ?>
+      <?php foreach ( $order as $id ): ?>
+      <?php if ( isset($prices[$id]) ): ?>
+      <?php $form = new PriceGaugeForm($prices[$id]); ?>
         <td data-submit-url="<?php echo url_for('manifestation/addGaugePrice') ?>" title="<?php echo $gauge ?>">
-          <?php if ( in_array($gauge->workspace_id, $price->Price->Workspaces->getPrimaryKeys()) ): ?>
+          <?php if ( in_array($gauge->workspace_id, $prices[$id]->Price->Workspaces->getPrimaryKeys()) ): ?>
           <table><?php echo $form ?></table>
           <?php else: ?>
           -
           <?php endif ?>
         </td>
+      <?php endif ?>
       <?php endforeach ?>
     </tr>
     <?php endforeach ?>
@@ -35,7 +52,11 @@
   <thead>
     <tr>
       <th></th>
-      <?php foreach ( $sf_user->getGuardUser()->Prices as $price ): ?>
+      <?php $prices = array(); foreach ( $sf_user->getGuardUser()->Prices as $price ) $prices[$price->id] = $price; ?>
+      <?php foreach ( $order as $id ): ?>
+      <td><?php echo $prices[$id]; unset($prices[$id]) ?></td>
+      <?php endforeach ?>
+      <?php foreach ( $prices as $price ): ?>
       <td><?php echo $price ?></td>
       <?php endforeach ?>
     </tr>
