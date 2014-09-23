b189afc3f085fa310aa854ab25410f3a1b68e0e4
beta <betaglop@users.noreply.github.com>
2014-09-23 15:11:39 +0000

    (pub) adding a global "per contact + per manifestation" limit for sales

--- apps/pub/modules/manifestation/templates/_show_prices.php
+++ apps/pub/modules/manifestation/templates/_show_prices.php
@@ -9,6 +9,33 @@
   }
 ?>
 
+<?php
+  // limitting the max quantity, especially for prices linked to member cards
+  $vel = sfConfig::get('app_tickets_vel');
+  $vel['max_per_manifestation'] = isset($vel['max_per_manifestation']) ? $vel['max_per_manifestation'] : 9;
+  if ( $gauge->Manifestation->online_limit_per_transaction && $gauge->Manifestation->online_limit_per_transaction < $vel['max_per_manifestation'] )
+    $vel['max_per_manifestation'] = $gauge->Manifestation->online_limit_per_transaction;
+  
+  // max per manifestation per contact ...
+  $vel['max_per_manifestation_per_contact'] = isset($vel['max_per_manifestation_per_contact']) ? $vel['max_per_manifestation_per_contact'] : false;
+  if ( $vel['max_per_manifestation_per_contact'] > 0 )
+  {
+    $max = $vel['max_per_manifestation_per_contact'];
+    foreach ( $sf_user->getContact()->Transactions as $transaction )
+    if ( $transaction->id != $sf_user->getTransaction()->id )
+    foreach ( $transaction->Tickets as $ticket )
+    if (( $ticket->transaction_id == $sf_user->getTransaction()->id || $ticket->printed_at || $ticket->integrated_at || $transaction->Order->count() > 0 )
+      && !$ticket->hasBeenCancelled()
+      && $gauge->Manifestation->id == $ticket->manifestation_id
+    )
+    {
+      $vel['max_per_manifestation_per_contact']--;
+    }
+    $vel['max_per_manifestation'] = $vel['max_per_manifestation'] > $vel['max_per_manifestation_per_contact']
+      ? $vel['max_per_manifestation_per_contact']
+      : $vel['max_per_manifestation'];
+  }
+?>
 
 <table class="prices">
 <?php if ( $gauge->Manifestation->PriceManifestations->count() > 0 ): ?>
@@ -28,31 +55,30 @@
       ->setQuantity($qty);
     
     // limitting the max quantity, especially for prices linked to member cards
-    $vel = sfConfig::get('app_tickets_vel');
-    $vel['max_per_manifestation'] = isset($vel['max_per_manifestation']) ? $vel['max_per_manifestation'] : 9;
-    if ( $gauge->Manifestation->online_limit_per_transaction && $gauge->Manifestation->online_limit_per_transaction < $vel['max_per_manifestation'] )
-      $vel['max_per_manifestation'] = $gauge->Manifestation->online_limit_per_transaction;
-      
     $max = $gauge->value - $gauge->printed - $gauge->ordered - $gauge->Manifestation->online_limit - (sfConfig::get('project_tickets_count_demands',false) ? $gauge->asked : 0);
     $max = $max > $vel['max_per_manifestation'] ? $vel['max_per_manifestation'] : $max;
+    
+    // member cards
     if ( $pm->Price->member_card_linked )
     {
-      $cpt = 0;
+      $mc_max = 0;
       
       if ( isset($mcp[$pm->price_id]) )
       {
-        $cpt += $mcp[$pm->price_id][''] < 0 ? 0 : $mcp[$pm->price_id][''];
+        $mc_max += $mcp[$pm->price_id][''] < 0 ? 0 : $mcp[$pm->price_id][''];
         if ( isset($mcp[$pm->price_id][$gauge->Manifestation->event_id]) )
-          $cpt += $mcp[$pm->price_id][$gauge->Manifestation->event_id];
+          $mc_max += $mcp[$pm->price_id][$gauge->Manifestation->event_id];
       }
       
-      if ( $max > $cpt )
-        $max = $cpt;
+      if ( $max > $mc_max )
+        $max = $mc_max;
     }
     
     if ( $max <= 0 )
+    {
+      echo '<tr><td class="price" colspan="4">'.__('Price %%price%% not available', array('%%price%%' => $pm->Price)).'</td></tr>';
       continue;
-    
+    }
     $form->setMaxQuantity($max);
   ?>
   <tr>
