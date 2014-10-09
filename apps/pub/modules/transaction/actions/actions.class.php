f92fe7f3ac16e4d42de876696fbbff3f10786fc0
beta <betaglop@users.noreply.github.com>
2014-10-09 10:43:52 +0000

    (pub) increasing the qty of manifestation to display in the list to 100 (pub) ordering the cart by date before by event name -IMPORT FROM v2.7-

--- apps/pub/modules/transaction/actions/actions.class.php
+++ apps/pub/modules/transaction/actions/actions.class.php
@@ -128,7 +128,7 @@ class transactionActions extends sfActions
       ->andWhere('tck.id NOT IN (SELECT tck3.duplicating FROM Ticket tck3 WHERE tck3.duplicating IS NOT NULL)')
       ->andWhere('tck.id NOT IN (SELECT tck2.cancelling FROM Ticket tck2 WHERE tck2.cancelling IS NOT NULL)')
       ->andWhere('tck.id IS NOT NULL')
-      ->orderBy('et.name, m.happens_at, w.name, g.id, p.id, tck.id')
+      ->orderBy('m.happens_at, et.name, w.name, g.id, p.id, tck.id')
     ;
     if ( $this->getUser()->hasContact() )
       $q->andWhere('t.contact_id = ?',$this->getUser()->getContact()->id);
