62de8533f963603bcb922f00b36b3403010c9edb
beta <betaglop@users.noreply.github.com>
2014-10-28 10:37:27 +0000

    (rp) adding the bcc & cc fields for emails -IMPORT FROM v2.7-

--- apps/rp/modules/email/templates/_show_email.php
+++ apps/rp/modules/email/templates/_show_email.php
@@ -1,8 +1,10 @@
 <?php $emails = $form->getObject()->$field ?>
 <div class="sf_admin_form_row">
   <label><?php echo __($label) ?>:</label>
+  <?php if ( $emails ): ?>
   <?php foreach ( explode(',',$emails) as $email ): ?>
   <a href="mailto:<?php echo htmlspecialchars(trim($email)) ?>"><?php echo htmlspecialchars($email) ?></a>
   <?php endforeach ?>
   <span class="nb">(<?php echo count(explode(',',$emails)) ?>)</span>
+  <?php endif ?>
 </div>
