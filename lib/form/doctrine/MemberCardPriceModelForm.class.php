ac71aabd19efd4689b12a47c940da908e0cf5b88
beta <betaglop@users.noreply.github.com>
2014-10-31 11:00:05 +0000

    (mc) adding liBarcodePlugin by default to avoid bugs with member cards (mc) correcting a i18n bug on events, for the link between member cards & prices...

--- lib/form/doctrine/MemberCardPriceModelForm.class.php
+++ lib/form/doctrine/MemberCardPriceModelForm.class.php
@@ -23,7 +23,7 @@ class MemberCardPriceModelForm extends BaseMemberCardPriceModelForm
     $this->widgetSchema   ['price_id']->setOption('query',$q = Doctrine::getTable('Price')->createQuery('p')->andWhere('p.member_card_linked = true'));
     $this->validatorSchema['price_id']->setOption('query',$q);
     
-    $this->widgetSchema   ['event_id']->setOption('order_by',array('name',''));
+    $this->widgetSchema   ['event_id']->setOption('order_by',array('translation.name',''));
     $this->widgetSchema   ['event_id']->setOption('query',$q = Doctrine::getTable('Event')->createQuery('e')->andWhereIn('e.meta_event_id',array_keys(sfContext::getInstance()->getUser()->getMetaEventsCredentials())));
     $this->validatorSchema['event_id']->setOption('query',$q);
   }
