53d24069138954b8954cea54072b35c782a37846
beta <betaglop@users.noreply.github.com>
2014-11-03 15:34:10 +0000

    (pub) optimizing the helper.js code (global) optimizing the display of "success" flashes

--- web/js/helper.js
+++ web/js/helper.js
@@ -2,31 +2,6 @@
 if ( LI == undefined )
   var LI = {};
 
-// display a flash for a limited time
-LI.alert = function(msg, type, time)
-{
-  if ( time == undefined )
-    time = 4000;
-  if ( type == undefined )
-    type = 'notice';
-  
-  var icons = {
-    success: 'ui-icon-circle-check',
-    notice:  'ui-icon-info',
-    error:   'ui-icon-alert',
-  }
-  var flash = '<div class="%%type%% ui-state-%%type%% ui-state-highlight ui-corner-all"><span class="ui-icon %%icon%% floatleft"></span>&nbsp;%%msg%%</div>';
-  
-  $('.sf_admin_flashes').append($(flash
-    .replace('%%msg%%',msg)
-    .replace('%%type%%',type).replace('%%type%%',type)
-    .replace('%%icon%%',icons[type])
-  ).hide().css('position', 'absolute').css('z-index', 10).fadeIn('slow'));
-  setTimeout(function(){
-    $('.sf_admin_flashes > *').fadeOut(function(){ $(this).remove(); })
-  },time);
-}
-
 LI.pad_num = function (num, digits){return(1e15+num+"").slice(-digits)}
 
 // THE CURRENCY
