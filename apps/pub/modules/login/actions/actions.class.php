4caa58cdda5057a9a6909a908a6481bc4324ed00
beta <betaglop@users.noreply.github.com>
2014-11-09 09:42:31 +0000

    (pub) submitting the current browser language as a default language at the login time

--- apps/pub/modules/login/actions/actions.class.php
+++ apps/pub/modules/login/actions/actions.class.php
@@ -25,6 +25,8 @@ class loginActions extends sfActions
   
   public function executeIndex(sfWebRequest $request)
   {
+    $this->getUser()->setDefaultCulture($request->getLanguages());
+    
     $this->register = $request->hasParameter('register');
     $this->form = new LoginForm;
     
