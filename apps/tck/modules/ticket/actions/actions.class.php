e8a193afda97046349bb78ad44e0dded7905b240
beta <betaglop@users.noreply.github.com>
2014-11-12 10:04:12 +0000

    (tck) removing a trailing debug code for orders

--- apps/tck/modules/ticket/actions/actions.class.php
+++ apps/tck/modules/ticket/actions/actions.class.php
@@ -227,8 +227,6 @@ class ticketActions extends sfActions
     $accounting->content = $request->getParameter('content');
     
     $accounting->save();
-    error_log(print_r($accounting->toArray(),true));
-    throw new sfException('here');
     return sfView::NONE;
   }
   // invoice
