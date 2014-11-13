ee2902df2df8fae1264ca6d4e2abda30907f144e
beta <betaglop@users.noreply.github.com>
2014-11-13 13:11:57 +0000

    (tck) correcting a bug on global printing with no manifestation_id given

--- apps/tck/modules/ticket/actions/print.php
+++ apps/tck/modules/ticket/actions/print.php
@@ -90,7 +90,7 @@
             }
             elseif ( strcasecmp($ticket->price_name,trim($request->getParameter('price_name'))) == 0
               && $ticket->printed_at
-              && $ticket->manifestation_id == $request->getParameter('manifestation_id') )
+              && !($request->getParameter('manifestation_id') && $ticket->manifestation_id != $request->getParameter('manifestation_id')) )
             {
               $cpt++;
               $newticket = $ticket->copy();
@@ -117,7 +117,7 @@
           
           else // not duplicates
           if ( !$ticket->printed_at && !$ticket->integrated_at
-            && $ticket->manifestation_id == $request->getParameter('manifestation_id') )
+            && !($request->getParameter('manifestation_id') && $ticket->manifestation_id != $request->getParameter('manifestation_id')) )
           {
             if ( $cpt >= $max['print'] )
             {
@@ -167,7 +167,7 @@
             }
             elseif ( strcasecmp($ticket->price_name,trim($request->getParameter('price_name'))) == 0
               && $ticket->printed_at
-              && $ticket->manifestation_id == $request->getParameter('manifestation_id') )
+              && !($request->getParameter('manifestation_id') && $ticket->manifestation_id != $request->getParameter('manifestation_id')) )
             {
               $cpt++;
               $newticket = $ticket->copy();
@@ -194,7 +194,7 @@
             }
             
             if ( !$ticket->printed_at && !$ticket->integrated_at
-              && $ticket->manifestation_id == $request->getParameter('manifestation_id') )
+              && !($request->getParameter('manifestation_id') && $ticket->manifestation_id != $request->getParameter('manifestation_id')) )
             {
               $cpt++;
               if ( $ticket->Manifestation->no_print )
