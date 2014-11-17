3f30705025468ea71cde46a1c6ccf52551364621
beta <betaglop@users.noreply.github.com>
2014-11-17 17:30:30 +0000

    (tck) improving the ticket ledger, adding seats...

--- apps/tck/modules/ticket/templates/_show_version_detail.php
+++ apps/tck/modules/ticket/templates/_show_version_detail.php
@@ -3,4 +3,5 @@
 <td class="user"><?php echo $version->user ?></td>
 <td class="date"><?php echo format_datetime($version->updated_at) ?></td>
 <td class="price_name"><?php echo $version->price_name ?></td>
+<td class="seat_name"><?php echo $version->seat_id ? Doctrine::getTable('Seat')->find($version->seat_id) : '-' ?></td>
 <td class="printed"><?php echo image_tag( $version->printed_at || $version->integrated_at ? '/sfDoctrinePlugin/images/tick.png' : '/sfDoctrinePlugin/images/delete.png') ?></td>
--- apps/tck/modules/ticket/templates/_show_version_header.php
+++ apps/tck/modules/ticket/templates/_show_version_header.php
@@ -1,5 +1,6 @@
 <td class="version"><?php echo __('Version') ?></td>
 <td class="user"><?php echo __('User') ?></td>
 <td class="date"><?php echo __('Updated at') ?></td>
+<td class="seat_name"><?php echo __('Seat') ?></td>
 <td class="price_name"><?php echo __('Price name') ?></td>
 <td class="printed"><?php echo __('Printed or integrated') ?></td>
