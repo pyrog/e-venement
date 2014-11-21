48b30638925d97a63f3f9ecb7e38f0e2a501dbaa
beta <betaglop@users.noreply.github.com>
2014-11-21 17:24:29 +0000

    (tck) correcting the detailed ledger for the "taxes" widget

--- apps/tck/modules/ledger/actions/both-taxes.php
+++ apps/tck/modules/ledger/actions/both-taxes.php
@@ -29,9 +29,10 @@
       ->leftJoin('g.Workspace ws')
       ->leftJoin('t.Users u')
       ->leftJoin('t.Prices p')
-      ->leftJoin('p.Tickets tck ON (tck.price_id = p.id OR tck.sf_guard_user_id = u.id OR tck.gauge_id = g.id) AND tck.taxes != 0')
       ->orderBy('t.name')
     ;
+    $where = '(%%tck%%.price_id = p.id OR %%tck%%.sf_guard_user_id = u.id OR %%tck%%.gauge_id = g.id) AND %%tck%%.taxes != 0 AND (%%tck%%.printed_at IS NOT NULL OR %%tck%%.integrated_at IS NOT NULL) AND %%tck%%.duplicating IS NULL AND %%tck%%.id NOT IN (SELECT c_%%tck%%.cancelling FROM Ticket c_%%tck%% WHERE c_%%tck%%.cancelling IS NOT NULL)';
+    
     if ( isset($criterias['manifestations']) && is_array($criterias['manifestations']) && count($criterias['manifestations']) > 0 )
       $q->andWhereIn('m.id', $criterias['manifestations']);
     else
@@ -39,10 +40,7 @@
       if ( isset($criterias['workspaces']) && is_array($criterias['workspaces']) && count($criterias['workspaces']) > 0 )
         $q->andWhereIn('ws.id', $criterias['workspaces']);
       
-      $q->andWhere('tck.printed_at >= ? AND tck.printed_at < ? OR tck.integrated_at >= ? AND tck.integrated_at < ? OR tck.id IS NULL',array(
-        $dates[0], $dates[1],
-        $dates[0], $dates[1],
-      ));
+      $where .= " AND (%%tck%%.printed_at >= '".$dates[0]."' AND %%tck%%.printed_at < '".$dates[1]."' OR %%tck%%.integrated_at >= '".$dates[0]."' AND %%tck%%.integrated_at < '".$dates[1]."')";
     }
     
     // restrict access to our own user
@@ -52,7 +50,7 @@
       $q->andWhereIn('tck.sf_guard_user_id',$criterias['users']);
     
     // optimizing stuff
-    $q->select('t.id, t.name, t.type, t.value, count(tck.id) AS qty, sum(tck.taxes) AS amount')
-      ->groupBy('t.id, t.name, t.type, t.value')
-      ->orderBy('count(tck.id) DESC, t.name, t.type, t.value');
+    $q->select('t.id, t.name, t.type, t.value, (SELECT count(tck1.id) FROM Ticket tck1 WHERE '.str_replace('%%tck%%', 'tck1', $where).') AS qty, (SELECT SUM(tck2.taxes) FROM Ticket tck2 WHERE '.str_replace('%%tck%%', 'tck2', $where).' AND (tck2.value > 0 OR tck2.taxes > 0)) AS amount')
+      //->groupBy('t.id, t.name, t.type, t.value')
+      ->orderBy('(SELECT count(tck3.id) FROM Ticket tck3 WHERE '.str_replace('%%tck%%', 'tck3', $where).') DESC, t.name, t.type, t.value');
     $this->taxes = $q->execute();
