ea1084aa0bbe29d20c4a37c5fd66172be24a889a
beta <betaglop@users.noreply.github.com>
2014-11-27 11:14:28 +0000

    (event) adding a comment for manifestation's stats

--- apps/event/modules/manifestation/templates/_show_stats_gauges.php
+++ apps/event/modules/manifestation/templates/_show_stats_gauges.php
@@ -1,5 +1,9 @@
     <div class="gauges sf_admin_list">
       <h4><?php echo __('Seats') ?></h4>
+      <div class="help">
+        <span class="ui-icon ui-icon-help floatleft"></span>
+        <?php echo __('This only concerns seated gauges and seats. It is not about global free tickets.') ?>
+      </div>
       <table>
         <tbody>
           <tr class="sf_admin_row ui-widget-content odd online"><th class="ui-state-default ui-th-column"><?php echo __('Available for online sales') ?></th><td></td></tr>
