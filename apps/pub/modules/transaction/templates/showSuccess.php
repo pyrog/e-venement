bd6f29b2104d4681297c2c68ee1311e3504eae8b
beta <betaglop@users.noreply.github.com>
2014-12-15 15:55:16 +0000

    (pub) adding a missing class in the command resume (table header / extra-taxes)

--- apps/pub/modules/transaction/templates/showSuccess.php
+++ apps/pub/modules/transaction/templates/showSuccess.php
@@ -189,7 +189,7 @@ $(document).ready(function(){
     <?php endif ?>
     <td class="qty"><?php echo __('Qty') ?></td>
     <td><?php echo sfConfig::get('app_options_synthetic_plans', false) ? '' : __('Total') ?></td>
-    <td title="<?php echo __('Booking fees') ?>"><?php echo __('Ticketting fees') ?></td>
+    <td class="extra-taxes" title="<?php echo __('Booking fees') ?>"><?php echo __('Ticketting fees') ?></td>
     <?php if ( sfConfig::get('app_options_synthetic_plans', false) && $current_transaction ): ?>
     <td class="linked-stuff"><?php echo __('Options') ?></td>
     <?php endif ?>
