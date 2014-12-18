25e54624cc3e25edc52cb25c520df8e6a6a313ea
beta <betaglop@users.noreply.github.com>
2014-12-18 15:05:46 +0000

    (pub) adding a H1 title on the login page (display: none; by default)

--- apps/pub/modules/login/templates/indexSuccess.php
+++ apps/pub/modules/login/templates/indexSuccess.php
@@ -2,6 +2,7 @@
 <?php include_partial('global/fake_flashes', array(
   'notice' => __('This website requires your authentication'),
 )) ?>
+<h1><?php echo __('Connection', array(), 'messages') ?></h1>
 <?php include_partial('global/ariane',array('active' => 0)) ?>
 <?php echo $form->renderFormTag(url_for('login/validate'.(isset($register) && $register ? '?register=true' : '')), array('autocomplete' => 'on', 'id' => 'login', )) ?>
   <?php include_partial('login',array('form' => $form, )) ?>
