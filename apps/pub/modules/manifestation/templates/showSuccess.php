d54e5b3f439ece9db3d65a412d873c0e570e365b
beta <betaglop@users.noreply.github.com>
2015-01-13 09:43:42 +0000

    (pub) adding the manifestation_bottom texts, when using sythetic_plans option (pub) correcting the ContactPublicForm for the new flash_on_control Contact's field

--- apps/pub/modules/manifestation/templates/showSuccess.php
+++ apps/pub/modules/manifestation/templates/showSuccess.php
@@ -38,6 +38,9 @@
     </div>
   </div>
   <div class="clear"></div>
+  <div class="text_config manifestation_bottom synthetic_plans">
+    <?php echo nl2br(sfConfig::get('app_texts_manifestation_bottom')) ?>
+  </div>
   
 <?php else: ?>
   
