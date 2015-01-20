246f8910ba0698a38b2d8872a9703a95f5cd95d7
beta <betaglop@users.noreply.github.com>
2015-01-20 20:16:50 +0000

    (tck) adding QRCodes on ticket ledger, if the option is set

--- apps/tck/modules/ticket/templates/_show_ticket_transaction.php
+++ apps/tck/modules/ticket/templates/_show_ticket_transaction.php
@@ -27,5 +27,8 @@
       <td><?php echo __('Integrated') ?></td>
     </tr></thead>
   </table>
-</td>
+  <?php if ( sfConfig::get('app_tickets_id', 'id') == 'barcode' ): ?>
+  <img class="qrcode" src="data:image/jpg;base64,<?php echo base64_encode($ticket->getRawValue()->getBarcodePng()) ?>" alt="#<?php echo $ticket->id ?>" />
+  <?php endif ?>
+  </td>
 <td></td>
