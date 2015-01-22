94508e0db3960521d3f0d5237bb7cb7c02b5acab
beta <betaglop@users.noreply.github.com>
2015-01-22 15:01:34 +0000

    (pub) applying the max_per_transaction of MemberCards to the HTML widget

--- apps/pub/modules/card/templates/_index_table.php
+++ apps/pub/modules/card/templates/_index_table.php
@@ -4,7 +4,7 @@
     <tr>
       <td class="name"><?php echo $type->description ? $type->description : $type ?></td>
       <td class="value"><?php echo format_currency($type->value,'€') ?></td>
-      <td class="qty">x <input type="number" name="member_card_type[<?php echo $type->id ?>]" value="<?php echo isset($mct[$type->id]) ? $mct[$type->id] : 0 ?>" min="0" maxlength="2" /></td>
+      <td class="qty">x <input type="number" name="member_card_type[<?php echo $type->id ?>]" value="<?php echo isset($mct[$type->id]) ? $mct[$type->id] : 0 ?>" min="0" max="<?php echo sfConfig::get('app_member_cards_max_per_transaction', 3) ?>" /></td>
       <td class="operand">=</td>
       <td class="total"><?php echo format_currency(0,'€') ?></td>
     </tr>
