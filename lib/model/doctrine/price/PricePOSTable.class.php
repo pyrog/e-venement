c52ddcb8651c3a9299c0500888d54753b88f86d5
beta <betaglop@users.noreply.github.com>
2015-01-22 10:52:55 +0000

    (pos) improving stuff for Price i18n

--- lib/model/doctrine/price/PricePOSTable.class.php
+++ lib/model/doctrine/price/PricePOSTable.class.php
@@ -9,8 +9,16 @@ class PricePOSTable extends PluginPricePOSTable
 {
   public function retrieveList()
   {
-    return $this->createQuery('pos')
-      ->leftJoin('pos.Price p');
+    $q = $this->createQuery('pos')
+      ->leftJoin('pos.Price p')
+    ;
+    
+    if ( !sfContext::hasInstance() )
+      $q->leftJoin('p.Translation pt');
+    else
+      $q->leftJoin('p.Translation pt WITH pt.lang = ?', sfContext::getInstance()->getUser()->getCulture());
+    
+    return $q;
   }
   
   /**
