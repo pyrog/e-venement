635754bba7e634654025cdb53df1fcfa2279171d
beta <betaglop@users.noreply.github.com>
2015-01-23 08:31:04 +0000

    (tck) adding a char "-" for unseated tickets, in the ticket ledger

--- apps/tck/modules/ticket/templates/_show_ticket_workspace.php
+++ apps/tck/modules/ticket/templates/_show_ticket_workspace.php
@@ -1,5 +1,5 @@
 <td><?php echo __('Workspace') ?></td>
 <td><?php echo $ticket->Gauge->Workspace ?></td>
-<td title="<?php echo __('seat') ?>"><?php echo $ticket->numerotation ?></td>
+<td title="<?php echo __('seat') ?>"><?php echo $ticket->seat_id ? $ticket->numerotation : '-' ?></td>
 <td></td>
 
