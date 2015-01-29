9b24860e8fdde941601f1c4280075bf05cd29cba
beta <betaglop@users.noreply.github.com>
2015-01-29 09:30:38 +0000

    (tck) correcting the link to the Contacts in the TransactionsList

--- apps/tck/modules/transactionsList/templates/_list_contact.php
+++ apps/tck/modules/transactionsList/templates/_list_contact.php
@@ -1,1 +1,1 @@
-<?php echo cross_app_link_to($transaction->Contact, 'rp', 'contact/edit?id='.$transaction->id) ?>
+<?php echo cross_app_link_to($transaction->Contact, 'rp', 'contact/edit?id='.$transaction->contact_id) ?>
