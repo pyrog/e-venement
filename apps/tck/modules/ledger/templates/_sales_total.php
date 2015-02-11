6f2da012ba8b5eb7e69356009e86e1cef97c6cff
beta <betaglop@users.noreply.github.com>
2015-02-11 17:02:02 +0000

    (tck) adding a super total (Taxes+Total) in the sales ledger

--- apps/tck/modules/ledger/templates/_sales_total.php
+++ apps/tck/modules/ledger/templates/_sales_total.php
@@ -1,8 +1,8 @@
     <?php $local_vat = 0; ?>
-    <td class="event"><?php echo __('Total') ?></td>
+    <td class="event"><?php echo __('Total') ?> <span class="super-total"><?php echo format_currency($total['value']+$total['taxes'],'€') ?></span></td>
     <td class="see-more"></td>
     <td class="id-qty"><?php echo $total['qty'] ?></td>
-    <td class="value"><?php echo format_currency($total['value'],'€'); ?></td>
+    <td class="value"><?php echo format_currency($total['value'],'€'); ?>)</td>
     <td class="extra-taxes"><?php echo format_currency($total['taxes'],'€'); ?></td>
     <?php foreach ( $total['vat'] as $v ): ?>
     <td class="vat"><?php echo format_currency(round($v,2),'€'); $local_vat += round($v,2); ?></td>
