4718520de99654a544cfb35ea8cee9d39795827a
beta <betaglop@users.noreply.github.com>
2015-02-13 10:25:36 +0000

    (event) adding a forgotten partial...

---
+++ apps/event/modules/event/templates/_show_checkpoints_notice.php
@@ -0,0 +1,4 @@
+<div class="ui-widget-content ui-corner-all notice">
+  <p><span class="ui-icon ui-icon-help floatleft"></span><?php echo __('A checkpoint is a logical point of entrance. If you have one venue and five doors, you only have one checkpoint. If you have five venues, and one can be only in one venue at the same time, then you have five checkpoints.') ?></p>
+  <p><span class="ui-icon ui-icon-help floatleft"></span><?php echo __('If you unckeck the "legal" property of a checkpoint, one can pass through the checkpoint more than once.') ?>
+</div>
