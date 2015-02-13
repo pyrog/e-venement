bf2e7302f813320c674e5c1a871c030a9a943550
beta <betaglop@users.noreply.github.com>
2015-02-13 09:25:33 +0000

    (event) adding a notice for checkpoints

--- apps/event/modules/event/templates/_show_checkpoints.php
+++ apps/event/modules/event/templates/_show_checkpoints.php
@@ -1,5 +1,5 @@
 <ol class="show_checkpoint">
   <?php foreach ( $event->Checkpoints as $checkpoint ): ?>
-  <li><?php echo $checkpoint ?></li>
+  <li><?php echo $checkpoint ?> <?php echo $checkpoint->legal ? '('.__('Legal').')' : '' ?></li>
   <?php endforeach ?>
 </ol>
