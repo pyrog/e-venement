138af0c2443fec80420e86326c3812465e0e4650
beta <betaglop@users.noreply.github.com>
2015-02-16 18:09:25 +0000

    (tck) improving the position of the "taxes" widget in the detailed ledger

--- apps/tck/modules/ledger/templates/bothSuccess.php
+++ apps/tck/modules/ledger/templates/bothSuccess.php
@@ -40,7 +40,7 @@
 <?php include_partial('both_price',array('byPrice' => $byPrice)) ?>
 <div class="clear"></div>
 <?php include_partial('both_value',array('byValue' => $byValue)) ?>
-<div class="clear"></div>
+<!--<div class="clear"></div>-->
 <?php include_partial('both_taxes',array('taxes' => $taxes)) ?>
 <div class="clear"></div>
 <?php include_partial('both_user',array('byUser' => $byUser)) ?>
