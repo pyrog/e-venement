998a23ebc985fd275947bcfcb0f9a757c9ab7464
beta <betaglop@users.noreply.github.com>
2015-02-25 11:20:56 +0000

    (pos) adding a helper after displaying the declinations

---
+++ apps/pos/modules/product/templates/_show_description_for_buyers_help.php
@@ -0,0 +1,6 @@
+<div class="label ui-helper-clearfix">
+  <div class="help">
+    <span class="ui-icon ui-icon-help floatleft"></span>
+    <?php echo __('The "description for buyers" is used to offer a special content when the product is bought. It can be used for videos to give a private link for instance.') ?>
+  </div>
+</div>
