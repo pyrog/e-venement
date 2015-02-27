abd69efefc81bbd58481a35737a6e517b3475eeb
beta <betaglop@users.noreply.github.com>
2015-02-27 16:57:50 +0000

    (pub) adding assets on the surveys

--- apps/pub/modules/cart/templates/surveysSuccess.php
+++ apps/pub/modules/cart/templates/surveysSuccess.php
@@ -1,4 +1,6 @@
 <?php include_partial('survey_assets') ?>
+<?php include_partial('global/ariane',array('active' => 3)) ?>
+<?php include_partial('global/oplog') ?>
 <h1><?php echo __('The survey') ?></h1>
 <?php foreach ( $forms as $form ): ?>
   <?php include_partial('survey_form', array('form' => $form)) ?>
