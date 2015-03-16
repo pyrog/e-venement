7ad02e1358fe144f8a1fe4133e39b42df1296a3e
beta <betaglop@users.noreply.github.com>
2015-03-16 15:37:04 +0000

    (rp) improving the image integration into email content

--- apps/rp/modules/email/templates/_attachments.php
+++ apps/rp/modules/email/templates/_attachments.php
@@ -8,14 +8,16 @@
   <?php foreach ( $form->getObject()->Attachments as $att ): ?>
   <li class="attachment-<?php echo $att->id ?>">
     <a href="<?php echo $att->getWebUri() ?>" target="_blank"><?php echo $att ?></a>
+    <?php if ( preg_match('!^image\/!', $att->mime_type) === 1 ): ?>
+    <a href="<?php echo url_for('email/integrateAttachment?id='.$form->getObject()->id.'&attachment_id='.$att->id) ?>"
+      title="<?php echo $title = __('Append to content') ?>"
+      class="fg-button-mini fg-button ui-state-default fg-button-icon-left add-to-content"
+    ><span class="ui-icon ui-icon-arrowstop-1-s"></span><?php echo $title ?></a>
+    <?php endif ?>
     <a href="<?php echo url_for('email/deleteAttachment?id='.$form->getObject()->id.'&attachment_id='.$att->id) ?>"
        class="fg-button-mini fg-button ui-state-default fg-button-icon-left"
        onclick="javascript: $.get($(this).attr('href')); $(this).closest('li').remove(); return false;"
     ><span class="ui-icon ui-icon-trash"></span><?php echo __('Delete',null,'sf_admin') ?></a>
-    <a href="<?php echo url_for('email/integrateAttachment?id='.$form->getObject()->id.'&attachment_id='.$att->id) ?>"
-      title="<?php echo __('Add to content') ?>"
-      class="fg-button-mini fg-button ui-state-default fg-button-icon-left add-to-content"
-    ><span class="ui-icon ui-icon-arrowstop-1-s"></span><?php echo __('Add to content') ?></a>
   </li>
   <?php endforeach ?>
   <li class="attachment-new">
