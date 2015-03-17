1c108c73e321d8b410f07bb87a73c52fda09c237
beta <betaglop@users.noreply.github.com>
2015-03-17 16:10:14 +0000

    (pub) adding a missing template for meta events

---
+++ apps/pub/modules/meta_event/templates/_list_header.php
@@ -0,0 +1,4 @@
+<?php include_partial('global/ariane',array('active' => 1)) ?>
+<?php include_partial('global/flashes') ?>
+
+<?php echo pubConfiguration::getText('app_texts_meta_events_header','') ?>
