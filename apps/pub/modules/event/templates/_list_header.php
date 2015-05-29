5d72e73c26c1cd9bae66ae1a605817f0152f0f08
FERRAND Bevlhay <drist-li@users.noreply.github.com>
2015-05-29 09:55:03 +0000

    (pub) adding meta-event bottom text (pub) displaying meta-event's name on event_list

--- apps/pub/modules/event/templates/_list_header.php
+++ apps/pub/modules/event/templates/_list_header.php
@@ -1,2 +1,6 @@
 <?php include_partial('global/ariane',array('active' => 1)) ?>
 <?php include_partial('global/flashes') ?>
+
+<?php if ( $slug = $sf_request->getParameter('meta-event', false) ): ?>
+<h2><?php echo Doctrine::getTable('MetaEvent')->findOneBySlug($slug) ?></h2>
+<?php endif ?>
---
+++ apps/pub/modules/meta_event/templates/_list_footer.php
@@ -0,0 +1,3 @@
+<div class="text_config meta_event_bottom">
+  <?php echo nl2br(pubConfiguration::getText('app_texts_meta_event_bottom')) ?>
+</div>
