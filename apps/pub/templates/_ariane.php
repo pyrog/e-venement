160363662e1cb305fc76f16b88a982cf71305994
FERRAND Bevlhay <drist-li@users.noreply.github.com>
2015-06-04 08:41:31 +0000

    (pub) improve the display of links into breadcrumb

--- apps/pub/templates/_ariane.php
+++ apps/pub/templates/_ariane.php
@@ -28,31 +28,48 @@
 ?>
 <div id="ariane">
   <div class="login choices <?php if ( $active == $nb ) echo 'active'; else echo 'past' ?> access">
-    <?php
-      echo $sf_user->hasContact()
-        ? link_to(__('My account'),'contact/index').' '.link_to(__('Logout'),'login/out')
-        : link_to(__('Login'),'login/index').' '.link_to(__('Create an account'),'contact/new')
-    ?>
+    <ul>
+      <li>
+        <?php
+        echo $sf_user->hasContact()
+          ? link_to(__('My account'),'contact/index')
+          : link_to(__('Login'),'login/index')
+        ?>
+      </li>
+      <li>
+        <?php
+        echo $sf_user->hasContact()
+          ? link_to(__('Logout'),'login/out')
+          : link_to(__('Create an account'),'contact/new')
+        ?>
+      </li>
+    </ul>
   </div>
   <?php $nb++ ?>
   <div class="event choices <?php if ( $active == $nb ) echo 'active'; else echo $active < $nb ? 'future' : 'past' ?> access <?php echo $sf_user->isStoreActive() ? 'with-store' : '' ?>">
-    <?php echo link_to(sfConfig::get('app_informations_index',__('Events')), '@homepage', array('class' => 'event')) ?>
-    <?php if ( $sf_user->isStoreActive() ): ?>
-    <?php echo link_to(sfConfig::get('app_informations_store',__('Store')), 'store/index', array('class' => 'store')) ?>
-    <?php endif ?>
-    <?php if ( $sf_user->getGuardUser()->MemberCards->count() > 0 ): ?>
-    <?php echo link_to(sfConfig::get('app_member_cards_title',false) ? pubConfiguration::getText('app_member_cards_title') : __('Member cards'),'card/index', array('class' => 'mc')) ?>
-    <?php endif ?>
+    <ul>
+      <li><?php echo link_to(sfConfig::get('app_informations_index',__('Events')), '@homepage', array('class' => 'event')) ?></li>
+      <?php if ( $sf_user->isStoreActive() ): ?>
+      <li><?php echo link_to(sfConfig::get('app_informations_store',__('Store')), 'store/index', array('class' => 'store')) ?></li>
+      <?php endif ?>
+      <?php if ( $sf_user->getGuardUser()->MemberCards->count() > 0 ): ?>
+      <li><?php echo link_to(sfConfig::get('app_member_cards_title',false) ? pubConfiguration::getText('app_member_cards_title') : __('Member cards'),'card/index', array('class' => 'mc')) ?></li>
+      <?php endif ?>
+    </ul>
   </div>
   <?php $nb++ ?>
   <div class="cart <?php if ( $active == $nb ) echo 'active'; else echo $active < $nb ? 'future' : 'past' ?> access">
-    <p class="cart"><?php echo link_to(__('Cart'),'cart/show') ?></p>
-    <?php if ( count($cultures = sfConfig::get('project_internals_cultures',array('fr' => 'Français'))) > 1 ): ?>
-    <?php endif ?>
+    <ul>
+      <li><?php echo link_to(__('Cart'),'cart/show') ?></li>
+      <?php if ( count($cultures = sfConfig::get('project_internals_cultures',array('fr' => 'Français'))) > 1 ): ?>
+      <?php endif ?>
+    </ul>
   </div>
   <?php $nb++ ?>
   <div class="command <?php if ( $active == $nb ) echo 'active'; else echo $active < $nb ? 'future' : 'past' ?> access">
-    <?php echo __('Command') ?>
+    <ul>
+      <li><?php echo __('Command') ?></li>
+    </ul>
     <?php if ( count($cultures) > 1 ): ?>
     <div id="translation">
       <?php foreach ( $cultures as $culture => $lang ): ?>
