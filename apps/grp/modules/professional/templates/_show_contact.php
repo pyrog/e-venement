a0b83eef2e92da503b05154bb6a471d2e18d994d
Baptiste SIMON <baptiste.simon@e-glop.net>
2015-06-15 10:11:57 +0200

    (pub) correcting blocking (but easy) bugs accessing the creation of professionals, the creation of new entries...

--- apps/grp/modules/professional/templates/_show_contact.php
+++ apps/grp/modules/professional/templates/_show_contact.php
@@ -1,8 +1,11 @@
-  <p class="contact">
+<p class="contact">
+  <?php if ( $professional ): ?>
     <?php echo cross_app_link_to($professional->Contact,'rp','contact/show?id='.$professional->Contact->id) ?>
     (<?php echo cross_app_link_to($professional->Organism,'rp','organism/show?id='.$professional->Organism->id) ?>
     -
     <?php echo $professional->name ?>)
-    <span class="picto"><?php echo $professional->getRaw('groups_picto') ?></span>
-  </p>
-
+    <span class="picto"><?php echo $sf_data->getRaw('professional')->groups_picto ?></span>
+  <?php else: ?>
+    &nbsp;
+  <?php endif ?>
+</p>
