617d83a8d1ca89577dd69ddddd260f3b751ee778
Baptiste SIMON <beta@e-glop.net>
2015-07-16 12:29:54 +0200

    (pub) implementing the "depends_on" features in the pub app -IMPORT FROM v2.7-

--- apps/pub/modules/manifestation/templates/_show_title.php
+++ apps/pub/modules/manifestation/templates/_show_title.php
@@ -1,5 +1,8 @@
 <?php use_helper('Date') ?>
 <h1><?php echo __('Choose tickets') ?></h1>
 <div id="event"><?php echo $manifestation->Event ?></div>
+<?php if ( $manifestation->depends_on ): ?>
+<div id="depends_on">+ <?php echo $manifestation->DependsOn->Event ?></div>
+<?php endif ?>
 <div id="manifestation"><?php echo __('on') ?> <?php echo $manifestation->getFormattedDate() ?></div>
 <div id="location"><?php echo __('location') ?> : <?php echo $manifestation->Location ?></div>
