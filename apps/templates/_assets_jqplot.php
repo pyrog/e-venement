7e7a58a6ba67acc62a03955b46a0c849753a7d7f
Baptiste SIMON <baptiste.simon@e-glop.net>
2015-07-21 18:00:57 +0200

    (global) adding a template that makes jqplot integration a bit more easy about the JS inclusions

---
+++ apps/templates/_assets_jqplot.php
@@ -0,0 +1,11 @@
+<?php use_stylesheet('/js/jqplot/jquery.jqplot.css') ?>
+<?php use_stylesheet('jqplot') ?>
+<?php use_javascript('jqplot') ?>
+<?php use_javascript('/js/jqplot/jquery.jqplot.js') ?>
+<?php use_javascript('/js/jqplot/jqplot.axisLabelRenderer.js') ?>
+<?php use_javascript('/js/jqplot/jqplot.axisTickRenderer.js') ?>
+<?php use_javascript('/js/jqplot/plugins/jqplot.categoryAxisRenderer.js') ?>
+<?php use_javascript('/js/jqplot/plugins/jqplot.pointLabels.js') ?>
+<?php use_javascript('/js/jqplot/plugins/jqplot.barRenderer.js') ?>
+<?php use_javascript('/js/jqplot/plugins/jqplot.highlighter.js') ?>
+
