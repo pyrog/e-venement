31a55ad2cdeccde1700e20bdd8162994d5512c70
Baptiste SIMON <baptiste.simon@e-glop.net>
2015-08-27 09:36:36 +0200

    (stats) adding a missing template -IMPORT FROM v2.7-

---
+++ apps/stats/modules/geo/templates/_show_header.php
@@ -0,0 +1,15 @@
+<div class="ui-widget-content show-header ui-corner-all">
+  <p>
+    Attention, par défaut le système décompte le nombre de contacts et non le nombre
+    de billets. Voir dans les filtres pour décompter les billets.
+  </p>
+  <p>
+    Si vous préférez afficher ici une approche budgétaire (contre l'approche en
+    nombre de billets par défaut), précisez-le dans les filtres également. Dans
+    tous les cas une extraction reprendra l'ensemble des informations.
+  </p>
+  <p>
+    Les chiffres annoncés ici peuvent être imprécis. Les seuls chiffres garantis sont
+    ceux issus des livres comptables.
+  </p>
+</div>
