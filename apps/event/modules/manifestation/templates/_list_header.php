66af53d0148d9e1c443ed6bcc123a2d21433e086
Baptiste SIMON <baptiste.simon@libre-informatique.fr>
2015-09-23 12:02:32 +0200

    (event) improving the comments about the generation of the neighborhood in seated plans

--- apps/event/modules/manifestation/templates/_list_header.php
+++
@@ -1,1 +0,0 @@
-<?php include_partial('event/list_header_quick_search') ?>
--- apps/event/modules/seated_plan/templates/_form_links_build.php
+++ apps/event/modules/seated_plan/templates/_form_links_build.php
@@ -3,6 +3,7 @@
   array(
     'name'  => 'contiguous',
     'label' => __('Contiguous?'),
+    'helper'=> __('If checked, A3 is between A2 & A4. If not, A3 is between A1 & A5...'),
     'type'  => 'checkbox',
     'value' => 'yes',
     'size'  => false,
