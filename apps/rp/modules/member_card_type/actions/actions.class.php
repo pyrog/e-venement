1f48582403c2015c144c494cf76a40828a21c9d4
Baptiste SIMON <baptiste.simon@e-glop.net>
2015-11-16 17:36:00 +0100

    (rp) adding a feature that resets all MemberCards' available Prices

--- apps/rp/modules/member_card_type/actions/actions.class.php
+++ apps/rp/modules/member_card_type/actions/actions.class.php
@@ -17,4 +17,52 @@ class member_card_typeActions extends autoMember_card_typeActions
   {
     $this->redirect('member_card_type/edit?id='.$request->getParameter('id'));
   }
+  
+  public function executeClean(sfWebRequest $request)
+  {
+    $q = Doctrine::getTable('MemberCardType')->createQuery('mct')
+      ->leftJoin('mct.MemberCards mc WITH mc.expire_at > ?', date('Y-m-d'))
+      ->leftJoin('mc.Tickets tck WITH tck.printed_at IS NOT NULL OR tck.integrated_at IS NOT NULL')
+      ->leftJoin('tck.Manifestation m')
+      ->leftJoin('mct.MemberCardPriceModels mcpm')
+      ->andWhere('mct.id = ?', $request->getParameter('id'));
+    $this->forward404Unless($this->member_card_type = $q->fetchOne());
+    
+    $mcps = Doctrine::getTable('MemberCardPrice')->createQuery('mcp')
+      ->andWhere('mcp.member_card_id IN (SELECT mc.id FROM MemberCard mc WHERE mc.member_card_type_id = ? AND mc.expire_at > ?)', array($this->member_card_type->id, date('Y-m-d')))
+      ->delete()
+      ->execute();
+    ;
+    
+    foreach ( $this->member_card_type->MemberCards as $mc )
+    {
+      $tickets = new Doctrine_Collection('Ticket');
+      $tickets->merge($mc->Tickets);
+      foreach ( $this->member_card_type->MemberCardPriceModels as $mcpm )
+      for ( $i = $mcpm->quantity ; $i > 0 ; $i-- )
+      {
+        $stop = false;
+        foreach ( $tickets as $key => $ticket )
+        if ( $mcpm->price_id == $ticket->price_id
+          && $mcpm->event_id == $ticket->Manifestation->event_id )
+        {
+          $stop = true;
+          unset($tickets[$key]);
+          break;
+        }
+        if ( $stop )
+        {
+          $mcp = new MemberCardPrice;
+          $mcp->price_id = $mcpm->price_id;
+          $mcp->event_id = $mcpm->event_id;
+          $mc->MemberCardPrices[] = $mcp;
+        }
+      }
+    }
+    $this->member_card_type->MemberCards->save();
+    
+    $this->getContext()->getConfiguration()->loadHelpers('I18N');
+    $this->getUser()->setFlash('notice', __('%%nb%% member card(s) have been cleaned up', array('%%nb%%' => $this->member_card_type->MemberCards->count())));
+    $this->redirect('member_card_type/edit?id='.$this->member_card_type->id);
+  }
 }
