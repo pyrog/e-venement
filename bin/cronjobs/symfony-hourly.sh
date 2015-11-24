65b846e741222770e62c027b8818b0f21f40525b
Baptiste SIMON <baptiste.simon@libre-informatique.fr>
2015-11-24 13:01:31 +0100

    (bin) improving the cronjobs to avoid multiple executions at the same time, because it can create a DOS

--- bin/cronjobs/symfony-hourly.sh
+++ bin/cronjobs/symfony-hourly.sh
@@ -3,6 +3,9 @@
 #EVENEMENT_DIRS='/var/www/e-venement.domain.tld'
 EVENEMENT_DIRS=''
 EVENEMENT_ROOT_DIRS=/var/www
+LOCKFILE_PREFIX=cache/cron
+
+touch /tmp/jypasse.log
 
 if [ -n "$EVENEMENT_ROOT_DIRS" ]; then
 for dir in `ls $EVENEMENT_ROOT_DIRS/`; do
@@ -16,7 +19,13 @@ for dir in $EVENEMENT_DIRS; do
   echo $dir
   cd /var/www/$dir
   if [ -x ./symfony ]; then
-    ./symfony project:send-emails --env=prod --application=rp
-    ./symfony e-venement:send-manifestation-notifications
+    [ -e $LOCKFILE.minutely.lock ] && rm $LOCKFILE.minutely.lock
+    if [ ! -e $LOCKFILE.hourly.lock ]; then
+      touch $LOCKFILE.hourly.lock
+      ./symfony e-venement:send-emails --env=prod --application=rp --time-limit=3600 &
+      ./symfony e-venement:send-manifestation-notifications
+      rm $LOCKFILE.hourly.lock
+      touch $LOCKFILE.hourly.quittime
+    if
   fi
 done
