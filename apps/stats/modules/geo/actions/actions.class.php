8d541dde85967c27bd929302a97086ff38961242
Baptiste SIMON <baptiste.simon@libre-informatique.fr>
2015-12-17 10:20:07 +0100

    (stats) correcting the geographical statistics to support postal codes starting with a "0" (a workaround intval() comparision with strings)

--- apps/stats/modules/geo/actions/actions.class.php
+++ apps/stats/modules/geo/actions/actions.class.php
@@ -244,7 +244,7 @@ class geoActions extends sfActions
       $cpt = 0;
       foreach ( $res[$count_tickets ? 'tickets' : 'nb'] as $code => $qty )
       {
-        if ( intval($code).'' !== ''.$code )
+        if ( str_pad(intval($code).'',5,'0',STR_PAD_LEFT) !== ''.$code )
         {
           foreach ( array('nb' => 1, 'tickets' => 'qty', 'value' => 'sum') as $approach => $field )
           {
@@ -293,7 +293,7 @@ class geoActions extends sfActions
       $cpt = 0;
       foreach ( $res[$count_tickets ? 'tickets' : 'nb'] as $code => $qty )
       {
-        if ( intval($code).'' !== ''.$code )
+        if ( str_pad(intval($code).'',2,'0',STR_PAD_LEFT) !== ''.$code )
         {
           foreach ( array('nb' => 1, 'tickets' => 'qty', 'value' => 'sum') as $approach => $field )
           {
