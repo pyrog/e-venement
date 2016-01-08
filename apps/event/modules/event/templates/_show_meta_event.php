5f0536b985029af6ca449af865dbb763aff672df
Baptiste SIMON <baptiste.simon@libre-informatique.fr>
2016-01-08 17:38:28 +0100

    (event) correcting a bad link to meta_event from an event

--- apps/event/modules/event/templates/_show_meta_event.php
+++ apps/event/modules/event/templates/_show_meta_event.php
@@ -1,4 +1,4 @@
 <div class="sf_admin_form_row">
   <label><?php echo __('Meta Event') ?>:</label>
-  <?php echo link_to($event->MetaEvent,'event_category/show?id='.$event->MetaEvent->id) ?>
+  <?php echo link_to($event->MetaEvent,'meta_event/show?id='.$event->MetaEvent->id) ?>
 </div>
