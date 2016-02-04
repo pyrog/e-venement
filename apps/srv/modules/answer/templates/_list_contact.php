804516f45a63cc01e4dc9fc000114e34b5328c0b
Baptiste SIMON <baptiste.simon@e-glop.net>
2016-02-04 15:30:19 +0100

    (srv) adding the support of the named (aka "with direct contact") answers in the list of answers

--- apps/srv/modules/answer/templates/_list_contact.php
+++ apps/srv/modules/answer/templates/_list_contact.php
@@ -1,7 +1,13 @@
-<?php if ( $survey_answer->Group->contact_id ): ?>
-<?php echo cross_app_link_to(
-  $survey_answer->Group->professional_id ? $survey_answer->Group->Professional : $survey_answer->Group->Contact,
+<?php
+  $contact = NULL;
+  if ( $survey_answer->contact_id )
+    $contact = $survey_answer->Contact;
+  elseif ( $survey_answer->Group->contact_id )
+    $contact = $survey_answer->Group->Contact;
+?>
+<?php if ( $contact ): ?>
+<?php echo cross_app_link_to(!$survey_answer->contact_id && $survey_answer->Group->professional_id ? $survey_answer->Group->Professional : $contact,
   'rp',
-  'contact/edit?id='.$survey_answer->Group->contact_id
+  'contact/edit?id='.$contact->id
 ) ?>
 <?php endif ?>
