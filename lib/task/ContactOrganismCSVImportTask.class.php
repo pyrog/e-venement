a2c96cc22cba017de60e7a2ae79f1ea6d820e1f6
Baptiste SIMON <baptiste.simon@libre-informatique.fr>
2016-02-12 19:01:33 +0100

    (rp) finishing the import task for RP using a task

--- lib/task/ContactOrganismCSVImportTask.class.php
+++ lib/task/ContactOrganismCSVImportTask.class.php
@@ -40,6 +40,8 @@ class ContactOrganismCSVImportTask extends sfBaseTask{
       new sfCommandArgument('go', sfCommandArgument::OPTIONAL, 'no'),
     ));
     $this->addOptions(array(
+      new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environement', 'dev'),
+      new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application', 'default'),
       new sfCommandOption('no-headers', sfCommandOption::PARAMETER_OPTIONAL),
     ));
     $this->namespace = 'e-venement';
@@ -50,50 +52,152 @@ class ContactOrganismCSVImportTask extends sfBaseTask{
 
   protected function execute($arguments = array(), $options = array())
   {
-    throw new liEvenementException('Work still in progress... contact@libre-informatique.fr for more information.');
+    //throw new liEvenementException('Work still in progress... contact@libre-informatique.fr for more information.');
     
     sfContext::createInstance($this->configuration, $options['env']);
     $databaseManager = new sfDatabaseManager($this->configuration);
     
-    if (!( $fp = fopen($arguments['input']) ))
+    if (!( $fp = fopen($arguments['input'],'r') ))
       throw new liEvenementException('Error reading the file!!');
+    
+    // init
+    $contacts  = new Doctrine_Collection('Contact');
+    $organisms  = new Doctrine_Collection('Organism');
+    $orgcats    = new Doctrine_Collection('OrganismCategory');
+    foreach ( Doctrine::getTable('OrganismCategory')->createQuery('oc')->execute() as $oc )
+      $orgcats[$oc->id] = $oc;
+    $protypes   = new Doctrine_Collection('ProfessionalType');
+    foreach ( Doctrine::getTable('ProfessionalType')->createQuery('pt')->execute() as $pt )
+      $protypes[$pt->id] = $pt;
     
     $lines = fgetcsv($fp);
     if ( $options['no-headers'] )
-      array_shift($lines);
-    foreach ( $lines as $line )
+      fgetcsv($fp);
+    for ( $l = 0 ; ($line = fgetcsv($fp)) ; $l++ )
     {
       $organism = $contact = NULL;
       $i = 0;
+      
       // there is a contact
-      if ( $line[1] )
-      {
+      // if an id is given for the organism
+      $contactrank = $i+1;
+      if ( !trim($line[$i]) )
         $contact = new Contact;
-        if ( trim($line[$i++]) )
-          $contact->id = trim($line[$i]);
-        $contact->name = trim($line[$i++]);
-        $contact->firstname = trim($line[$i++]);
-        $contact->address = trim($line[$i++])."\n".trim($line[$i++]);
-        $contact->postalcode = trim($line[$i++]);
-        $contact->city = trim($line[$i++]);
-        $contact->country = trim($line[$i++]);
-        
-        if ( trim($line[$i+1]) )
-        {
-          $pn = new ContactPhonenumber;
-          $pn->name = $line[$i++];
-          $pn->number = $line[$i++];
+      elseif ( isset($contacts[trim($line[$i])]) )
+        $contact = $contacts[trim($line[$i])];
+      else
+        $contact = new Contact;
+      if ( trim($line[$i++]) )
+        $contact->id = trim($line[$i-1]);
+      foreach ( array('name', 'firstname', 'address', 'postalcode', 'city', 'country') as $field )
+        $contact->$field = trim($line[$i++]);
+      
+      for ( $j = 0 ; $j < 2 ; $j++ )
+      {
+        $pn = new ContactPhonenumber;
+        $pn->name = $line[$i++];
+        $pn->number = $line[$i++];
+        if ( trim($pn->number) )
           $contact->Phonenumbers[] = $pn;
+      }
+      
+      $contact->email = trim($line[$i++]);
+      $contact->email_no_newsletter = trim($line[$i++]) ? true : false;
+      $contact->description = trim($line[$i++]);
+      
+      if ( trim($line[$contactrank]) )
+      {
+        if ( $arguments['go'] )
+        {
+          $contact->save();
+          $contacts[$contact->id] = $contact;
+        }
+        $this->logSection('Contact', $contact.' added or updated with id #'.$contact->id);
+      }
+      
+      // there is an organism
+      // if an id is given for the organism
+      $orgrank = $i+1;
+      if ( !trim($line[$i]) || !isset($organisms[trim($line[$i])]) )
+        $organism = new Organism;
+      else
+        $organism = $organisms[$line[$i]];
+      
+      if ( trim($line[$i++]) )
+        $organism->id = trim($line[$i-1]);
+      $organism->name = trim($line[$i++]);
+      
+      // Organism Category
+      $cat = $line[$i++];
+      if ( trim($line[$i+1]) )
+      {
+        if ( ($key = array_search($cat, $orgcats->toKeyValueArray('id', 'name'))) !== false )
+          $organism->Category = $orgcats[$key];
+        else
+        {
+          $organism->Category->name = trim($cat);
+          $organism->Category->save();
+          $orgcats[$organism->Category->id] = $organism->Category;
+        }
+      }
+      
+      foreach ( array('administrative_number', 'url', 'address', 'postalcode', 'city', 'country') as $field )
+        $organism->$field = trim($line[$i++]);
+      
+      for ( $j = 0 ; $j < 2 ; $j++ )
+      {
+        $pn = new OrganismPhonenumber;
+        $pn->name = $line[$i++];
+        $pn->number = $line[$i++];
+        if ( trim($pn->number) )
+          $organism->Phonenumbers[] = $pn;
+      }
+      
+      $organism->email = trim($line[$i++]);
+      $organism->email_no_newsletter = trim($line[$i++]) ? true : false;
+      
+      if ( trim($line[$orgrank]) )
+      {
+        if ( $arguments['go'] )
+        {
+          $organism->save();
+          $organisms[$organism->id] = $organism;
+        }
+        $this->logSection('Organism', $organism.' added or updated with id #'.$organism->id);
+      }
+      
+      // there is a professional
+      if ( trim($line[1]) && trim($line[$orgrank]) )
+      {
+        $pro = new Professional;
+        $pro->Contact = $contact;
+        $pro->Organism = $organism;
+        
+        $type = $line[$i++];
+        if ( ($key = array_search($type, $protypes->toKeyValueArray('id', 'name'))) !== false )
+          $pro->ProfessionalType = $protypes[$key];
+        else
+        {
+          $pro->ProfessionalType = new ProfessionalType;
+          $pro->ProfessionalType->name = $type;
+          $pro->ProfessionalType->save();
+          $protypes[$pro->ProfessionalType->id] = $pro->ProfessionalType;
         }
         
-        $contact->email = trim($line[$i++]);
-        $contact->email_no_newsletter = trim($line[$i++]) ? true : false;
-        $contact->description = trim($line[$i++]);
+        foreach ( array('name', 'department', 'contact_number', 'contact_email') as $field )
+          $pro->$field = $line[$i++];
         
-        // TODO
+        $pro->contact_email_no_newsletter = trim($line[$i++]) ? true : false;
+        
+        if ( $arguments['go'] )
+          $pro->save();
+        $this->logSection('Professional', 'A position has been added for contact '.$contact.' in organism '.$organism);
       }
     }
     
-    $this->logSection('Clean', 'Useless WebOrigins ... '.$cpt.' ... done');
+    fclose($fp);
+    $this->logSection('Done', '... done '.(!$arguments['go'] ? 'but for fake: add the [go] parameter to do it for real' : ''));
+    if ( $arguments['go'] )
+      $this->logSection('Warning', 'Check out your sequences (contact_id_seq & organism_id_seq), if ids were given by your input file instead of the internal sequences.');
   }
 }
