79d4ff0c4cfa08b3a70e2db71e6659702646b093
Baptiste SIMON <baptiste.simon@libre-informatique.fr>
2016-02-23 21:29:46 +0100

    (rp) correcting the "new" action of contacts

--- apps/rp/modules/contact/templates/_tdp/assets.php
+++ apps/rp/modules/contact/templates/_tdp/assets.php
@@ -1,4 +1,5 @@
 <?php include_partial('assets') ?>
+<?php use_stylesheet('/sfFormExtraPlugin/css/jquery.autocompleter.css') ?>
 <?php use_stylesheet('rp-tdp') ?>
 <?php use_javascript('rp-tdp') ?>
 
